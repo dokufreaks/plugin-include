1270a31f9de95e659e59d1d863dd2b62691340e8
wikidesign <chi@chimeric.de>
2006-12-23 22:27:26 +0100

    16 Swedish language by Christer Nygren
    darcs-hash:20061223212726-89468-ccd7993a8dd482767327f7802fcd6b78c131c429.gz

---
+++ lang/se/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Swedish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christer Nygren <wiki@fronet.fro.se>
+ */
+
+// custom language strings for the plugin
+$lang['readmore']   = '→ Läs mer...';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
