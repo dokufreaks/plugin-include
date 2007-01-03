99d07c071e5cbac61b43e39b695ef1dac0d6d9e7
wikidesign <chi@chimeric.de>
2007-01-03 16:12:12 +0100

    17 French language update by Stanislas Reltgen
    darcs-hash:20070103151212-89468-6b6aa1c3857564198e87d45d3b65502153b8fcfd.gz

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,11 +4,14 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author     Stanislas Reltgen <stanislas@reltgen.net>
  */
  
 // for the configuration manager
-$lang['firstseconly']     = 'afficher uniquement la premiere section des billets';
-$lang['showdate']         = 'afficher les dates sous les billets';
-$lang['showuser']         = 'afficher le nom de l\'utilisateur sous les billets';
+$lang['firstseconly']    = 'afficher uniquement la premiere section des billets';
+$lang['showlink']        = 'afficher les permaliens sous les billets';
+$lang['showdate']        = 'afficher les dates sous les billets';
+$lang['showuser']        = "afficher le nom de l'utilisateur sous les billets";
+$lang['usernamespace']   = 'espace de nom pour les pages des utilisateurs';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
