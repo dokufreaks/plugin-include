df2ed74c603c38f28e8e0cb02cb01756d3e69d65
wikidesign <chi@chimeric.de>
2007-01-11 00:19:17 +0100

    24 Solvenian translation by Blaž Mertelj
    darcs-hash:20070110231917-89468-4ee826bc7ab59122bf25c5bc8514eb3f148578bb.gz

---
+++ lang/sl-si/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Slovenian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Blaž Mertelj <Blaz.Mertelj@atol.si>
+ */
+ 
+// for the configuration manager
+$lang['firstseconly']       = 'prikaži samo prvo sekcijo blog vnosov';
+
+$lang['showlink']      = 'prikaži permalinke pod blog vnosi';
+$lang['showdate']      = 'prikaži datume pod blog vnosi';
+$lang['showuser']      = 'prikaži uporabniška imena pod blog vnosi';
+
+$lang['usernamespace'] = 'namespace za uporabniške strani';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
