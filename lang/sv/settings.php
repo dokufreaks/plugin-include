ae8379d6fed588110fe78f4869fac7544ea3721e
wikidesign <chi@chimeric.de>
2007-04-27 12:29:09 +0200

    42 Corrected folder name for Swedish translation se -> sv
    darcs-hash:20070427102909-89468-e4a72e260e995998762cce9c2fcf4d37f89cf157.gz

---
+++ lang/sv/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Swedish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christer Nygren <wiki@fronet.fro.se>
+ */
+ 
+// for the configuration manager
+$lang['firstseconly']  = 'visa endast första delen av inlägg';
+$lang['showlink']      = 'visa permalinks under inlägg';
+$lang['showdate']      = 'visa datum under inlägg';
+$lang['showuser']      = 'visa användarnamn under inlägg';
+$lang['usernamespace'] = 'vy för användarsidor';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
