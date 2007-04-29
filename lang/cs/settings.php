ec6a713ad57740ee40f736d89483c5c08a4b2d93
lukas.zapletal <chi@chimeric.de>
2007-04-29 23:46:33 +0200

    47 Czech translation
    darcs-hash:20070429214633-d7814-33b7a78de6c0eb1e37e0fc7c2d4265a185c3d7be.gz

---
+++ lang/cs/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Czech language file (UTF-8 encoding)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
+ */
+ 
+// for the configuration manager
+$lang['firstseconly']  = 'zobrazovat pouze první sekci vkládaných stránek';
+$lang['showtaglogos']  = 'zobrazovat obrázek prvních tagů';
+$lang['showfooter']    = 'zobrazovat dole info o vkládané stránce';
+$lang['showlink']      = 'zobrazovat dole permalinky vkládaných stránek';
+$lang['showdate']      = 'zobrazovat dole datum vkládané stránky';
+$lang['showuser']      = 'zobrazovat dole jméno uživatele vkládaných stránek';
+$lang['showcomments']  = 'zobrazovat dole komentář u vkládaných stránek vyžadován Discussion plugin)';
+$lang['showlinkbacks'] = 'zobrazovat dole zpětné odkazy vkládaných stránek (vyžadován Linkback plugin)';
+$lang['showtags']      = 'zobrazovat dole tagy vkládaných stránek (vyžadován Tag plugin)';
+$lang['showeditbtn']   = 'zobrazovat tlačítko pro editaci';
+$lang['usernamespace'] = 'jmenný prostor pro uživatelské stránky';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
