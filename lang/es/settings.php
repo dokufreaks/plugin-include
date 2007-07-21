770edfae5a4dbdbaac3b9703ec7cf3081a6becfe
wikidesign <chi@chimeric.de>
2007-07-21 10:46:02 +0200

    48 Spanish language update by Herman Fabián Sandoval Manrique
    darcs-hash:20070721084602-89468-1fe5b3f6e48e6166c7f4f66993146afbe09e6ac8.gz

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,16 +1,20 @@
 <?php
 /**
- * Archivo en español
+ * Spanish language file
  *
- * @licecia    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @autor     Esther Brunner <wikidesign@gmail.com>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
  */
  
 // para el gestor de configuraciones (configuration manager)
 $lang['firstseconly']  = 'mostrar solamente la primera sección de las entradas al blog';
+$lang['showtaglogos']  = 'mostrar imagen para la primera etiqueta';
+$lang['showfooter']    = 'mostrar en la parte inferior información acerca de la entrada al blog';
 $lang['showlink']      = 'mostrar los enlaces a continuación de las entradas al blog';
 $lang['showdate']      = 'mostrar la fecha a continuación de las entradas al blog';
 $lang['showuser']      = 'mostrar los nombres de usuarios a continuación de las entradas al blog';
+$lang['showcomments']  = 'mostrar comentarios a continuación de las entradas al blog';
+$lang['showtags']      = 'mostrar etiquetas a continuación de la entrada al blog';
 $lang['usernamespace'] = 'namespace para las páginas de usuario';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
