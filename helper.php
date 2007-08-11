50579d3b8e4f485cae4aa75d383cc7b1238d7868
wikidesign <chi@chimeric.de>
2007-08-11 15:00:43 +0200

    62 Release preparations
    darcs-hash:20070811130043-89468-1b6f58aa266030918e6b3039afb852015f2c0ee7.gz

--- helper.php
+++ helper.php
@@ -44,7 +44,7 @@ class helper_plugin_include extends DokuWiki_Plugin { // DokuWiki_Helper_Plugin
     return array(
       'author' => 'Esther Brunner',
       'email'  => 'wikidesign@gmail.com',
-      'date'   => '2007-08-10',
+      'date'   => '2007-08-11',
       'name'   => 'Include Plugin (helper class)',
       'desc'   => 'Functions to include another page in a wiki page',
       'url'    => 'http://www.wikidesign/en/plugin/include/start',
