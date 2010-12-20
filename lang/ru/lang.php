285d5ec5636a1a3ce9b0a64943ffb18070033fea
Michael Hamann <michael@content-space.de>
2010-12-20 21:22:16 +0100

    Update of the Russian translation by Aleksandr Selivanov

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,6 +4,7 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Spike <Spike@Foobar2000.Ru>
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
  */
 
 // custom language strings for the plugin
