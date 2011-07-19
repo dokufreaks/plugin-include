5cf0268cc0b3f9a20baa6229fbd2618e736ac851
Michael Hamann <michael@content-space.de>
2011-07-20 00:22:24 +0200

    Add special table section edit handling for the edittable plugin
    This should make the include plugin fully compatible with the
    edittable plugin.

--- action.php
+++ action.php
@@ -189,6 +189,13 @@ class action_plugin_include extends DokuWiki_Action_Plugin {
         } elseif ($data['target'] == 'plugin_include_end') {
             array_shift($page_stack);
         } elseif (!empty($page_stack)) {
+
+            // Special handling for the edittable plugin
+            if ($data['target'] == 'table' && !plugin_isdisabled('edittable')) {
+                $edittable =& plugin_load('action', 'edittable');
+                $data['name'] = $edittable->getLang('secedit_name');
+            }
+
             if ($page_stack[0]['writable'] && isset($data['name']) && $data['name'] !== '') {
                 $name = $data['name'];
                 unset($data['name']);
