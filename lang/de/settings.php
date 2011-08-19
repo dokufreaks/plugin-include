6dda95b37bb8eb50323eef207aba6c283e6a2a45
Michael Hamann <michael@content-space.de>
2011-08-19 14:14:51 +0200

    Translate the remaining untranslated settings into German

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -10,7 +10,8 @@
 $lang['firstseconly']  = 'Nur erster Abschnitt von eingebundenen Seiten';
 $lang['showtaglogos']  = 'Bild für erstes Tag anzeigen';
 $lang['showfooter']    = 'Infos über eingebundene Seite darunter anzeigen';
-$lang['showlink']      = 'Permalink unter eingebunderer Seite anzeigen';
+$lang['showlink']      = 'Erste Überschrift der eingebundenen Seite als Link anzeigen';
+$lang['showpermalink']      = 'Permalink unter eingebunderer Seite anzeigen';
 $lang['showdate']      = 'Datum unter eingebunderer Seite anzeigen';
 $lang['showuser']      = 'Autorenname unter eingebunderer Seite anzeigen';
 $lang['showcomments']  = 'Kommentare unter eingebundener Seite anzeigen (Discussion Plugin wird benötigt)';
@@ -19,6 +20,10 @@ $lang['showtags']      = 'Tags unter eingebundener Seite anzeigen (Tag Plugin wi
 $lang['showeditbtn']   = 'Bearbeiten-Tasten anzeigen';
 $lang['doredirect']    = 'Nach dem Bearbeiten der eingebundenen Seite zur ursprünglichen Seite weiterleiten';
 $lang['usernamespace'] = 'Namensraum für Benutzerseiten';
+$lang['doindent']      = 'Eingebundene Seiten relativ zur Seite, in der sie eingebunden sind einrücken';
 $lang['linkonly']      = 'Nur einen Link anzeigen statt dem Inhalt der eingebundenen Seite';
+$lang['title']         = 'Erste Überschrift im Link benutzen auch wenn "useheading" ausgeschaltet ist (betrifft nur den "linkonly"-Modus)';
+$lang['pageexists']    = 'Keinen Link anzeigen, wenn die verlinkte Seite nicht existiert (betrifft nur den "linkonly"-Modus)';
+$lang['parlink']       = 'Einen Absatz um den Link herum anzeigen (betrifft nur den "linkonly"-Modus)';
 
 //Setup VIM: ex: et ts=2 :
