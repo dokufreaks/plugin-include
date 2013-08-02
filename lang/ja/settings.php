ca626d5623b4d212d3925304c75b627d7bf2e3ff
Satoshi Sahara <sahara.satoshi@gmail.com>
2013-08-02 23:03:42 +0900

    Japanese language files

---
+++ lang/ja/settings.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Japanese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['noheader']      = 'インクルードするページまたはセクションの最初の見だしを表示しない';
+$lang['firstseconly']  = '最初のセクションだけをインクルードする';
+$lang['showtaglogos']  = '（タグが設定されている場合） show image for first tag';
+$lang['showlink']      = 'インクルード部の最初の見だしをインクルード先のページまたはセクションへのリンクとする';
+$lang['showfooter']    = 'インクルード部の下にフッターを表示する';
+$lang['showpermalink'] = 'フッター情報: インクルード先への permalink を表示';
+$lang['showdate']      = 'フッター情報: ページ作成日付(date) を表示';
+$lang['showmdate']     = 'フッター情報: ページ修正日付(modified date) を表示';
+$lang['showuser']      = 'フッター情報: ユーザ名(username) を表示';
+$lang['showcomments']  = 'フッター情報: コメントを表示する (Discussion plugin が必要)';
+$lang['showlinkbacks'] = 'フッター情報: バックリンク(linkback) を表示 (Linkback Plugin が必要)';
+$lang['showtags']      = 'フッター情報: タグ(tag) を表示 (Tag Plugin が必要)';
+$lang['showeditbtn']   = 'インクルード先を編集するボタンを表示';
+$lang['doredirect']    = 'インクルード先の編集後、元のページにリダイレクトして戻る';
+$lang['usernamespace'] = 'ユーザーページの名前空間';
+$lang['doindent']      = 'インクルード部を元ページの当該位置での見出しレベルより1段階下げる';
+$lang['linkonly']      = 'linkonly モードをデフォルトにする： インクルードせず、指定ページまたはセクションへのリンクだけを表示';
+$lang['title']         = '(linkonly モード時)： リンクタイトルに最初の見出しを使う（useheading の指定によらず適用）';
+$lang['pageexists']    = '(linkonly モード時)： インクルードするページが存在しない場合は何も表示しない ';
+$lang['parlink']       = '(linkonly モード時)： リンクを「段落」とする （インラインリストにする場合は無効にしてください）';
+$lang['safeindex']     = 'prevent indexing of metadata from non-public included pages';
+$lang['order']         = '(名前空間インクルード時) ページソートに使う項目';
+$lang['order_o_id']    = 'ページ ID';
+$lang['order_o_title'] = 'タイトル';
+$lang['order_o_created'] = '作成日時';
+$lang['order_o_modified'] = '修正日時';
+$lang['order_o_indexmenu'] = 'indexmenu プラグイン互換のソート方法';
+$lang['order_o_custom'] = 'include プラグイン互換のソート方法';
+$lang['rsort']         = '(名前空間インクルード時) ページソートを逆順にする';
+$lang['depth']         = '(名前空間インクルード時) インクルード対象とする名前空間の最大深さ （0:制限なし）';
+//Setup VIM: ex: et ts=2 :
