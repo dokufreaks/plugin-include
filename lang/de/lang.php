5189be7aafc730343d701348d5d83a91f8b923ba
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-03 22:40:05 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,12 +1,8 @@
 <?php
+
 /**
- * German language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Esther Brunner <wikidesign@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['readmore']   = '→ Weiter lesen...';
-
-//Setup VIM: ex: et ts=2 :
+$lang['readmore']              = '→ Weiter lesen...';
