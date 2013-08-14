3b4bd7670c61ab50a6845b91b156bd2343a957b5
Myeongjin <aranet100@gmail.com>
2013-08-14 15:05:49 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['noheader']              = '포함된 문서/문단의 첫 문단 제목을 보이지 않기';
+$lang['firstseconly']          = '포함된 문서의 첫 문단만 보이기';
+$lang['showtaglogos']          = '첫 태그에 대한 그림 보이기';
+$lang['showfooter']            = '포함된 문서 아래에 대한 정보 보이기';
+$lang['showlink']              = '포함된 문서의 첫 문단 제목에 링크';
+$lang['showpermalink']         = '포함된 문서 아래에 고유링크 보이기';
+$lang['showdate']              = '포함된 문서 아래에 날짜 보이기';
+$lang['showmdate']             = '포함된 문서 아래에 수정된 날짜 보이기';
+$lang['showuser']              = '포함된 문서 아래에 사용자 이름 보이기';
+$lang['showcomments']          = '포함된 문서 아래에 덧글 보이기 (Discussion 플러그인 필요)';
+$lang['showlinkbacks']         = '포함된 문서 아래에 링크백 보이기 (Linkback 플러그인 필요)';
+$lang['showtags']              = '포함된 문서 아래에 태그 보이기 (Tag 플러그인 필요)';
+$lang['showeditbtn']           = '편집 버튼 보이기';
+$lang['doredirect']            = '포함된 문서를 편집하고 나서 원래 문서로 넘겨주기';
+$lang['usernamespace']         = '사용자 문서에 대한 이름공간';
+$lang['doindent']              = '원래 문서의 해당 위치에서 포함된 문서를 들여쓰기';
+$lang['linkonly']              = '포함된 문서 대신 내용에 링크만 보이기';
+$lang['title']                 = 'useheading이 꺼져 있어도 링크에서 문서의 첫 제목을 사용 (linkonly 모드만 영향을 줍니다)';
+$lang['pageexists']            = '문서가 존재하지 않을 떄 링크를 보이지 않기 (linkonly 모드만 영향을 줍니다)';
+$lang['parlink']               = '링크 주위에 문단 넣기 (linkonly 모드만 영향을 줍니다)';
+$lang['safeindex']             = '공개되지 않은 포함된 문서에서 메타데이터의 색인을 방지';
+$lang['order']                 = '여러 문서로 포함의 조건 정렬';
+$lang['order_o_id']            = '문서 ID';
+$lang['order_o_title']         = '제목';
+$lang['order_o_created']       = '만든 날짜';
+$lang['order_o_modified']      = '수정한 날짜';
+$lang['order_o_indexmenu']     = 'indexmenu 문법 호환 정렬 방법';
+$lang['order_o_custom']        = 'include 문법 호환 정렬 방법';
+$lang['rsort']                 = '포함된 문서의 정렬 순서를 반대로';
+$lang['depth']                 = '이름공간 포함의 최대 깊이, 제한 없는 깊이는 0';
