3d05920b3654375b288ef4b683d0b52f3fd95280
LauraEun <laura.eun@live.de>
2013-08-25 22:54:05 +0200

    added de-informal

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+
+// for the configuration manager
+$lang['noheader']      = 'Zeige nicht den ersten Header von eingeschlossenen Seiten/Sektionen';
+$lang['firstseconly']  = 'Nur erster Abschnitt von eingebundenen Seiten';
+$lang['showtaglogos']  = 'Bild für erstes Tag anzeigen';
+$lang['showfooter']    = 'Infos über eingebundene Seite darunter anzeigen';
+$lang['showlink']      = 'Erste Überschrift der eingebundenen Seite als Link anzeigen';
+$lang['showpermalink'] = 'Permalink unter eingebunderer Seite anzeigen';
+$lang['showdate']      = 'Datum unter eingebundender Seite anzeigen';
+$lang['showmdate']     = 'Datum der Bearbeitung unter eingebundender Seite anzeigen';
+$lang['showuser']      = 'Autorenname unter eingebunderer Seite anzeigen';
+$lang['showcomments']  = 'Kommentare unter eingebundener Seite anzeigen (Discussion Plugin wird benötigt)';
+$lang['showlinkbacks'] = 'Linkbacks unter eingebundener Seite anzeigen (Linkback Plugin wird benötigt)';
+$lang['showtags']      = 'Tags unter eingebundener Seite anzeigen (Tag Plugin wird benötigt)';
+$lang['showeditbtn']   = 'Bearbeiten-Tasten anzeigen';
+$lang['doredirect']    = 'Nach dem Bearbeiten der eingebundenen Seite zur ursprünglichen Seite weiterleiten';
+$lang['usernamespace'] = 'Namensraum für Benutzerseiten';
+$lang['doindent']      = 'Eingebundene Seiten relativ zur Seite, in der sie eingebunden sind einrücken';
+$lang['linkonly']      = 'Nur einen Link anzeigen statt dem Inhalt der eingebundenen Seite';
+$lang['title']         = 'Erste Überschrift im Link benutzen auch wenn "useheading" ausgeschaltet ist (betrifft nur den "linkonly"-Modus)';
+$lang['pageexists']    = 'Keinen Link anzeigen, wenn die verlinkte Seite nicht existiert (betrifft nur den "linkonly"-Modus)';
+$lang['parlink']       = 'Einen Absatz um den Link herum anzeigen (betrifft nur den "linkonly"-Modus)';
+$lang['safeindex']     = 'verhindern der Indizierung der Metadaten von nicht-öffentlichen Seiten';
+$lang['order']         = 'Sortierkriterium von Einschlüssen mit multiplen Seiten';
+$lang['rsort']         = 'kehre die Sortierreihenfolge von eingeschlossenen Seiten um';
+$lang['depth']         = 'maximale Tiefe von Namespace Einschlüssen, 0 für unbegrenzte Tiefe';
