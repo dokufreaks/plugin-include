306441b323f1875b372e9de7237f64143817837d
Michael Hamann <michael@content-space.de>
2013-11-25 00:04:04 +0100

    Change pagemove support into move support
    The new pagemove plugin has been renamed into move.

--- _test/pagemove_support.test.php
+++ _test/pagemove_support.test.php
@@ -5,31 +5,31 @@
  */
 class plugin_include_pagemove_support_test extends DokuWikiTest {
     public function setup() {
-        $this->pluginsEnabled[] = 'pagemove';
+        $this->pluginsEnabled[] = 'move';
         $this->pluginsEnabled[] = 'include';
         parent::setup();
     }
 
     public function test_relative_include() {
         global $ID;
-        /** @var $pagemove helper_plugin_pagemove */
-        $pagemove = plugin_load('helper', 'pagemove');
-        if (!$pagemove) return; // disable the test when pagemove is not installed
+        /** @var $move helper_plugin_move */
+        $move = plugin_load('helper', 'move');
+        if (!$move) return; // disable the test when move is not installed
         saveWikiText('editx', '{{page>start#start}} %%{{page>start}}%% {{section>wiki:syntax#tables&nofooter}} {{page>:}} {{section>test:start#test}}', 'Testcase created');
         idx_addPage('editx');
         $ID = 'editx';
         $opts['ns']      = '';
         $opts['newname'] = 'editx';
         $opts['newns']   = 'test';
-        $pagemove->move_page($opts);
+        $move->move_page($opts);
         $this->assertEquals('{{page>:start#start}} %%{{page>start}}%% {{section>wiki:syntax#tables&nofooter}} {{page>:}} {{section>test:start#test}}',rawWiki('test:editx'));
     }
 
     public function test_rename() {
         global $ID;
-        /** @var $pagemove helper_plugin_pagemove */
-        $pagemove = plugin_load('helper', 'pagemove');
-        if (!$pagemove) return; // disable the test when pagemove is not installed
+        /** @var $move helper_plugin_move */
+        $move = plugin_load('helper', 'move');
+        if (!$move) return; // disable the test when move is not installed
         saveWikiText('editx', 'Page to rename', 'Testcase create');
         saveWikiText('links', '{{section>links#foo}} {{page>editx}} {{page>:eDitX&nofooter}} {{section>editx#test}} {{page>editx&nofooter}}', 'Testcase created');
         idx_addPage('editx');
@@ -39,7 +39,7 @@ class plugin_include_pagemove_support_test extends DokuWikiTest {
         $opts['ns']      = '';
         $opts['newname'] = 'edit';
         $opts['newns']   = 'test';
-        $pagemove->move_page($opts);
+        $move->move_page($opts);
         $this->assertEquals('{{section>links#foo}} {{page>test:edit}} {{page>test:edit&nofooter}} {{section>test:edit#test}} {{page>test:edit&nofooter}}', rawWiki('links'));
     }
 }
