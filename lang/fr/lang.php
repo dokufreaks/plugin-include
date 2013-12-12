e375bed971ebce8114ad093ba60f88303af7d948
Christian "Na_kai" Sueur <sueur.christian@gmail.com>
2013-12-12 12:10:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,12 +1,8 @@
 <?php
+
 /**
- * French language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Damien Raude-Morvan <drazzib@drazzib.com>
+ * 
+ * @author Damien Raude-Morvan <drazzib@drazzib.com>
  */
-
-// custom language strings for the plugin
-$lang['readmore'] = 'Lire la suite...';
-
-//Setup VIM: ex: et ts=2 :
+$lang['readmore']              = 'Lire la suite...';
