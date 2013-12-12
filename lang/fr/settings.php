e33820a04ca98e1ad53a2bd3ea854283a44b88b5
bruno <bruno@ninesys.fr>
2013-12-12 23:15:16 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,17 +1,18 @@
 <?php
-/**
- * French language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Damien Raude-Morvan <drazzib@drazzib.com>
- * @author     Stanislas Reltgen <stanislas@reltgen.net>
- */
- 
-// for the configuration manager
-$lang['firstseconly']    = 'afficher uniquement la premiere section des billets';
-$lang['showlink']        = 'afficher les permaliens sous les billets';
-$lang['showdate']        = 'afficher les dates sous les billets';
-$lang['showuser']        = "afficher le nom de l'utilisateur sous les billets";
-$lang['usernamespace']   = 'espace de nom pour les pages des utilisateurs';
 
-//Setup VIM: ex: et ts=2 :
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author Stanislas Reltgen <stanislas@reltgen.net>
+ * @author bruno <bruno@ninesys.fr>
+ */
+$lang['firstseconly']          = 'afficher uniquement la premiere section des billets';
+$lang['showlink']              = 'afficher les permaliens sous les billets';
+$lang['showdate']              = 'afficher les dates sous les billets';
+$lang['showuser']              = 'afficher le nom de l\'utilisateur sous les billets';
+$lang['usernamespace']         = 'espace de nom pour les pages des utilisateurs';
+$lang['order_o_id']            = 'Page ID';
+$lang['order_o_title']         = 'titre';
+$lang['order_o_created']       = 'date de création';
+$lang['order_o_modified']      = 'date de modification';
