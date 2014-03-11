04dd3073f5b77923db91fec71f291d86ba1f6a79
Aleksandr Selivanov <alexgearbox@yandex.ru>
2014-03-11 19:00:29 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -1,26 +1,29 @@
 <?php
-/**
- * Russian language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Spike <Spike@Foobar2000.Ru>
- * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
- */
- 
-// for the configuration manager
-$lang['firstseconly']  = 'Показывать только первую секцию внедряемой страницы';
-$lang['showtaglogos']  = 'Показывать изображение для первого тега';
-$lang['showfooter']    = 'Снизу показывать информацию о&nbsp;внедряемой странице';
-$lang['showlink']      = 'Снизу показывать статичную ссылку на внедряемую страницу';
-$lang['showpermalink'] = 'Снизу показывать пермассылку на&nbsp;внедряемую страницу';
-$lang['showdate']      = 'Снизу показывать дату создания внедряемой страницы';
-$lang['showuser']      = 'Снизу показывать имя пользователя';
-$lang['showcomments']  = 'Показывать комментарии ниже внедряемой страницы (требуется плагин &laquo;Обсуждение&raquo;)';
-$lang['showlinkbacks'] = 'Показывать обратные ссылки ниже внедряемой страницы (требуется плагин &laquo;Обратные ссылки&raquo;)';
-$lang['showtags']      = 'Показывать теги ниже внедряемой страницы (требуется плагин &laquo;Тег&raquo;)';
-$lang['showeditbtn']   = 'Показывать кнопку &laquo;Править&raquo;';
-$lang['doredirect']    = 'Переходить на&nbsp;оригинальную страницу после редактирования внедрённой страницы';
-$lang['usernamespace'] = 'Пространство имён для страниц пользователей';
-$lang['doindent']      = 'Создавать отступ для внедряемой страницы относительно основной страницы';
 
-//Setup VIM: ex: et ts=2 :
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Spike <Spike@Foobar2000.Ru>
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['noheader']              = 'Не показывать первый заголовок внедряемых страниц, секций';
+$lang['firstseconly']          = 'Показывать только первую секцию внедряемой страницы';
+$lang['showtaglogos']          = 'Показывать изображение для первого тега';
+$lang['showfooter']            = 'Снизу показывать информацию о внедряемой странице';
+$lang['showlink']              = 'Снизу показывать статичную ссылку на внедряемую страницу';
+$lang['showpermalink']         = 'Снизу показывать пермассылку на внедряемую страницу';
+$lang['showdate']              = 'Снизу показывать дату создания внедряемой страницы';
+$lang['showmdate']             = 'Снизу показывать дату изменения внедряемой страницы';
+$lang['showuser']              = 'Снизу показывать имя пользователя';
+$lang['showcomments']          = 'Показывать комментарии ниже внедряемой страницы (требуется плагин Discussion)';
+$lang['showlinkbacks']         = 'Показывать обратные ссылки ниже внедряемой страницы (требуется плагин Linkback)';
+$lang['showtags']              = 'Показывать теги ниже внедряемой страницы (требуется плагин Tag)';
+$lang['showeditbtn']           = 'Показывать кнопку «Править»';
+$lang['doredirect']            = 'Переходить на оригинальную страницу после редактирования внедрённой страницы';
+$lang['usernamespace']         = 'Пространство имён для страниц пользователей';
+$lang['doindent']              = 'Создавать отступ для внедряемой страницы относительно основной страницы';
+$lang['order_o_id']            = 'ID страницы';
+$lang['order_o_title']         = 'заголовку';
+$lang['order_o_created']       = 'дате создания';
+$lang['order_o_modified']      = 'дате изменения';
