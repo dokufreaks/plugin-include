35f6db9f1ea646fe7fa44e9f8687203b2c8ce13c
Johan Wijnker <johan@wijnker.eu>
2015-05-05 17:25:10 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,6 +3,6 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author     Gijs H. van Gemert <g.v.gemert@inter.nl.net>
+ * @author Gijs H. van Gemert <g.v.gemert@inter.nl.net>
  */
 $lang['readmore']              = '→ Lees verder...';
