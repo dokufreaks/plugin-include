6ec4907aa6966874c66fa8164e403943be80ce92
DelD <deldadam@gmail.com>
2015-05-09 19:00:39 +0200

    translation update

--- lang/hu/lang.php
+++ lang/hu/lang.php
@@ -1,12 +1,9 @@
 <?php
+
 /**
- * Hungarian language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Norbert Csík <norbert.csik@gmail.com>
+ * 
+ * @author Norbert Csík <norbert.csik@gmail.com>
+ * @author DelD <deldadam@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['readmore'] = 'tovább...';
-
-//Setup VIM: ex: et ts=2 :
+$lang['readmore']              = 'Tovább...';
