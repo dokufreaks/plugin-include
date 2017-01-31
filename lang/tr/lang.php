29713f6f5d33ec58daa3f3052a3b8690932e80b1
na czym można zarobić <charlesmccart@yahoo.com>
2017-01-31 19:30:21 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author İlker R. Kapaç <irifat@gmail.com>
+ * @author na czym można zarobić <charlesmccart@yahoo.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'What\'s up colleagues, fastidious piece of writing and good arguments commented 
+at this place, I am genuinely enjoying by these.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,55 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author İlker R. Kapaç <irifat@gmail.com>
+ * @author na czym można zarobić <charlesmccart@yahoo.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hi!';
+$lang['firstseconly']          = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I am genuinely enjoying by these.';
+$lang['showtaglogos']          = '18';
+$lang['showfooter']            = 'Chana Twitchell is how she\'s called and her husband doesn\'t like it at many of. Kentucky is his birth place but his wife wants for you to move. Badge collecting is something that I did for decades. Since she was 18 she\'s been working to be a dispatcher. He\'s not godd at design however, you might desire to check his website: http://dobrakasa.co.pl';
+$lang['showlink']              = 'http://dobrakasa.co.pl';
+$lang['showpermalink']         = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at 
+this place, I am genuinely enjoying by these.';
+$lang['showdate']              = 'What\'s up colleagues, fastidious piece of writing and good 
+arguments commented at this place, I am genuinely enjoying by these.';
+$lang['showmdate']             = 'What\'s up colleagues, fastidious piece of writing and good 
+arguments commented at this place, I am genuinely enjoying by 
+these.';
+$lang['showuser']              = 'What\'s up colleagues, fastidious piece of writing and 
+good arguments commented at this place, I am genuinely enjoying by these.';
+$lang['showcomments']          = 'What\'s up colleagues, fastidious piece of 
+writing and good arguments commented at this place, I am genuinely enjoying by these.';
+$lang['showlinkbacks']         = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I am genuinely enjoying by 
+these.';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'What\'s up colleagues, fastidious piece of writing and good arguments 
+commented at this place, I am genuinely enjoying by these.';
+$lang['doredirect']            = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I 
+am genuinely enjoying by these.';
+$lang['doindent']              = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I 
+am genuinely enjoying by these.';
+$lang['linkonly']              = 'http://dobrakasa.co.pl';
+$lang['title']                 = 'Just want to say Hi!';
+$lang['pageexists']            = 'http://dobrakasa.co.pl';
+$lang['parlink']               = 'http://dobrakasa.co.pl';
+$lang['safeindex']             = 'Charles';
+$lang['order']                 = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, 
+I am genuinely enjoying by these.';
+$lang['order_o_id']            = 'What\'s up colleagues, fastidious piece of writing and good 
+arguments commented at this place, I am genuinely enjoying by these.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this 
+place, I am genuinely enjoying by these.';
+$lang['order_o_modified']      = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this 
+place, I am genuinely enjoying by these.';
+$lang['order_o_indexmenu']     = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I 
+am genuinely enjoying by these.';
+$lang['order_o_custom']        = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place, I am genuinely 
+enjoying by these.';
+$lang['rsort']                 = 'Kobenhavn V';
+$lang['depth']                 = 'Charles';
+$lang['readmore']              = 'http://dobrakasa.co.pl';
+$lang['debugoutput']           = 'What\'s up colleagues, fastidious piece of writing and good arguments commented at this place,
+I am genuinely enjoying by these.';
