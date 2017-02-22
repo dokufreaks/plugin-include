5bed9a9b5f97a75af29a45a014b5ac724d9d9b25
BDSwiss jak grać <leomabelisario@bigstring.com>
2017-02-22 08:15:11 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author İlker R. Kapaç <irifat@gmail.com>
+ * @author BDSwiss jak grać <leomabelisario@bigstring.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Fantastic beat ! I wish to apprentice even as you amend your site, how can i subscribe for a blog 
+web site? The account aided me a appropriate deal. I were 
+a little bit familiar of this your broadcast provided vivid clear concept';
