bd76ad707cd941d74d3ba2df9bc112af6856fadc
free seo comparison tool <olivia_knouse@yahoo.de>
2019-11-05 02:05:20 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author free seo comparison tool <olivia_knouse@yahoo.de>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'As a result aids to get website traffic from search engines.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,39 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author free seo comparison tool <olivia_knouse@yahoo.de>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = '(Pdf) Search Engine Optimization Procedures For Organic Search';
+$lang['firstseconly']          = 'As a result aids to get website traffic from search engines.';
+$lang['showtaglogos']          = '26';
+$lang['showfooter']            = '23 year-old Marine Biologist Elvin Adney from Igloolik, enjoys to spend time 4-wheeling, organic search engine optimisation and tutoring children. Finds the globe an motivating place having spent 7 weeks at Flemish Béguinages.';
+$lang['showlink']              = 'https://seohero.uk';
+$lang['showpermalink']         = 'As a result aids to get website traffic from search engines.';
+$lang['showdate']              = 'As a result aids to get website traffic from search engines.';
+$lang['showmdate']             = 'As a result aids to get website traffic from search engines.';
+$lang['showuser']              = 'As a result aids to get website traffic from search engines.';
+$lang['showcomments']          = 'As a result aids to get website traffic from search engines.';
+$lang['showlinkbacks']         = 'As a result aids to get website traffic from search engines.';
+$lang['showtags']              = '24';
+$lang['showeditbtn']           = 'As a result aids to get website traffic from 
+search engines.';
+$lang['doredirect']            = 'As a result aids to get website traffic from search engines.';
+$lang['doindent']              = 'As a result aids to get website traffic from search engines.';
+$lang['linkonly']              = 'https://seohero.uk';
+$lang['title']                 = '(Pdf) Search Engine Optimization Procedures For Organic Search';
+$lang['pageexists']            = 'https://seohero.uk';
+$lang['parlink']               = 'https://seohero.uk';
+$lang['safeindex']             = 'Olivia';
+$lang['order']                 = 'As a result aids to get website traffic from search engines.';
+$lang['order_o_id']            = 'As a result aids to get website traffic from search engines.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'As a result aids to get website traffic from search engines.';
+$lang['order_o_modified']      = 'As a result aids to get website traffic from search engines.';
+$lang['order_o_indexmenu']     = 'As a result aids to get website traffic from search engines.';
+$lang['order_o_custom']        = 'As a result aids to get website traffic from search engines.';
+$lang['rsort']                 = 'Neuaigen';
+$lang['depth']                 = 'Olivia';
+$lang['readmore']              = 'https://seohero.uk';
+$lang['debugoutput']           = 'As a result aids to get website traffic from search engines.';
