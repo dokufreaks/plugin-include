d4695c13f01367c22c576b3e1e56be584a6fb4e0
Thien Hau <thienhau.9a14@gmail.com>
2020-10-08 15:10:05 +0200

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Thien Hau <thienhau.9a14@gmail.com>
+ */
+$lang['noheader']              = 'Không hiển thị đầu đề đầu tiên của các trang/phần được bao gồm';
+$lang['firstseconly']          = 'Chỉ hiển thị phần đầu tiên của các trang được bao gồm';
+$lang['showtaglogos']          = 'Hiển thị hình ảnh cho thẻ đầu tiên';
+$lang['showfooter']            = 'Hiển thị thông tin về trang được bao gồm bên dưới';
+$lang['showlink']              = 'Liên kết đầu đề đầu tiên của trang được bao gồm';
+$lang['showpermalink']         = 'Hiển thị liên kết cố định bên dưới trang được bao gồm';
+$lang['showdate']              = 'Hiển thị ngày bên dưới trang được bao gồm';
+$lang['showmdate']             = 'Hiển thị ngày sửa đổi bên dưới trang được bao gồm';
+$lang['showuser']              = 'Hiển thị tên thành viên bên dưới trang bao gồm';
+$lang['showcomments']          = 'Hiển thị nhận xét bên dưới trang được bao gồm (Cần có plugin Discussion)';
+$lang['showlinkbacks']         = 'Hiển thị linkbacks bên dưới trang được bao gồm (Cần có Plugin Linkback)';
+$lang['showtags']              = 'Hiển thị các thẻ bên dưới trang được bao gồm (Cần có Plugin Tag)';
+$lang['showeditbtn']           = 'Hiển thị nút sửa đổi';
+$lang['doredirect']            = 'Đổi hướng đến trang gốc sau khi sửa đổi trang được bao gồm';
+$lang['doindent']              = 'Thụt lề các trang được bao gồm liên quan đến trang mà chúng được đưa vào';
+$lang['linkonly']              = 'Chỉ liên kết đến trang được bao gồm thay vì hiển thị nội dung';
+$lang['title']                 = 'Sử dụng đầu đề đầu tiên của trang trong liên kết ngay cả khi useheading bị tắt (chỉ ảnh hưởng đến chế độ chỉ liên kết (linkonly))';
+$lang['pageexists']            = 'Không hiển thị liên kết nếu trang không tồn tại (chỉ ảnh hưởng đến chế độ chỉ liên kết)';
+$lang['parlink']               = 'Đặt một đoạn quanh liên kết (chỉ ảnh hưởng đến chế độ chỉ liên kết)';
+$lang['safeindex']             = 'Ngăn lập chỉ mục siêu dữ liệu từ các trang được bao gồm không công khai';
+$lang['order']                 = 'Xếp tiêu chí của bao gồm với nhiều trang';
+$lang['order_o_id']            = 'ID trang';
+$lang['order_o_title']         = 'tiêu đề';
+$lang['order_o_created']       = 'ngày khởi tạo';
+$lang['order_o_modified']      = 'ngày sửa đổi';
+$lang['order_o_indexmenu']     = 'Xếp tùy chỉnh với cú pháp indexmenu';
+$lang['order_o_custom']        = 'Xếp tùy chỉnh với cú pháp bao gồm';
+$lang['rsort']                 = 'Đảo ngược thứ tự sắp xếp của các trang được bao gồm';
+$lang['depth']                 = 'Độ sâu tối đa của không gian tên bao gồm, 0 cho không giới hạn độ sâu';
+$lang['readmore']              = 'Hiển thị hay không liên kết \'Đọc thêm\' chỉ trong trường hợp phần đầu tiên';
+$lang['debugoutput']           = 'In thông tin gỡ lỗi dài dòng vào nhật ký gỡ lỗi dokuwiki nếu tùy chọn toàn cục "allowdebug" được bật';
