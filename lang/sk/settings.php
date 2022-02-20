15f9eded67f4bdb2f62ba2bfdd6db1d1a4bcc0b8
Igor <korcek.igor@gmail.com>
2022-02-20 18:45:16 +0100

    translation update

---
+++ lang/sk/settings.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Igor <korcek.igor@gmail.com>
+ */
+$lang['noheader']              = 'Nezobrazovať prvý nadpis zahrnutej stránky/sekcie';
+$lang['firstseconly']          = 'Zobraziť len prvú sekciu zahrnutej stránky';
+$lang['showtaglogos']          = 'Zobraziť obrázok pre prvý tag';
+$lang['showfooter']            = 'Zobraziť informáciu o zahrnutej stránke nižšie';
+$lang['showlink']              = 'Prepoj prvý nadpis zahrnutej stránky';
+$lang['showpermalink']         = 'Zobraziť trvalé odkazy pod zahrnutou stránkou';
+$lang['showdate']              = 'Zobraziť dátum pod zahrnutou stránkou';
+$lang['showmdate']             = 'Zobraziť dátum úpravy pod zahrnutou stránkou';
+$lang['showuser']              = 'Zobraziť užívateľov pod zahrnutou stránkou';
+$lang['showcomments']          = 'Zobraziť komentáre pod zahrnutou stránkou (potrebný Discussion plugin)';
+$lang['showlinkbacks']         = 'Zobraziť spätné odkazy pod zahrnutou stránkou (potrebný Linkback plugin)';
+$lang['showtags']              = 'Zobraziť tagy pod zahrnutou stránkou (potrebný Tag plugin)';
+$lang['showeditbtn']           = 'Zobraziť tlačítko upraviť';
+$lang['doredirect']            = 'Po úprave zahrnutej stránky presmerovať na pôvodnú stránku';
+$lang['doindent']              = 'Odsadiť zahrnuté stránky relatívne k stránke na ktorej budú zahrnuté';
+$lang['linkonly']              = 'Len odkazy bez zobrazenia obsahu';
+$lang['title']                 = 'Používaj prvý nadpis stránky v odkazoch aj keď je používanie nadpisov vypnuté (ovplyvní len linkonly mód)';
+$lang['pageexists']            = 'Nezobrazovať odkaz ak stránka neexistuje (ovplyvňuje len linkonly mód)';
+$lang['parlink']               = 'Vlož odstavec k odkazu (ovplyvňuje iba mód linkonly)';
+$lang['safeindex']             = 'Zabrániť indexovaniu meta údajov z neverejných zahrnutých stránok';
+$lang['order']                 = 'Kritéria radenia zahrnutých stránok pri viacerých stránkach';
+$lang['order_o_id']            = 'ID stránky';
+$lang['order_o_title']         = 'Názov';
+$lang['order_o_created']       = 'dátum vytvorenia';
+$lang['order_o_modified']      = 'dátum úpravy';
+$lang['order_o_indexmenu']     = 'vlastné poradie pomocou syntaxe indexmenu';
+$lang['order_o_custom']        = 'vlastné poradie pomocou syntaxe include';
+$lang['rsort']                 = 'Prevrátiť poradie zahrnutých stránok';
+$lang['depth']                 = 'Maximálna hĺbka zahrnutých menných priestorov (namespaces), 0 pre neobmedzenú hĺbku';
+$lang['readmore']              = 'Zobraziť odkaz \'Čítať viac\' iba v prípade prvej sekcie';
