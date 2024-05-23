b43721f4c1191453e45d553a0f21be3c5be8e6c8
Georgetta <gilda_dupre@alice.it>
2024-05-23 17:30:37 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Georgetta <gilda_dupre@alice.it>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Good post. I will be facing some of these issues as well..';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,38 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Georgetta <gilda_dupre@alice.it>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just wanted to say Hi.';
+$lang['firstseconly']          = 'Good post. I will be facing some of these issues as well..';
+$lang['showtaglogos']          = '19';
+$lang['showfooter']            = 'The writer is called Mei Ream but it is not the most feminine name out presently there. To go to karaoke are some things that I\'m totally enslaved by. Some time ago he chose to live in Kansas and then he will never move. Accounting is things i do for a living but I\'ve always wanted my own business. I\'ve been working on my website for some time now. Investigate for yourself here: http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['showlink']              = 'http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['showpermalink']         = 'Good post. I will be facing some of these issues as well..';
+$lang['showdate']              = 'Good post. I will be facing some of these issues as well..';
+$lang['showmdate']             = 'Good post. I will be facing some of these issues as well..';
+$lang['showuser']              = 'Good post. I will be facing some of these issues as well..';
+$lang['showcomments']          = 'Good post. I will be facing some of these issues as well..';
+$lang['showlinkbacks']         = 'Good post. I will be facing some off these issues aas well..';
+$lang['showtags']              = '14';
+$lang['showeditbtn']           = 'Good post. I will bbe facing some of these issues as well..';
+$lang['doredirect']            = 'Good post. I will be facing some of these issues as well..';
+$lang['doindent']              = 'Good post. I will be facing some of these issues as well..';
+$lang['linkonly']              = 'http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['title']                 = 'Just wanted to say Hi.';
+$lang['pageexists']            = 'http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['parlink']               = 'http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['safeindex']             = 'Gilda';
+$lang['order']                 = 'Good post. I will be facing skme of thes isxsues as well..';
+$lang['order_o_id']            = 'Good post. I will be facing some of these issues as well..';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Good post. I will be facing some of these issues as well..';
+$lang['order_o_modified']      = 'Good post. I will be facing some of these issues as well..';
+$lang['order_o_indexmenu']     = 'Good post. I will be facing some of thesde issues as well..';
+$lang['order_o_custom']        = 'Good post. I will be facing some of these issues as well..';
+$lang['rsort']                 = 'Godenstorf';
+$lang['depth']                 = 'Gilda';
+$lang['readmore']              = 'http://www.taiwanaudio.com/__media__/js/netsoltrademark.php?d=sites.google.com%2Fview%2Fdiyarbakirofisescort%2Fhome';
+$lang['debugoutput']           = 'Good post. I will be facing some of these issues as well..';
