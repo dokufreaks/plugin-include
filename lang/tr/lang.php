bbcf6b7db0f81b893c5a193192f9e064a1ad6b3b
Diyarbakir Escort Hizmeti <klaudiamendis@web.de>
2024-06-10 13:05:13 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir Escort Hizmeti <klaudiamendis@web.de>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Do you have a spam problem on this blog; I also am a blogger, and I was curious about your situation; many 
+of us have devloped some nice prcedures and we are looking to swap 
+methods with others, please shoot me an e-mail if interested.';
