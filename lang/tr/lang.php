e7b53132d592de841188f33ef38aba0564176e9e
diyarbakir'\''da pop�Skortlar <colinbrauer@ymail.com>
2024-06-12 22:50:39 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakir'da pop�Skortlar <colinbrauer@ymail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Thank you for the good writeup. It in fact was a amusement account it.
+Look advancedd to more adeded agreeable from you!
+
+However, how can we communicate?';
