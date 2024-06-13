6e665ff549054df9d7ce78e5760984e67b614821
Diyarbakir'\''da escort hizmeti <jerethalberg@terra.com.br>
2024-06-13 07:15:37 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir'da escort hizmeti <jerethalberg@terra.com.br>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to set up your own blog? I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate 
+it';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,120 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir'da escort hizmeti <jerethalberg@terra.com.br>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to sset up your own blog?
+
+I\'m not very techincal but I can figure things out pretty 
+quick. I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate 
+it';
+$lang['showtaglogos']          = '27';
+$lang['showfooter']            = 'Kristina precisely what my husband loves to call me and I feel comfortable recognize use complete name. Guam is where his house and his parents live nearby. What me and my family love is playing chess but I\'ve not made money with the item. After being out of his project for years he became a medical laborer. If you want to read more check out my website: https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['showlink']              = 'https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['showpermalink']         = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to set up your own blog?
+
+I\'m not very techincal but I can figure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['showdate']              = 'Howdy! This is kind of off topic but I need some guidance from aan established blog.
+Is it very difficult to set up your own blog?
+
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinkiing about making mmy own but I\'m not sure where tto start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['showmdate']             = 'Howdy! Thiss is kind of off topic but I need some guidance from ann established blog.
+
+Is it very difficult to set up your own blog? I\'mnot very techincal 
+but I can figure things out pretty quick. I\'m thinking about making my own but I\'m not sjre where 
+to start. Do you hae any ideas or suggestions?  Appreciate it';
+$lang['showuser']              = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is itt very difficult to set up your oown blog?
+
+I\'m not very techincal but I can figure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['showcomments']          = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to set up your own blog? I\'m not very techincal but I can figure things out 
+pretty quick. I\'m thinking about making my own but I\'m not sure where to start.Do you 
+have any ideas or suggestions?  Appreciate it';
+$lang['showlinkbacks']         = 'Howdy! This is kind of off topic but I need some guidance from an establishewd blog.
+Is it very difficult to set up your own blog?
+I\'m not very techincal but I can figure things outt pretty quick.
+
+I\'m thinking about making my own but I\'m not sure whhere to start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['showtags']              = '19';
+$lang['showeditbtn']           = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to set up your own blog?
+I\'m not very techincal but I can figure things 
+out pretty quick. I\'m thinking about making my own but I\'m not sure 
+where to start. Do you have aany ideas or suggestions?  Appreciiate it';
+$lang['doredirect']            = 'Howdy! This is kind of off topic butt I need some guidance from an established blog.
+Is it veryy difficult to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own bbut I\'m nnot sure 
+where to start. Do you have any ideas or suggestions?
+Appreciate it';
+$lang['doindent']              = 'Howdy! This is kind of off topic but Ineed some guidance 
+from an established blog. Is it very difficult to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m nnot sure where to start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['linkonly']              = 'https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['parlink']               = 'https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['safeindex']             = 'Jere';
+$lang['order']                 = 'Howdy! This is kind of offf topic but I need some guidance from aan established blog.
+Is it very difficult to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any idas or suggestions?  Appreciate it';
+$lang['order_o_id']            = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+
+Is iit very difficult to set up your own blog? I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate it';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Howdy! This is kind of off topic but I need some gyidance from 
+an established blog. Is it very difficult to set 
+up your own blog? I\'m not very techincal but I ccan fogure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure where to 
+start. Do you have any ideas or suggestions?
+Appreciate it';
+$lang['order_o_modified']      = 'Howdy! This is kind oof off topic but I need some guidance 
+from an established blog. Is it very difficult to set up your oown blog?
+
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  Appreciate it';
+$lang['order_o_indexmenu']     = 'Howdy! This is kind of off topic but I need soe guidance 
+from an established blog. Is it very difficult to set uup your own blog?
+I\'m nott very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where 
+to start. Do you have anny ideas or suggestions?
+Appreciate it';
+$lang['order_o_custom']        = 'Howdy! This is kind of off topic but I need some guidance 
+from an established blog. Is it very difficult to set up ylur 
+own blog? I\'m not vewry techincal but I can figure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not 
+sure where to start. Do you have any ideas or suggestions?
+Appreciate it';
+$lang['rsort']                 = 'Ohrenbach';
+$lang['depth']                 = 'Jere';
+$lang['readmore']              = 'https://Gatherbookmarks.com/story17902257/wedding-guest-expectations-for-reception-seating';
+$lang['debugoutput']           = 'Howdy! This is kind of off topic but I need some guidance from an established blog.
+Is it very difficult to set up your own blog? I\'m not very techincal but I can figure things out pretty quick.
+
+I\'m thinking aboutt makiung my own but I\'m not sure 
+where to start. Do you have any ideas or suggestions?  Appreciate it';
