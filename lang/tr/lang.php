8e9a2d2775b25da16557ef0801a03326092d178d
Diyarbakirescort <teena_trouton@yahoo.com>
2024-07-09 14:35:41 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakirescort <teena_trouton@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo 
+matter but usually people do not speak about 
+these subjects. To the next! Many thanks!!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,86 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakirescort <teena_trouton@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I finally signed up';
+$lang['firstseconly']          = 'An intriguing discussion iis definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo 
+matter but usually people do not speak about these subjects.To the next!
+Many thanks!!';
+$lang['showtaglogos']          = '10';
+$lang['showfooter']            = 'Britt is what\'s written on my birth certificate but it is not the most masculine business name. Guam is the only place I\'ve been residing with. What her in addition to her love is dominoes but is actually struggling to search out time for doing it. His job is a credit authoriser. See what\'s new on my small website here: https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['showlink']              = 'https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['showpermalink']         = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may not 
+be a taboo matter but usuwlly people do not speak about these subjects.
+To the next! Many thanks!!';
+$lang['showdate']              = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue,
+it may not be a taboo matter but usually people do not speak about these subjects.
+To the next! Many thanks!!';
+$lang['showmdate']             = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue,
+it may not be a taboo matter but usually people ddo not speak about these subjects.
+To the next! Manyy thanks!!';
+$lang['showuser']              = 'An intriguing discussion iis definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo matter but 
+usually people do not speak about these subjects. To the next!
+Many thanks!!';
+$lang['showcomments']          = 'An intriguing discussion is definitely worth comment. I believe that you 
+ought to write more about this issue, it may noot be a taboo matter but usually people do not 
+speak about these subjects. To the next! Many thanks!!';
+$lang['showlinkbacks']         = 'An intriguing discussion is definitely worth comment. I believe that you ought to write more about this issue,
+it may not be a taboo matter but usually people do not speak about these subjects.
+
+To the next! Many thanks!!';
+$lang['showtags']              = '19';
+$lang['showeditbtn']           = 'An intriguing discussion is definitely worth comment. I believe that you ought to write more about this 
+issue, it may not be a taboo matter but usually 
+people do not speak about these subjects. To the next!
+
+Many thanks!!';
+$lang['doredirect']            = 'An iintriguing discussion is definitely worth comment.
+
+I believe that you ought to write more about this issue, it may not be a taboo matter 
+but usually people do not speak about these subjects.
+To the next! Many thanks!!';
+$lang['doindent']              = 'An intriguing discussion is definitely worth comment. I bwlieve that you 
+ought to write more about this issue, iit may not be a taboo matter but 
+usually people do not speak about these subjects. To the next!
+Many thanks!!';
+$lang['linkonly']              = 'https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['title']                 = 'Im happy I finally signed up';
+$lang['pageexists']            = 'https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['parlink']               = 'https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['safeindex']             = 'Teena';
+$lang['order']                 = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo matter but usually people do 
+not speak about thee subjects. To the next! Many thanks!!';
+$lang['order_o_id']            = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may 
+not be a taboo matter but usually people do not speak about these 
+subjects. To the next! Many thanks!!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'An intriguing discussion is definitely worth comment. I believe that you ought to write more about this issue, itt may not 
+be a taboo matter but usually people do not speak about these subjects.
+To the next! Many thanks!!';
+$lang['order_o_modified']      = 'An intriguing discussion is definitely worth comment. I believe that you ought to write more about this issue, it may not be a taboo 
+matter but usually people do not speak about these subjects.
+To tthe next! Many thanks!!';
+$lang['order_o_indexmenu']     = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo matter but usually people do 
+not speak about these subjects. To the next! Many thanks!!';
+$lang['order_o_custom']        = 'An intriguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue,
+it may not be a taboo matter but usually people do nott speak 
+about thewse subjects. To the next! Many thanks!!';
+$lang['rsort']                 = 'Hazelwood';
+$lang['depth']                 = 'Teena';
+$lang['readmore']              = 'https://Mediajx.com/story19294570/diyarbakir-eskortml';
+$lang['debugoutput']           = 'An intrkguing discussion is definitely worth comment.
+I believe that you ought to write more about this issue, it may not be a taboo 
+matter but usually people do not spak about these subjects.
+To the next! Manny thanks!!';
