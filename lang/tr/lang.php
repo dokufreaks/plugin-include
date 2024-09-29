5422be711d94eb0663a6987ea438b2bf935bbe02
dental clinic deals turkey <karma_halse@gmail.com>
2024-09-29 21:30:05 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author dental clinic deals turkey <karma_halse@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I\'m impressed, I must say. Rarely do I come across a 
+blog that\'s equally educatuve annd entertaining, and let me tell you, yyou have hit the naill on the head.
+The issue is something not enough people are speaking intelligently about.
+
+I am very happy that I found tis during my search for something conerning this.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,103 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author dental clinic deals turkey <karma_halse@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'very happy I now registered';
+$lang['firstseconly']          = 'I\'m impressed, I mujst say. Rarely do I come across a blog that\'s equally educative and entertaining, and leet me telol you, you have hhit the nail on the head.
+The issue is something not enough people are speaking intelligently about.
+I amm very happy that I found tis during mmy search for something concerning this.';
+$lang['showtaglogos']          = '23';
+$lang['showfooter']            = 'I\'m impressed, I must say. Rarely do I come across a 
+blog that\'s equally educative and entertaining, and let me tell you,
+yyou have hit the nail oon the head. The issue is something not enough people aree speaking intelligently about.
+I am very hapoy that I found this during my search 
+for something concrning this.';
+$lang['showlink']              = 'https://overlandx.com/index.php?page=user&action=pub_profile&id=139422';
+$lang['showpermalink']         = 'I\'m impressed, I must say. Rarely doo I come across a blog that\'s equally educative and entertaining, 
+and let me tell you, you have hit the nail on the head.
+The issue iss something not enogh people are speakiong intelligently about.
+
+I am very happy that I found this during my search 
+for someting concerning this.';
+$lang['showdate']              = 'I\'m impressed, I must say. Rarely do I come across a blog that\'s equally 
+educative and entertaining, and let me tell you, you have hit the 
+nail on the head. The isse iss something not enough people are 
+speaking intelligently about. I am very happy that I found this during my search for 
+something concerning this.';
+$lang['showmdate']             = 'I\'m impressed, I must say. Rarely do I come across a blog 
+that\'s equally educative and entertaining, and lett me tell you, you 
+have hit the nail on the head. The issuue is something not enough pwople are speaking intelligently about.
+
+I am very happy that I found this durinng my search for something concerning this.';
+$lang['showuser']              = 'I\'m impressed, I must say. Rarely do I come across a blog 
+that\'s equally educative andd entertaining, and let mee ttell you, you have hit the nail on the head.
+
+The issue is something not enough people are speaking intelligently about.
+I am very happy that I found this during my search for somethiing concerning this.';
+$lang['showcomments']          = 'I\'m impressed, I must say. Rarelyy do I come across a blog that\'s equally educative and entertaining, and let me tell you, you have hit the nail on the head.
+The issue is something not enough people are speaking 
+intelligsntly about. I am very happy that I found this during 
+my search for something concerning this.';
+$lang['showlinkbacks']         = 'I\'m impressed, I must say. Rarely do I come across a bog that\'s equally educative and entertaining, and let 
+me tll you, you have hit the nail on the head.
+
+The issu is something not enough people are speaking intelligently 
+about. I am very happy that I found this during my search for something 
+conncerning this.';
+$lang['showtags']              = '27';
+$lang['showeditbtn']           = 'I\'m impressed, I must say. Rarely do I come across a log that\'s equually esucative and entertaining, and let me tell 
+you, you have hit the nail on the head. The issue iss something 
+not enough people are speaking intelligently about.
+I am very happy that I fokund this during my search for something concerning this.';
+$lang['doredirect']            = 'I\'m impressed, I must say. Rarely do I come acrkss a blog that\'s equally educative and entertaining, and let me tell you, 
+you hace hit tthe nail on the head. The issue is something not 
+enough people are speaking intelligently about.
+I am very happy that I found this during my search for 
+something concerning this.';
+$lang['doindent']              = 'I\'m impressed, I must say. Rarely ddo I come across a blog that\'s equaally educative and entertaining, and let me tell you, youu have hhit the nail on the head.
+Thee isssue is something not enoough people arre 
+speaking intelligently about. I am very happy thaat I found this during 
+my search for something concerning this.';
+$lang['linkonly']              = 'https://overlandx.com/index.php?page=user&action=pub_profile&id=139422';
+$lang['title']                 = 'very happy I now registered';
+$lang['pageexists']            = 'https://overlandx.com/index.php?page=user&action=pub_profile&id=139422';
+$lang['parlink']               = 'https://overlandx.com/index.php?page=user&action=pub_profile&id=139422';
+$lang['safeindex']             = 'Karma';
+$lang['order']                 = 'I\'m impressed, I must say. Rarely do I come across a blog that\'s equally educative 
+and entertaining, and let mee tell you, you have hit the nail on the head.
+The iszsue is something not enough peoplle aree speaking intelligently about.
+I am very happy that I foumd this during my search for something concerning this.';
+$lang['order_o_id']            = 'I\'mimpressed, I must say. Rarely do I come across a blog that\'s equally educative and entertaining, and let me tell you, youu have hit the nail on thee head.
+
+The issue is something not enough people are speaking 
+intelligently about. I am very happy that I found this during my search for something 
+concerning this.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I\'m impressed, I must say. Rarely do I come across a blog that\'s 
+equyally educative and entertaining, annd let me tell you, you have hit the nnail on the head.
+The issuhe is something not enough people are speaking intelligently about.
+I am very happy that I found this during mmy search for something concerning this.';
+$lang['order_o_modified']      = 'I\'m impressed, I must say. Rarely do I come across a 
+blog that\'s equally educative and entertaining,and let me tell you, you 
+hzve hitt the nail on the head. The issue is something not enough people are speaking intelligently about.
+
+I am very happy that I found this during my search for something concerning this.';
+$lang['order_o_indexmenu']     = 'I\'m impressed, I must say. Rarely do I come across a blog that\'s equally educative and entertaining, and let 
+me tell you, you have hit the naail on the head. The issue is somethimg not enough 
+people are speaking intelligently about. I am very happy tnat I found this during my search for somethging 
+concerning this.';
+$lang['order_o_custom']        = 'I\'m impressed, I must say. Rarely ddo I come across a blog 
+that\'s equally educastive and entertaining, and leet me tell you, you have hit the nail on the head.
+The isse is something not enough people are soeaking intelligently about.
+
+I am very happy that I found this during my search for something concerning this.';
+$lang['rsort']                 = 'Nussbaum';
+$lang['depth']                 = 'Karma';
+$lang['readmore']              = 'https://overlandx.com/index.php?page=user&action=pub_profile&id=139422';
+$lang['debugoutput']           = 'I\'m impressed, I must say. Rarely doo I come across 
+a blog that\'s equally educative and entertaining, and lett me tell 
+you, you have hit the nail on the head. The issue is something not enough people are speaking intelligently about.
+I am very happy that I found this during my search for something concerning this.';
