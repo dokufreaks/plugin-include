dcded4a0030d7cfe4570f6bd6018a74990ae5243
xây nhà trọn gói Bình Minh <ron_cayton@yahoo.com>
2024-11-18 23:15:45 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,16 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xây nhà trọn gói Bình Minh <ron_cayton@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could 
+add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and 
+was hoping maybe you would have some experience with something like 
+this. Please let me know if you run into anything. I truly enjoy reading your blog and I look forward to your new updates.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,155 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xây nhà trọn gói Bình Minh <ron_cayton@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new guy';
+$lang['firstseconly']          = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any 
+widgets I could add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe 
+you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy 
+reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showtaglogos']          = '17';
+$lang['showfooter']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+I\'m Ron and I live in San Leonardello. 
+I\'m interested in American Studies, Air sports and Turkish art. I like travelling and watching American Dad.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showlink']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showpermalink']         = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically tweet my newest 
+twitter updates. I\'ve been looking for a plug-in like 
+this for quite some time and was hoping maybe 
+you would have some experience with something like 
+this. Please let me know if you run into anything. I truly 
+enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showdate']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to 
+my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite 
+some time and was hoping maybe you would have some experience with something like 
+this. Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your 
+new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showmdate']             = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could 
+add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite 
+some time and was hoping maybe you would have some experience with something like this.
+
+Please let me know if you run into anything. I truly enjoy reading your blog and I look forward to 
+your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showuser']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any 
+widgets I could add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with something like this.
+
+Please let me know if you run into anything.
+
+I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showcomments']          = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any 
+widgets I could add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was 
+hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy 
+reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showlinkbacks']         = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my 
+blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some 
+experience with something like this. Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward 
+to your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showtags']              = '21';
+$lang['showeditbtn']           = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of 
+any widgets I could add to my blog that automatically tweet 
+my newest twitter updates. I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['doredirect']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog 
+that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was 
+hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['doindent']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you 
+knew of any widgets I could add to my blog that 
+automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with 
+something like this. Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to 
+your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['linkonly']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['title']                 = 'I am the new guy';
+$lang['pageexists']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['parlink']               = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['safeindex']             = 'Ron';
+$lang['order']                 = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering 
+if you knew of any widgets I could add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was 
+hoping maybe you would have some experience with something like 
+this. Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_id']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I 
+could add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was 
+hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically tweet my newest 
+twitter updates. I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have 
+some experience with something like this. Please let me 
+know if you run into anything. I truly enjoy reading your blog and I look forward to your new updates.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_modified']      = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time 
+and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new 
+updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_indexmenu']     = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could 
+add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time 
+and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and 
+I look forward to your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_custom']        = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading 
+your blog and I look forward to your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['rsort']                 = 'San Leonardello';
+$lang['depth']                 = 'Ron';
+$lang['readmore']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['debugoutput']           = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hey I know this is off topic but I was wondering if you knew of any widgets I could 
+add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time 
+and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and 
+I look forward to your new updates. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-binh-minh-vinh-long-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
