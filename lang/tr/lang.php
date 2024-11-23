2bc1b87bdc770192d7c8bff638f180b0a1cd2ad8
Diyarbakir Eskort <jamaalgoninan@gmail.com>
2024-11-23 11:40:45 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir Eskort <jamaalgoninan@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Excellent post. I used to be checking constantly this bloog and I\'m impressed!
+Very helpful information particularly the ultimmate phase :) I take care of such information a lot.
+I used to be looking for this certain info for a very long time.
+Thanks annd best of luck.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,97 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir Eskort <jamaalgoninan@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im glad I finally registered';
+$lang['firstseconly']          = 'Excellent post. I used to bbe checking constantly this blog and 
+I\'m impressed! Very helpful information particularly the 
+ultimate phase :) I take care of such information a lot.
+I used to be looking for this certain info for a very long time.
+Thanks and best of luck.';
+$lang['showtaglogos']          = '14';
+$lang['showfooter']            = 'The that wrote write-up is called Britt fantastic wife doesn\'t like it at each of. I used to be unemployed but now i\'m an information officer and I\'ll be promoted rapidly. Delaware is where his house is. Body building is something he really enjoys doing. Check out her website here: http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['showlink']              = 'http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['showpermalink']         = 'Excellent post. I used to be checking constantly this blog and I\'m impressed!
+Very helpful informmation particularly the ultimate phase :) I take care of 
+such information a lot. I used to be looking for this certain info for a very long time.
+Thanks and best of luck.';
+$lang['showdate']              = 'Excellent post. I used to be checking constantly this blog 
+and I\'m impressed! Very helpful information particularly the ultimate 
+phase :) I take care of such information a lot.
+I used to be looking for this certain info for a 
+very long time. Thanks and best of luck.';
+$lang['showmdate']             = 'Excellebt post. I used to be checking constantly this blog 
+and I\'m impressed! Very helpful information particularly the ultimate phase :) I take care of 
+such information a lot. Iused to be looking for this 
+certain info for a very long time. Thanks and best of luck.';
+$lang['showuser']              = 'Excellent post. I used to be checking constantly this blog 
+and I\'m impressed! Very helpful information particularly the ultimate phase :
+) I take care of such information a lot. I used tto be looking for this certain info for a very long 
+time. Thanks and best of luck.';
+$lang['showcomments']          = 'Excellent post. I used to be checking constantly this blog and 
+I\'m impressed! Very helpful information particularly the ultimate phase :
+) I take care of such information a lot. I used 
+to be looking for this certain info for a very long time.
+
+Thsnks and best of luck.';
+$lang['showlinkbacks']         = 'Excellent post. I used to be checking constantly this blog and I\'m impressed!
+Very helpful information particularly the ultimate phase :) I take care 
+of such information a lot. I used to be looking for this certain info 
+for a very lohg time. Thanks and best of luck.';
+$lang['showtags']              = '17';
+$lang['showeditbtn']           = 'Excellent post. I used to be checking constantly this blog 
+and I\'m impressed! Very helpful information particularly the ultimate 
+phase :) I take care of such information a lot. I used to be 
+looking for this certain info for a very long time. Thanks and best of luck.';
+$lang['doredirect']            = 'Excellent post. I used to be checking constantly this blog and I\'m 
+impressed! Very helpful information particularly the ultimate phase :
+) I take care of such information a lot. I used 
+to be looking for this certain info for a very long time. Thanks and best of luck.';
+$lang['doindent']              = 'Excellent post. I ussd to be checking constantly this blog and I\'m impressed!
+Very helpful information particularly the ultimate phase :) I take care off 
+such information a lot. I uded to be looking for this certain info for 
+a very long time. Thanks and best of luck.';
+$lang['linkonly']              = 'http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['title']                 = 'Im glad I finally registered';
+$lang['pageexists']            = 'http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['parlink']               = 'http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['safeindex']             = 'Jamaal';
+$lang['order']                 = 'Excellent post. I used to be checking constantly this blog and I\'m 
+impressed! Very helpful information particularly the ultimate phase :) I take 
+care of such information a lot. I ued to be looking for this certain info for a very long time.
+Thaznks and best oof luck.';
+$lang['order_o_id']            = 'Excellent post. I used tto be checking constantly this blog and I\'m impressed!
+Very helpful information particularly the 
+ultimate phase :)I take care of such information a 
+lot. I used too be looking for this certain info for a very 
+long time. Thanks and best of luck.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Excellent post. I used to be checking constantly this blog and I\'m impressed!
+Very helpful information particularly the ultimate phase :) I take caee of such information a lot.
+I used to be looking ffor thiss certain info for a very long time.
+Thanks and best of luck.';
+$lang['order_o_modified']      = 'Excellent post. I used to be checking constantly this blog and I\'m impressed!
+
+Very helpful information particularly the ultimate phase 
+:) I take care of such information a lot.
+I used to be looking for this certain info for a very long time.
+Thanks and best of luck.';
+$lang['order_o_indexmenu']     = 'Excellent post. I used to be checking constantly thius blog and I\'m impressed!
+Very helpful information particularly the ultimate phase :
+) I take care of such information a lot. I used to be looking for 
+this certain info for a very long time.Thanks and best 
+of luck.';
+$lang['order_o_custom']        = 'Excellent post. I used to be checking constantly this blog and I\'m impressed!
+Very helpful inforjation particularly the ultimate phase :) I take care of such information a lot.
+I usaed to be lookihg for this certain info 
+for a very long time. Thanks and best of luck.';
+$lang['rsort']                 = 'Deventer';
+$lang['depth']                 = 'Jamaal';
+$lang['readmore']              = 'http://www.Webuygoldanddiamonds.com/__media__/js/netsoltrademark.php?d=Sites.Google.com%2Fview%2Fbenzersizseksgzelliiolandiyarb%2Fana-sayfa';
+$lang['debugoutput']           = 'Excellent post. I used to be checking constantly this blog annd I\'m impressed!
+Very helpful information particularly the ultimate 
+phase :) I take care of such information a lot. I used to be looking for this certain info for a 
+very long time. Thanks and best of luck.';
