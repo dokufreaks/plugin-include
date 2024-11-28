82ff4e9c91da0736c84fc08ed23135a6d9cac007
Escort Diyarbakir <teshalytle@live.nl>
2024-11-28 14:40:45 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Escort Diyarbakir <teshalytle@live.nl>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I lovge your blog.. very nice colors & theme. Did you create this website yourself or did 
+you hire someone to do it for you? Plz answer back as I\'m looking to create 
+mmy own blog and would like to find out where 
+u got this from. thanks a lot';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,86 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Escort Diyarbakir <teshalytle@live.nl>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'I love your blog.. very nice colors & theme. Did you crdeate ths website yourself or did 
+you hire someone to do it for you? Plz answer back as I\'m looking 
+to create my own blog and would like to find out 
+where u got this from. thanks a lot';
+$lang['showtaglogos']          = '11';
+$lang['showfooter']            = 'Hello! Allow me to start by saying my name - Kristina. Arkansas is as it\'s a lucrative place she\'s been residing in but now she is considering card readers and wifi. Filing is my profession. What me and my family love is body building but I have been taking on new things lately. She is running and a blog here: http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['showlink']              = 'http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['showpermalink']         = 'I love your blog.. very nice colors & theme. Did you 
+create this website yourself or did you hire someone to 
+do it for you? Plz answer back as I\'m looking to create my own blog aand would like 
+to find out where u got this from. thaanks a lot';
+$lang['showdate']              = 'I love your blog.. very nice colors & theme. Did you create this website yourself or 
+did you hire someone tto do it for you? Plz answer back as I\'m looking too create my own blog and wwould like 
+to finnd out where u got this from. thanks a lot';
+$lang['showmdate']             = 'I love your blog.. very nice colors & theme. Diid 
+you create this website yourself or did youu hire someone to do it 
+for you? Pllz answer back as I\'m looking to create my oown blog and would 
+like to find out where u got this from. thanks a 
+lot';
+$lang['showuser']              = 'I love your blog.. very nice colors & theme.
+Did you crezte this website yourself or did you hire someone to doo it for you?
+Plz answer back as I\'m looking to create my own blog andd would like 
+to find out where u got this from. thanks a lot';
+$lang['showcomments']          = 'I love your blog.. very nice colors & theme. Did you create this website yourself or did you hire someone to do 
+it for you? Plz amswer back as I\'m looking to create my own blpog annd would like to find out where u got this from.
+thanks a lot';
+$lang['showlinkbacks']         = 'I love your blog.. very nice colors & theme. Did you create this website yourself or did you hire someone to doo it for 
+you? Plz answer back as I\'m looking to create my own blog and wold like to find out where u got 
+this from. thanks a lot';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'I love your blog.. very nice colors & theme. Did you create this website yourself or did you hire someone to do iit for you?
+Plz answer back as I\'m looking to creawte my owwn blog and would like to find out where u got this from.
+thanks a lot';
+$lang['doredirect']            = 'I love your blog.. very nice colrs & theme. Did 
+you create this website yourself or did you hire someone to do 
+it for you? Plz answer back as I\'m looking to create 
+mmy own blog and would like to find out where u got this from.
+thanks a lot';
+$lang['doindent']              = 'I love your blog.. very nice colors & theme. 
+Did you create this website yourself or did yoou hire someone to do it for you?
+Plz answer back as I\'m looking to create my own blog and would like to find out 
+where u got this from. thanks a lot';
+$lang['linkonly']              = 'http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['parlink']               = 'http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['safeindex']             = 'Tesha';
+$lang['order']                 = 'I love your blog.. very nice colors & theme.
+Did you create this websute yourself or ddid you hire someone 
+to do it forr you? Plz answer back as I\'m loooking to creazte 
+my own blog and woulld like to find out where u got this from.
+thanks a lot';
+$lang['order_o_id']            = 'I love your blog.. veery nice colors & theme. Did you create this website yourself or did you hir someone 
+to do it for you? Plz anwer bacxk aas I\'m looking to create my own blog and would like to find out where u got this 
+from. thanks a lot';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I love your blog.. very nice colors & theme.
+Did you create this website yourself orr did you hire someone to ddo it 
+for you? Plz answer back as I\'m looking to create my own blog and would like to find out where 
+u ggot this from. thanks a lot';
+$lang['order_o_modified']      = 'I love your blog.. very nice colors & theme. Did 
+you create this website yourself or did youu hire someone to do 
+it forr you? Plz answer back as I\'m looking to create my own blog annd would like to find 
+outt where u got this from. thanks a lot';
+$lang['order_o_indexmenu']     = 'I love your blog.. very nice coklors & theme. Did you create this website yourself or did you hire 
+someone too do it for you? Plz answer back as I\'m looking 
+to create my own blog and would like to find out where u got this from.
+thanks a lot';
+$lang['order_o_custom']        = 'I love your blog..very nice colors & theme. Did you create this website 
+yoursellf or did you hire someoe to do it for you?
+Plz answer back as I\'m looking tto create my own blog and would like to find out where u got this from.
+thanks a lot';
+$lang['rsort']                 = 'Poughkeepsie';
+$lang['depth']                 = 'Tesha';
+$lang['readmore']              = 'http://Dk-books.com/bitrix/click.php?goto=https://sites.google.com/view/memnunedicilikteharikaolandiya/ana-sayfa';
+$lang['debugoutput']           = 'I love your blog.. veery nice colors & theme. Did you create this website yourself or did you hire someone to do it 
+for you? Plz answer back as I\'m looking to create my own blog and would like to find out where u got this from.
+thanks a lot';
