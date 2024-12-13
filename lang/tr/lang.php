e1c8c926fb115fe81f7f98334be1df40e68dd9b9
Oscar <ariannesong@gmail.com>
2024-12-13 19:40:05 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Oscar <ariannesong@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Hi there, I do believe your website may be having browser compatibility problems.
+
+Whenever I look att your site in Safari, it 
+looks fine but when opening in IE, itt has some overlapping issues.
+I merely wanted to provide you with a quick heads up!
+Other than that, edcellent website!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,98 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Oscar <ariannesong@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'Hi there, I do believe your website may be having browser compatibility problems.
+Whenver I look at your site in Safari, it looks finbe but when opening in IE,it has some overlapping issues.
+
+I merely wanted to provide you with a quick heads up! Other than that, excellent website!';
+$lang['showtaglogos']          = '12';
+$lang['showfooter']            = 'Greetings. Allow me to start by telling the author\'s name - Kristina Ream but people always misspell in which. To research fashion is a behavior which she\'s been doing walkman from sony. Kansas is the place he loves most. Filing is my profession and i am doing very good financially. See what\'s new on my small website here: http://www.Plazoo.com/';
+$lang['showlink']              = 'http://www.Plazoo.com/';
+$lang['showpermalink']         = 'Hi there, I do believe your website may be having browser compatibility problems.
+Whhenever I look at your site in Safari, it looks fine but when opening in IE, it has some overlapping 
+issues. I merely wanted too provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['showdate']              = 'Hi there, I do believe your website may be having 
+browser compatibility problems. Whenever I look at your site in Safari, it looks 
+fine but when opening in IE, it has some overlapping issues.
+I merely wanted to provide you with a quick heads up! Other than that, excellent website!';
+$lang['showmdate']             = 'Hi there, I do believe your website may be having browser compatibility problems.
+
+Whenever I look at yor site in Safari, it looks fine but when opening in IE,
+it has some overlapping issues. I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['showuser']              = 'Hi there, I do believe your website may be having browser compatibility problems.
+Whenever I look at your site in Safari, it looks fine but when opening 
+in IE, it has some overlapping issues. I merely wanted to provide yoou with a quick heads up!
+Other than that, excellent website!';
+$lang['showcomments']          = 'Hi there, I do believe your website may be 
+having browser compatibility problems. Whenever I look at your sitre in Safari, 
+it looks fine but when opening in IE, it has some overlapping issues.
+I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['showlinkbacks']         = 'Hi there, I do believe your website may be having browser compatibility problems.
+
+Whenever I look at your site in Safari, it looks fine but when opening in IE, it has some overlapping issues.
+I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['showtags']              = '20';
+$lang['showeditbtn']           = 'Hi there, I do believe your website may be having browser compatibility problems.
+Whenever I look at your site in Safari, it looks fine but when opening in IE, it 
+has some overlapping issues. I merely wanted to provide yoou with 
+a quick heads up! Other than that, excellent website!';
+$lang['doredirect']            = 'Hi there, I do believe your website may be having brokwser compatibility problems.
+Whenever I look at your site in Safari, it looks fine but when opening in IE,
+it has some overlapping issues. I merely wanted to provide you 
+with a quick heads up! Other than that, excellent website!';
+$lang['doindent']              = 'Hi there, I do believe your website may be having browser compatibility 
+problems. Whenever I look at your site in Safari, it looks fine but when opening in IE, it has some overlapping issues.
+I merely wanted to provide you with a quick heads up!
+
+Other than that, excellent website!';
+$lang['linkonly']              = 'http://www.Plazoo.com/';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'http://www.Plazoo.com/';
+$lang['parlink']               = 'http://www.Plazoo.com/';
+$lang['safeindex']             = 'Arianne';
+$lang['order']                 = 'Hi there, I do believe your website may be having 
+browser conpatibility problems. Whenever I look at your site 
+in Safari, it looks fine but when opening in IE, it 
+has some overlapping issues. I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['order_o_id']            = 'Hi there, I do believe your website may be having browser compatibility problems.
+
+Whenever I look at your site in Safari, it looks finee but when opening in IE, 
+it has some overlapping issues. I merely wanted to provide you with 
+a quick heads up! Other than that, excellent website!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Hi there, I do believe your website may be having browser 
+compatibility problems. Whenever I look at your site in Safari, it looks fine but when opening 
+in IE, it has some overlapping issues. I merely wanted to provide you with a quick heads 
+up! Other than that, excellent website!';
+$lang['order_o_modified']      = 'Hi there, I do believe your website may 
+be having browser compatibility problems.Whenever I 
+look at your site in Safari, itt looks fine but when opening in IE, it has some overlapping 
+issues. I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['order_o_indexmenu']     = 'Hi there, I do believe your website may be having browser compatibility problems.
+Whenever I look at your site in Safari, it looks fine but when opwning in IE, it has some overlapping issues.
+I merely wanted too provide you with a quick heads 
+up! Other than that, excellent website!';
+$lang['order_o_custom']        = 'Hi there, I do believe your website may be having browser compatibility 
+problems. Whenever I look at your site in Safari, 
+it looks fine but when opening in IE, it has some overlapping issues.
+
+I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
+$lang['rsort']                 = 'La Celle-Saint-Cloud';
+$lang['depth']                 = 'Arianne';
+$lang['readmore']              = 'http://www.Plazoo.com/';
+$lang['debugoutput']           = 'Hi there, I do believe your website may be having browser compatibility problems.
+Wheever I look at your site in Safari, it looks fine but when opening in IE, it has some overlapping issues.
+I merely wanted to provide you with a quick heads up!
+Other than that, excellent website!';
