e897cee8a6bdbc40f3a5c6f0dca5bed383dabff2
Diyarbakır eskort <kandisshapiro@bigpond.com>
2024-12-22 18:35:06 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakır eskort <kandisshapiro@bigpond.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Thanks for sharing such a good thinking, paragraph is pleasant, thats whhy i hazve 
+read it fully';
