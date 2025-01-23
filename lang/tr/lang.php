944e13868e0b97ab28403ad16b348492736c8e72
russianmarket <alphonsomarkley@hotmail.es>
2025-01-23 19:40:09 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author russianmarket <alphonsomarkley@hotmail.es>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I\'ve learn a few just right stuff here. Definitely price bookmarking for revisiting.
+
+I surprise how so much effort you put to make any such great informative website.';
