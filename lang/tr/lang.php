8d45309bd073634b83bd245a4e90bd85c4f16f8e
DiyarbakıR Escort <shennamann@att.net>
2025-01-26 17:35:46 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR Escort <shennamann@att.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Hey I know this is off topic but I was wondering if you knew of any 
+widgets I could add to my blog that automatically tweet my newest twitter updates.
+
+I\'ve been lookihg for a plug-in like this for 
+quite some time and was hoping maybe you would have some experience wigh something like 
+this. Please let me now if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,117 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR Escort <shennamann@att.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'Hey I know this is off topic but I was wondering if yyou knew of any widgets I could add to my blog that automatically tweet 
+my newest twitter updates. I\'ve been looking forr a plug-in like 
+this for quite some time and was hoping maybe you would have some experience with somethung like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and I look forward to yor new updates.';
+$lang['showtaglogos']          = '16';
+$lang['showfooter']            = 'Hello dear visitor. I\'m Lino. As a woman what she really likes is perform rock and roll but she will not have the time lately. Credit authorising is the I support my as well as the salary has been really completing. Arkansas is generate place she has been residing in but now she is considering alternatives. He is running and maintaining a blog here: https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['showlink']              = 'https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['showpermalink']         = 'Hey I know this is off topic but I was wondering if you knew of any widgets 
+I could add to my bog that automatically tweet my newest 
+twitter updates. I\'ve been looking for a plug-in like this for quite some time and was 
+hoping maybe you would have some experience with something 
+like this. Please let me know iif you run into anything.
+I truly enjoy reading your blog and I lkok forward to your new updates.';
+$lang['showdate']              = 'Hey I know tis is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically 
+tweet my newest twitter updates. I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you 
+would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy 
+reading your blog and I look forward to your new updates.';
+$lang['showmdate']             = 'Hey I know this is off topic but I was wondering if you kneww of any widgets I could add to my blog that automatically tweet my 
+neest twitter updates. I\'ve been looking for a plug-in like this 
+for quite some time and was hoping maybe you would have soke experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog and I 
+look forward to your new updates.';
+$lang['showuser']              = 'Hey I know this is ooff topic but I was wondeting if you knew of 
+any widgets I could add to my blog that automaticallly tweset my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and 
+was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly 
+enjoy reading your blog and I look forward to your new updates.';
+$lang['showcomments']          = 'Hey I know this is off topic but I was wondering if you knew of any widgets I could add to myy blog that automaatically tweet 
+my newest twitter updates. I\'ve beren looking for a plug-in like this for quite some time and 
+was hoping maybe you would have some experience witfh something like this.
+Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.';
+$lang['showlinkbacks']         = 'Hey I know this is off topic but I was wondering if you knew of any widgets 
+I could add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was hoping 
+maybe you would have some experience with something like this.
+
+Please let me know if you run into anything. I truly enjoy reading your 
+blog and I look forward to your new updates.';
+$lang['showtags']              = '26';
+$lang['showeditbtn']           = 'Hey I know this iis off topic but I was wondering if you knew of any widgets I could 
+add to my blog that automatically twet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.';
+$lang['doredirect']            = 'Hey I know this is off topic bbut I was wondering if you knew of any widgets I could 
+add to my blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite 
+some time andd wwas hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly 
+enjoy rewding your blog and I look forwaed too your 
+new updates.';
+$lang['doindent']              = 'Hey I know this is off topic but I was wondering if you knew of any widgets I could add 
+to my bllog that automatically twret my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have 
+some experience with something like this. Please let me know if 
+you run ibto anything. I truly enjoy reading your blog and I look 
+forward to your new updates.';
+$lang['linkonly']              = 'https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['parlink']               = 'https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['safeindex']             = 'Shenna';
+$lang['order']                 = 'Hey I know thjis is off topic but I was wondering if you knew of any widgets I could 
+add to my blog that utomatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quite some time 
+and was hoping maybe you would have some experience wigh something like this.
+Please let me know if you run ito anything. I truly enjoy reading your bblog and I look forward to 
+your new updates.';
+$lang['order_o_id']            = 'Hey I know this is off topic but I was wondering if you knew of any 
+wjdgets I could add to my blog that automatically tweet my newest twitter 
+updates. I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would have some experience with something like this.
+Please let me know if you run into anything. I truly enjoy reading your blog 
+and I look forward to your new updates.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Hey I know this iss off topic but I was wondering if you knew of any widgets I could add tto my 
+blog that automatically tweet my newest twitter updates.
+I\'ve been looking for a plug-in like this for quiite some 
+tiime and was hoping maybe you would have som experience with something like this.
+Please let mee know if you run ijto anything. I truly enjoy reading your blog aand I look forward 
+to your new updates.';
+$lang['order_o_modified']      = 'Hey I know thhis is off topic but I was wondering if you knew of anyy widgets 
+I could add to my blog that automatically tweet my newest 
+twitter updates. I\'vebeen looking for a plug-in like this for quite some time and was hoping maybe you 
+would have sone experience with something like this.
+
+Please let me know if you run into anything.
+
+I truly enjoy reading your blog and Ilook forward to your new updates.';
+$lang['order_o_indexmenu']     = 'Hey I know this is off topic but I was wondering if you knew of any widgets I could add to my blog that automatically tweet 
+myy newest twitter updates. I\'ve been looking for a plug-in likke this for quite some 
+time and was hoping maybe you would have some experience with something like this.
+Please leet me know if you run into anything.
+I trujly enjoy reading youhr blog and I look forward to your new updates.';
+$lang['order_o_custom']        = 'Hey I know this is off topic but I was wondering if you 
+knew of any widgets I could add tto my blog that automatically tweet 
+my newest twitter updates. I\'ve been looking for a plug-in like this for quite some time and was hoping maybe you would 
+have some experience with something like this. Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.';
+$lang['rsort']                 = 'Omak';
+$lang['depth']                 = 'Shenna';
+$lang['readmore']              = 'https://10.Staikudrik.com/index/d1?diff=0&utm_source=ogdd&utm_campaign=26607&utm_content=&utm_clickid=snqcg0skg8kg8gc0&aurl=http%3A//Penzu.com%2Fp%2F1a780d67c994a219&member%5Bsite%5D=https%3A//ggslotwallet.com/&member%5Bsignature%5D=%3Cspan';
+$lang['debugoutput']           = 'Hey I know this is off toic but I was wondering if you new of any widgets I could add too my blog 
+that automatically tweet my newest twitter updates. I\'ve been looking for a 
+plug-in like this for quite some time and was hoping maybe you would 
+have some experience with something like this.Please let me know if you run into anything.
+I truly enjoy reading your blog and I look forward to your new updates.';
