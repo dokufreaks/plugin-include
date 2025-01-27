831f398047bb01dce9c84b58672ab8243c3b7ac5
escort diyarbakır <collinslocum@yahoo.ca>
2025-01-27 13:50:05 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author escort diyarbakır <collinslocum@yahoo.ca>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Thanks for the auspicious writeup. It if truth be told was once a entertainment account it.
+Look advanced to more brought agreeable from you!
+By the way, how could we keep up a correspondence?';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,74 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author escort diyarbakır <collinslocum@yahoo.ca>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new guy';
+$lang['firstseconly']          = 'Thanks for the auspicious writeup. It if truth be told was once a 
+entertainment account it. Loook advanced to more brought agreeable from you!
+By the way, hhow could we keep up a correspondence?';
+$lang['showtaglogos']          = '20';
+$lang['showfooter']            = 'Mei could be the name people use to call me and I\'m comfortable as well as use the full name. His job is a credit authoriser. His wife doesn\'t like it the way he does but what he really likes doing is kayaking but he do not have the time lately. She\'s always loved living in Delaware luckily she is considering likewise. If you want to gather more information check out my website: http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['showlink']              = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['showpermalink']         = 'Thanks for the auspicious writeup. It if 
+truth be told was once a entertainment account it. Look advanced 
+to more brought agreeable from you! By the way, how could we keep 
+up a correspondence?';
+$lang['showdate']              = 'Thanks for the auspicious writeup. It if truth be told was once a entertainment 
+account it. Look advanced to more brought agreeable from 
+you! By the way, how could we keep upp a correspondence?';
+$lang['showmdate']             = 'Thanks for the auspicious writeup. It if truth be told was once 
+a entertainment account it. Look advanced to more 
+brought agreeable from you! By the way, how could wwe keep up a correspondence?';
+$lang['showuser']              = 'Thanks for the auspicious writeup. It if truth be told 
+was once a entertainment account it. Look advanced 
+to more brought agreeable from you! By the way, how coupd we 
+keep up a correspondence?';
+$lang['showcomments']          = 'Thanks for the auspicious writeup. It iff truth be told was once a entertainment account it.
+
+Look advanced to more brought agreeable from you!
+By the way, how could we keep up a correspondence?';
+$lang['showlinkbacks']         = 'Thanks for the auspicious writeup. It if truth be told was once a entertainment account it.
+Look advanced to more brught agreeable from you! By the way, how could we keep up a correspondence?';
+$lang['showtags']              = '24';
+$lang['showeditbtn']           = 'Thanks for the auspicious writeup. It if truth be told was once a entertainment account it.
+Look advanced to more brought agreeable from you!
+By thhe way, how could we keeep uup a correspondence?';
+$lang['doredirect']            = 'Thanks for the auspicious writeup. It iff truth bee told was once a entertainment 
+account it. Look advanced to more brought agreeable from 
+you! By the way, how could we keep up a correspondence?';
+$lang['doindent']              = 'Thanks for tthe auspicious writeup. It if truth be told was once a 
+entertainment account it. Look advanced to more brought agreeable from you!
+By the way, how could wee keep up a correspondence?';
+$lang['linkonly']              = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['title']                 = 'I am the new guy';
+$lang['pageexists']            = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['parlink']               = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['safeindex']             = 'Collin';
+$lang['order']                 = 'Thanks for the auspicious writeup. It if truth be 
+told was once a entertainment account it. Look advanced to more brought 
+agreeable from you! By the way, how could we keep up a correspondence?';
+$lang['order_o_id']            = 'Thanks for the auspicious writeup. It if truth be told was once a entertainment 
+account it. Look advanced to more brought agreeable from you!
+By the way, how could we keep up a correspondence?';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Thanks for the auspicious writeup. It if truth be told was once 
+a entertainment account it. Look advanced to more brought agreeable from you!
+By the way, how could we keep up a correspondence?';
+$lang['order_o_modified']      = 'Thanks for the auspicious writeup. It if truth be toold was once a entertainment account it.
+Loook advanced to more brought agreeable from you! By tthe way,
+how could we keep up a correspondence?';
+$lang['order_o_indexmenu']     = 'Thanks for the auspicious writeup. It if truth be told 
+was once a entertainment account it. Look advanced to more brought 
+agreeable from you! By the way, how could wee keep up a correspondence?';
+$lang['order_o_custom']        = 'Thanks for the auspicious writeup. It if truth be tolld was once a entertainment account it.
+Loook advanced to molre brolught agreeable from you!
+By the way, how could wee keeep up a correspondence?';
+$lang['rsort']                 = 'Uplees';
+$lang['depth']                 = 'Collin';
+$lang['readmore']              = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7m.nb.v.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://penzu.com/p/407ff570b9141d14';
+$lang['debugoutput']           = 'Thanks for the auspicious writeup. It if truth be tolkd was once 
+a entertainmentt account it. Look advanced to more brought agreeable from you!
+By the way, how could we keep up a correspondence?';
