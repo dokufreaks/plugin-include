f339beb12d04fe4fd2bad6ecd1ad95780d34d0db
Diyarbakır Ofis eskort <henrybettis@yahoo.co.in>
2025-02-16 13:55:45 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakır Ofis eskort <henrybettis@yahoo.co.in>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Saved as a favorite, I really like your website!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,38 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakır Ofis eskort <henrybettis@yahoo.co.in>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I now registered';
+$lang['firstseconly']          = 'Saved as a favorite, I really like your website!';
+$lang['showtaglogos']          = '27';
+$lang['showfooter']            = 'I\'m Jacques but it can be not one of the most masculine word. Delaware is where I\'ve for ages been living and my parents live near. Kayaking is an item she\'s been doing remember. I used to be unemployed but now i am an information officer and I\'ll be promoted instantly. She is running and maintaining a blog here: https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['showlink']              = 'https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['showpermalink']         = 'Saved as a favorite, I really like your website!';
+$lang['showdate']              = 'Saved as a favorite, I really like your website!';
+$lang['showmdate']             = 'Saved as a favorite, I really like your website!';
+$lang['showuser']              = 'Saved as a favorite, I really like your website!';
+$lang['showcomments']          = 'Saved as a favorite, I really like your website!';
+$lang['showlinkbacks']         = 'Saved as a favorite,I really like your website!';
+$lang['showtags']              = '15';
+$lang['showeditbtn']           = 'Saved as a favorite, I really like your website!';
+$lang['doredirect']            = 'Saved as a favorite, I really like your website!';
+$lang['doindent']              = 'Saved as a favorite, I really like your website!';
+$lang['linkonly']              = 'https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['title']                 = 'Im happy I now registered';
+$lang['pageexists']            = 'https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['parlink']               = 'https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['safeindex']             = 'Henry';
+$lang['order']                 = 'Saved as a favorite, I really like your website!';
+$lang['order_o_id']            = 'Saved as a favorite, I really like your website!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Saved as a favorite, I really like your website!';
+$lang['order_o_modified']      = 'Saved as a favorite, I really like your website!';
+$lang['order_o_indexmenu']     = 'Saved as a favorite, I really like your website!';
+$lang['order_o_custom']        = 'Saved as a favorite,I really like your website!';
+$lang['rsort']                 = 'Ajaccio';
+$lang['depth']                 = 'Henry';
+$lang['readmore']              = 'https://mervebaskan.com/question/cilveli-diyarbakir-ofis-escort-arzu-ile-tanisin/';
+$lang['debugoutput']           = 'Saved as a favorite, I really like your website!';
