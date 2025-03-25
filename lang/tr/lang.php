2ed4a3ce218d7beaabfb2fc778eb360daa0f2bd8
xây nhà trọn gói tại Tịnh Biên <sal.lanning@aol.com>
2025-03-25 08:00:33 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xây nhà trọn gói tại Tịnh Biên <sal.lanning@aol.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and a huge section of 
+other people will pass over your wonderful writing 
+because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,117 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xây nhà trọn gói tại Tịnh Biên <sal.lanning@aol.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new guy';
+$lang['firstseconly']          = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+IE nonetheless is the marketplace chief and a huge section of other people will pass over your 
+wonderful writing because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showtaglogos']          = '17';
+$lang['showfooter']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ Interior design is among the most subject of broadcast tv. In the United Kingdom, popular design and decorating programs include 60 Minute Makeover (ITV), Changing Rooms (BBC), and Selling Houses (Channel 4). Famous interior designers whose efforts are featured of these programs include Linda Barker and Laurence Llewelyn-Bowen. In the United States, the TLC Network aired a favorite program called Trading Spaces, a show in accordance with the UK program Changing Rooms. In addition, both HGTV and also the DIY Network also televise many programs about decor and decorating, featuring the works of any variety of interior designers, decorators, and redesigning experts in the myriad of projects.
+
+Fictional interior decorators range from the Sugarbaker sisters on Designing Women and Grace Adler on Will & Grace. There is also another show called Home MADE. There are two teams as well as houses and whoever contains the designed making it the worst room, in line with the judges, is eliminated. Another show about the Style Network, hosted by Niecy Nash, is Clean House where they re-do messy homes into themed rooms the clients would love. Other shows include Design using a Dime, Designed to Sell, and The Decorating Adventures of Ambrose Price. The show called Design Star happens to be more popular with the 5 seasons which may have already aired. The winners on this show end up having their own TV shows, that are Color Splash hosted by David Bromstad, Myles of Style hosted by Kim Myles, Paint-Over! hosted by Jennifer Bertrand, The Antonio Treatment hosted by Antonio Ballatore, lastly Secrets coming from a Stylist hosted by Emily Henderson. Bravo also offers a number of shows that explore the lives of interior designers. These include Flipping Out, which explores the life span of Jeff Lewis and the team of designers; Million Dollar Decorators explores the lives of interior designers Nathan Turner, Jeffrey Alan Marks, Mary McDonald, Kathryn Ireland, and Martyn Lawrence Bullard.
+
+Interior design has additionally become the subject of radio shows. In the U.S., popular decor & lifestyle shows include Martha Stewart Living and Living Large featuring Karen Mills. Famous interior designers whose jobs are featured on these programs include Bunny Williams, Barbara Barry, and Kathy Ireland, and the like.
+
+Many home design magazines exist to provide advice regarding color palette, furniture, art, along with elements that belong to the umbrella of decor. These magazine often give attention to related subjects to draw in a more specific audience. For instance, architecture like a primary facet of Dwell, while Veranda is well known like a luxury living magazine. Lonny Magazine and also the newly relaunched, Domino Magazine, meet the needs of a young, hip, metropolitan audience, and emphasize accessibility as well as a do-it-yourself (DIY) approach to decor. 
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showlink']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showpermalink']         = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site 
+in web explorer, might test this? IE nonetheless is the 
+marketplace chief and a huge section of other people will 
+pass over your wonderful writing because of this problem.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showdate']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and 
+a huge section of other people will pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showmdate']             = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site 
+in web explorer, might test this? IE nonetheless is the marketplace 
+chief and a huge section of other people will pass over 
+your wonderful writing because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showuser']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and a huge section of other people will pass over 
+your wonderful writing because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showcomments']          = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+IE nonetheless is the marketplace chief and a huge section of other people will 
+pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showlinkbacks']         = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might 
+test this? IE nonetheless is the marketplace chief and 
+a huge section of other people will pass over your wonderful 
+writing because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site 
+in web explorer, might test this? IE nonetheless 
+is the marketplace chief and a huge section of other people 
+will pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['doredirect']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and a huge section of 
+other people will pass over your wonderful writing because 
+of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['doindent']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and a huge section of other people 
+will pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['linkonly']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['title']                 = 'I am the new guy';
+$lang['pageexists']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['parlink']               = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['safeindex']             = 'Sal';
+$lang['order']                 = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer,
+might test this? IE nonetheless is the marketplace chief and a huge section of other 
+people will pass over your wonderful writing because 
+of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_id']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your 
+web site in web explorer, might test this? IE nonetheless is the marketplace chief and a huge section of other people will 
+pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+IE nonetheless is the marketplace chief and a huge 
+section of other people will pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_modified']      = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+IE nonetheless is the marketplace chief and a huge section of other people will pass over your wonderful writing because of this problem.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_indexmenu']     = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer,
+might test this? IE nonetheless is the marketplace chief and a huge section of other people will pass over your wonderful writing because of 
+this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['order_o_custom']        = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+IE nonetheless is the marketplace chief and a huge section of other people will pass over your wonderful writing because of this problem.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['rsort']                 = 'Stuckgowan';
+$lang['depth']                 = 'Sal';
+$lang['readmore']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['debugoutput']           = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Hello, Neat post. There\'s an issue along with your web site in web explorer, might test this?
+
+IE nonetheless is the marketplace chief and a huge section of 
+other people will pass over your wonderful writing 
+because of this problem. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-tinh-bien-an-giang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
