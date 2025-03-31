13f6065646a6f9f2ad7d06e84b6ba77d0afa25e6
diyarbakır Escort Bayan <kianwhitmore@gmail.com>
2025-03-31 19:15:37 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakır Escort Bayan <kianwhitmore@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'This is a great tip particularly to those new to the blogosphere.
+
+Short bbut very precise information… Thanks for sharing thks one.
+A must read post!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,81 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakır Escort Bayan <kianwhitmore@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new guy';
+$lang['firstseconly']          = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['showtaglogos']          = '28';
+$lang['showfooter']            = 'Zachary precisely what you can call me but hand calculators call me anything such as. I used to be unemployed but now i\'m an information officer and I\'ll be promoted in no time. What her in addition to her love is dominoes but she is struggling much more time so as. My husband and My home is Arkansas. If you want to get more information check out my website: https://oke.zone/viewtopic.php?pid=1248755';
+$lang['showlink']              = 'https://oke.zone/viewtopic.php?pid=1248755';
+$lang['showpermalink']         = 'This is a great tipp particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A mmust read post!';
+$lang['showdate']              = 'This is a great tip particularly to those new to the blogosphere.
+
+Short but very precise information… Thanks for sharing this one.
+
+A must read post!';
+$lang['showmdate']             = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing tis one.
+
+A must rread post!';
+$lang['showuser']              = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['showcomments']          = 'This is a great tip particularly to those new to the blogosphere.
+
+Short but very precise information… Thankss for sharing this one.
+A must read post!';
+$lang['showlinkbacks']         = 'This is a great tip particularly to thise new to the blogosphere.
+Short but very precise information… Thanks for sharing thiss one.
+A must read post!';
+$lang['showtags']              = '18';
+$lang['showeditbtn']           = 'This is a great tip particularly to those new to the blogosphere.
+
+Short but vefy precise information… Thhanks foor sharing this one.
+A must read post!';
+$lang['doredirect']            = 'This iis a great tip particularly to those new to the blogosphere.
+Short buut very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['doindent']              = 'This is a great tip particularly to those new to the blogosphere.
+Sort but very precise information… Thsnks for sharing 
+thi one. A must read post!';
+$lang['linkonly']              = 'https://oke.zone/viewtopic.php?pid=1248755';
+$lang['title']                 = 'I am the new guy';
+$lang['pageexists']            = 'https://oke.zone/viewtopic.php?pid=1248755';
+$lang['parlink']               = 'https://oke.zone/viewtopic.php?pid=1248755';
+$lang['safeindex']             = 'Kian';
+$lang['order']                 = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['order_o_id']            = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['order_o_modified']      = 'Thhis is a reat tip particularly to those new to the blogosphere.
+
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['order_o_indexmenu']     = 'This is a great tip particularly to those new to the blogosphere.
+Short but very precise information… Thanks for sharing this one.
+A must read post!';
+$lang['order_o_custom']        = 'This is a great tip particularly to those new to the blogosphere.
+
+Short but very precise information… Thanks for sharing this one.
+
+A must read post!';
+$lang['rsort']                 = 'Peoria';
+$lang['depth']                 = 'Kian';
+$lang['readmore']              = 'https://oke.zone/viewtopic.php?pid=1248755';
+$lang['debugoutput']           = 'This is a great tip particularly to those new to the blogosphere.
+
+Short but very precise information… Thaks for sharing this one.
+A must read post!';
