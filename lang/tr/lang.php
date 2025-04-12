5ffb78cf26f06dd7cfe3841f164f5e4f999c0b81
diyarbakır escort bayan <sherrykowalski@yahoo.com>
2025-04-12 19:30:14 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakır escort bayan <sherrykowalski@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'You actually make it appear so easy alonhg with your presentation but I in finding this matter to be really something that I think I might never understand.
+It kind of feels too complex and very large for me.
+
+I\'m taking a look ahead on yyour next publish, I\'ll attempt to get the dangle of it!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,100 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakır escort bayan <sherrykowalski@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im glad I now registered';
+$lang['firstseconly']          = 'You actually make it appear so easy along with your 
+presentation but I in finding this matter to be really something that I think I might never understand.
+It kind of feels too complex and very large for 
+me. I\'m taking a look ahead on your next publish, I\'ll 
+attempt to get the dangle of it!';
+$lang['showtaglogos']          = '14';
+$lang['showfooter']            = 'Jacques Diggins is what you are able call me but it\'s not the most masculine details. After being out of his job regarding the he was a medical worker and it is something he revel in. What me and my family love is playing chess but I have not made any money with the item. Guam is the only place I\'ve been residing in. If you want to find out more check out my website: https://Myclassictv.com/@winstonledesma?page=about';
+$lang['showlink']              = 'https://Myclassictv.com/@winstonledesma?page=about';
+$lang['showpermalink']         = 'You actually make it appear so easy along with your 
+presentation bbut I in finding this matter to be really something that I think I might never 
+understand. It kind of feels too complex and very large 
+ffor me. I\'m taking a look ahead on your next publish, I\'ll attempt to 
+get the dangle of it!';
+$lang['showdate']              = 'You actually make it appear so easy along with your presentation but 
+I in finding this matter to be really something that I think I might never understand.
+It kind of feels too complex and very large for me.
+I\'m taking a look ahead on your next publish, I\'ll 
+attempt to get the danbgle of it!';
+$lang['showmdate']             = 'You actually make it appear so easy along with your presentation but I in finding this matter to be really something thatt I think I 
+might never understand. It kind of feels too complex and vedry 
+lrge for me. I\'m taking a look ahead onn your next publish, I\'ll attempt to 
+get the dangle oof it!';
+$lang['showuser']              = 'You actually make it appear so easy along with your presentation but I in finding this matter to be really something 
+that I think I might never understand. It kind of feels too complex and very large for 
+me. I\'m taking a look ahead on your next publish, I\'ll attempt to get the dangle of it!';
+$lang['showcomments']          = 'You actually make it appear so easy along 
+with your presentation but I inn finding this matter to be really something thatt I think I might never understand.
+
+It kind of feels too complex and very large for 
+me. I\'m taking a look ahead on your next publish,
+I\'ll attempt to get the dangle of it!';
+$lang['showlinkbacks']         = 'You actually make it appear so easy along with your presentation butt I in finding this 
+matter to be really something that I think I might never understand.
+It kind oof feels too complex and very large for me.
+I\'m taking a look ahead on your next publish, I\'ll attempt to get 
+the dangle of it!';
+$lang['showtags']              = '21';
+$lang['showeditbtn']           = 'You acttually make iit appear so easy along with your presentation but I in finding this matter to be really something that 
+I think I might never understand. It kind of feels too complex and very large forr me.
+I\'m taking a look ahead on your next publish, I\'ll attempt to get the dangle 
+of it!';
+$lang['doredirect']            = 'You actually make it appear so easy along with your presentation butt I in finding 
+this matter to be really something that I thinjk I might never understand.
+It kind of feels too complex and very large for me.
+I\'m taking a look ahead on youjr next publish, I\'ll attempt to get 
+the dangle of it!';
+$lang['doindent']              = 'You actually mazke it appear so easy along with your presentation but I in finding this matter to be really something 
+that I think I might never understand. It kind of feels too complex 
+and very large for me. I\'m taking a look ahead on your next 
+publish, I\'ll attempt to get the dangle of it!';
+$lang['linkonly']              = 'https://Myclassictv.com/@winstonledesma?page=about';
+$lang['title']                 = 'Im glad I now registered';
+$lang['pageexists']            = 'https://Myclassictv.com/@winstonledesma?page=about';
+$lang['parlink']               = 'https://Myclassictv.com/@winstonledesma?page=about';
+$lang['safeindex']             = 'Sherry';
+$lang['order']                 = 'You actually make it appear so easy along with your presentation but I in finding this matter to be really 
+something that I think I might never understand. It kind of feels too complex annd very large for me.
+I\'m taking a look ahead on your next publish, I\'ll attempt to get the dangle of it!';
+$lang['order_o_id']            = 'You actually make it appear so easy along with your presentation but I 
+in finding this matter to be really something that I 
+think I might never understand. It kind of feels too complex and very large for me.
+
+I\'m taking a look ahead on your next publish, I\'ll attempt to get the dangle of it!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'You actually make it appear so easy along with your 
+presentation but I in finding this matter to bbe really something that I 
+think I might never understand. It kind of feels too complex and very large 
+for me. I\'m taking a look ahead on your next publish, I\'ll attempt 
+to get the dangle of it!';
+$lang['order_o_modified']      = 'You actually make it appear sso easy along with 
+you presentation but I in finding this matter to bee really something that I think I might never understand.
+It kind of feels too complex and very large for me.
+
+I\'m taking a look ahead on your next publish, I\'ll attempt to get the dangle of it!';
+$lang['order_o_indexmenu']     = 'You actually make it appear so easy along with your presentation but 
+I in finding this matter to be really something thhat I think 
+I might never understand. It kind of feels too complex 
+and very large for me. I\'m taking a look ahead on your next publish, I\'ll attempt to get 
+the dangle of it!';
+$lang['order_o_custom']        = 'You actually make it appear so easy alonbg with your presentation but I 
+in finding this matter to be really something that I think I 
+might never understand. It kind of fsels too complex and 
+very large for me. I\'m takinmg a look ahead oon yopur next publish,
+I\'ll attempt to get the dangle off it!';
+$lang['rsort']                 = 'Bole';
+$lang['depth']                 = 'Sherry';
+$lang['readmore']              = 'https://Myclassictv.com/@winstonledesma?page=about';
+$lang['debugoutput']           = 'You actually make it appear so easy along with your presentation but 
+I iin finding this maqtter to be really something that I think 
+I might never understand. It kind oof feels too 
+complex and very large for me. I\'m taking a look ahead on your next publish, I\'ll 
+attempt to get the dangle of it!';
