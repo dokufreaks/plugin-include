1d58b60bb91cc0727141f096825cfab8bcfde0b0
Ashton <gabrielasowerby@orange.fr>
2025-04-13 00:50:37 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Ashton <gabrielasowerby@orange.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I’m not thqt much off a internet rezder to be honest but your sites really nice, keep 
+it up! I\'ll go ahead and bookmark your website to 
+come back down the road. Many thanks';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,75 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Ashton <gabrielasowerby@orange.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im glad I finally registered';
+$lang['firstseconly']          = 'I’m not that much of a internet reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come bck down tthe road.
+Many thanks';
+$lang['showtaglogos']          = '12';
+$lang['showfooter']            = 'The author is known by the category of Mei. Years ago she gone to live in Guam. To go to karaoke is a thing that I\'m totally addicted to. Credit authorising is what she does. He is running as well as a blog here: https://Host360.com.tr/@raymondchristi';
+$lang['showlink']              = 'https://Host360.com.tr/@raymondchristi';
+$lang['showpermalink']         = 'I’m noot that much of a internet reader to be 
+honest but your sites really nice, keep it up! I\'ll ggo ahead and 
+bookmark your website to come back down the road. Many thanks';
+$lang['showdate']              = 'I’m not that much of a internet reader to be honest but 
+your sites really nice, keep it up!I\'ll go ahead and bookmark yoir website 
+to come back down the road. Many thanks';
+$lang['showmdate']             = 'I’m not that much off a internet reader 
+to be honest but your sites really nice, kedp it up!
+I\'ll go aheadd and bookmark yiur website to come back down the road.
+Many thanks';
+$lang['showuser']              = 'I’m not that much of a internet reader to be honesst but your 
+sites really nice, keep it up! I\'ll go ahead and bookmark your website to come baxk down the 
+road. Many thanks';
+$lang['showcomments']          = 'I’m not that much of a internet reader to be honest but your sites really nice, keep it 
+up! I\'ll go ahead and bookmark your website to come back down the road.
+
+Many thanks';
+$lang['showlinkbacks']         = 'I’m not that much of a internet reader to be honest but 
+your sites really nice, keep it up! I\'ll go ahead and bookmark your website to come back down the road.
+Many thanks';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'I’m not that much of a internet reader too be honest but your sites really 
+nice, keep it up! I\'llgo ahead and bookmark your website to come back 
+down the road. Many thanks';
+$lang['doredirect']            = 'I’m not that much of a internet reader to be honest but your sites really nice, keep it 
+up! I\'ll go ahead and bookmark your website to come back 
+down the road. Many thanks';
+$lang['doindent']              = 'I’m not that much of a internet reader to be 
+honest but your sites really nice, keep it up! I\'ll go ahead and bookmark your website to 
+come back down the road. Many thanks';
+$lang['linkonly']              = 'https://Host360.com.tr/@raymondchristi';
+$lang['title']                 = 'Im glad I finally registered';
+$lang['pageexists']            = 'https://Host360.com.tr/@raymondchristi';
+$lang['parlink']               = 'https://Host360.com.tr/@raymondchristi';
+$lang['safeindex']             = 'Gabriela';
+$lang['order']                 = 'I’m not that much of a internet reader to be honest 
+but your sites really nice, keep it up! I\'ll go ahead and bookmark your website to come back down the road.
+Many thanks';
+$lang['order_o_id']            = 'I’m not that much of a internet reader to be honest but your sites really 
+nice, keep it up! I\'ll go ahead and bookmark your website to come back down the 
+road. Many thanks';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I’m not that much of a internet reader to be honest but your 
+sites really nice, kep it up! I\'ll go ahead and bookmark your website to come back down the road.
+Many thanks';
+$lang['order_o_modified']      = 'I’m not that much of a internet reader to be honest but your sites really nice, keep 
+it up! I\'ll go ahead and bookmark your website to come back down the road.
+
+Many thanks';
+$lang['order_o_indexmenu']     = 'I’m not that much of a internet reader to be honest but your sites really nice, 
+keep it up! I\'ll go ahead and boojmark your website to come back down the 
+road. Many thanks';
+$lang['order_o_custom']        = 'I’m not that much of a internet reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back down the road.
+Many thanks';
+$lang['rsort']                 = 'Nykoping';
+$lang['depth']                 = 'Gabriela';
+$lang['readmore']              = 'https://Host360.com.tr/@raymondchristi';
+$lang['debugoutput']           = 'I’m not that much of a internet reader to be honest but your 
+sites really nice, keep itt up! I\'ll go ahead and bookmark your website to come back down the 
+road. Many thanks';
