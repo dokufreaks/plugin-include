081a62e009abde36466406c12a49a698378d2f63
Eartha <mai_vanwagenen@cox.net>
2025-04-14 21:20:05 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Eartha <mai_vanwagenen@cox.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Fantastic blog you have here but I was wanting to know if 
+you knew of any message boards that cover the same topics discussed in this article?
+I\'d really like to be a part of community where I can get responss from otther 
+knowledgeable people that share the same interest. If you have aany 
+suggestions, please let me know. Kudos!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,111 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Eartha <mai_vanwagenen@cox.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hi!';
+$lang['firstseconly']          = 'Fantastic blog you have here but I was wanting to know if you 
+knew of any message boards that cover the same topics 
+discussed in this article? I\'d really like to be a part of community where I can get 
+responses from other knowledgeable people that share the same interest.
+If you have any suggestions, lease let me know. Kudos!';
+$lang['showtaglogos']          = '12';
+$lang['showfooter']            = 'The person that wrote content is called Jacques but he doesn\'t like making certain you\'re use his full establish. My family lives in Delaware. His wife doesn\'t like it the way he does but what he really likes doing is archaeology but he is struggling obtain time so as. Credit authorising is what she does for a living. She is running and looking after a blog here: https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['showlink']              = 'https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['showpermalink']         = 'Fantastic blog you have here but I was wanting to know if you knew of any message boards that cover 
+the same topics discussed in this article? I\'d really like 
+to be a part of commjnity where I can get responses from 
+other knowledgeable people that share the same interest.
+If you have any suggestions, pleazse let me know.
+Kudos!';
+$lang['showdate']              = 'Fantastic blog you have here but I was wanting to know if you knew of any message boards that cover the same 
+topics discussed in this article? I\'d really like to be a part of community where I can get responses from other knowledgeable people that share the 
+same interest. If you have any suggestions, please let 
+me know. Kudos!';
+$lang['showmdate']             = 'Fantastic blog you have here buut I was wanting to know 
+if you knew of any message biards thhat cover the ssame topics discussed in this article?
+I\'d really like to be a part of community where I can get responses from other knowledgeable 
+people that share the same interest. If you have any suggestions, please let mme know.
+Kudos!';
+$lang['showuser']              = 'Fantastic blog you have hsre but I was wanting to know 
+if you knew of any message boards that cover the same topics discussed in this article?
+I\'d really like too be a part oof community where I can get responses from other 
+knowledgeable people that share thhe same interest. If you have any suggestions, please let me 
+know. Kudos!';
+$lang['showcomments']          = 'Fantastic blog you have here but I was wanting to know if you knew of any message boards that cover the same topics discussed in this article?
+I\'d really like tto be a part of community where I can get responses from other knowledgeable people that 
+share the same interest. If you have any suggestions, please let me know.
+Kudos!';
+$lang['showlinkbacks']         = 'Fantastic blog you have here but I was wanting to knoww if you knew of any message boards that cover the same topics discussed in this article?
+I\'d really like to be a part of community where I 
+can get responses from other knowledgeable people that share the same interest.
+If you have any suggestions, please let me know.
+Kudos!';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'Fantastic blog you have here but I was wanting to 
+know if you knew of any message boards that cover 
+the same topics discussed in this article? I\'d really 
+like to bee a part of communiy wheere I can get 
+responses from other knowledgeaable peope that share the same interest.
+If you have any suggestions, please let me know.
+Kudos!';
+$lang['doredirect']            = 'Fantastic blog you have here but I was wanting to know if you knew 
+of any message boards that cover the same topics discussed in ths article?
+I\'d really like to be a part of community where I can get responses 
+from other knowledgeable people that sshare the same interest.
+
+If you have anyy suggestions, please let me know. Kudos!';
+$lang['doindent']              = 'Fantastic blog you have here but I was wanting to knlw if you kneew of any message 
+boards that coverr the same topics discussed 
+in this article? I\'d really like too be a part 
+of community where I can get responses from other knowledgeable people 
+that share the same interest. If you have any suggestions, please let me 
+know. Kudos!';
+$lang['linkonly']              = 'https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['title']                 = 'Just want to say Hi!';
+$lang['pageexists']            = 'https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['parlink']               = 'https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['safeindex']             = 'Mai';
+$lang['order']                 = 'Fantastic blog yoou have here but I was waning to know 
+if you knew of any message boards that cover the same topics discussed in this article?
+I\'d really like to bee a part of community where I can get responses from other knowledgeable people that share the same interest.
+
+If you have any suggestions, please let me know. Kudos!';
+$lang['order_o_id']            = 'Fantastic blog you haqve here but I was wanting to know if you knew of any message boards 
+that cover the same topics discussed in this article? I\'d realky like to be a part of community where I can get 
+responsds from other knowledgeable people that share the 
+same interest. If you have any suggestions, please let me know.
+Kudos!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Fantastic blog you have here but I was wantijg to know if you knew of any message boards 
+that cover thee same tpics discussed in this article? I\'d really like to be a part of 
+community where I can get responses from other knowledgeable people 
+that share the same interest. If you have any suggestions, please let 
+me know. Kudos!';
+$lang['order_o_modified']      = 'Fantastic blog you have here but I was wanting to know if you knew of any message boards that cover the 
+sae topics disxcussed in this article? I\'d really like to 
+be a part of community where I can get responses from other knowledgeable people that share tthe 
+same interest. If you have any suggestions, please let me know.
+Kudos!';
+$lang['order_o_indexmenu']     = 'Fantastic blog you have here but I was wanting to know if you 
+knew of any message boards that cover the same topics discussed in this article?
+
+I\'d realky like to be a part of community where I can get responses from other knowledgeable people 
+that share the saame interest. If you have any suggestions, please let me 
+know. Kudos!';
+$lang['order_o_custom']        = 'Fantastic blog you have here but I was wanting to know if you knew of any message 
+boards that cover the same topics discussed in this article?
+I\'d really like to bbe a part of communiy where I can get responses from other 
+knowledgeable people that share the same interest.
+If you have anny suggestions, please let me know. Kudos!';
+$lang['rsort']                 = 'Ceto';
+$lang['depth']                 = 'Mai';
+$lang['readmore']              = 'https://api-2-0.Spot.im/v1.0.0/feed/redirect?url=aHR0cHM6Ly9ncmFwaC5vcmcvJUM0JUIwcmktRyVDMyVCNiVDNCU5RiVDMyVCQ3NsZXJlLVNhaGlwLU9sZ3VuLURpeWFyYmFrJUM0JUIxci1Fc2NvcnQtQmF5YW5sYXIlQzQlQjEtMDQtMTE=&params=eyJwYWdlX3ZpZXdfaWQiOiI3OWI2YmEwNy0xMmE0LTQyYTYtYjUyZi1hOThkNWYxYjQyOGIiLCJyZWZlcnJlciI6IiIsInVzZXJfaWQiOiIwIiwiaWJfYnJvd3Nlcl9pZCI6IjI5ZTMxN2IyYjdmYWMxOTE4NDdhY2M4MTYxNGIzNzVjIiwidmlzaXRfaWQiOiI4MGJjNGZlNDdmMjIyODQ3YzBjMjg3MjZiNTVkMTM0ZCIsInYiOjEwLCJkb21haW4iOiJ3d3cubXVzY2xlYW5kZml0bmVzcy5jb20iLCJ1cmwiOiJodHRwczovL3d3dy5tdXNjbGVhbmRmaXRuZXNzLmNvbS9hdGhsZXRlcy1jZWxlYnJpdGllcy9uZXdzL211c2NsZS1maXRuZXNzLXNhbHV0ZXMtZ3JlYXRlc3Q/dXRtX3NvdXJjZT1mZWVkYnVybmVyJnV0bV9tZWRpdW09ZmVlZCZ1dG1fY2FtcGFpZ249RmVlZCUzQSUyMGJsb2dzcG90L25FWFNtJTIwJTI4SGVhbHRoJTIwJTI2YW1wJTNCJTIwV2VsbG5lc3MlMjkiLCJsYW5nIjoiZW4tVVMiLCJ1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzb3VyY2VfdmVyc2lvbiI6IiIsInRlc3RfZmllbGQiOiIzMjpDfDMzOkN8MzQ6Q3wzNTpCfDM3OkIiLCJzcGxpdF9uYW1lIjoiMzU6QiIsInNwb3RfaWQiOiJzcF9LVmQ0V1hNRCIsImNvbnZlcnNhdGlvbl9pZCI6InNwX0tWZDRXWE1EXzcyMTUiLCJzb3VyY2UiOiJwaXRjIiwicm93IjoxLCJzZWdtZW50IjoiaG9yaXpvbnRhbCIsImVsZW1lbnQiOiJ7XCJtZXNzYWdlX2lkXCI6XCJzcF9LVmQ0V1hNRF8yMzMzMl9jXzJBYWZwdFwiLFwidXNlcl9pZFwiOlwidV9TaEpTeUZjWFd3RmZcIixcImNvbnZlcnNhdGlvbl9pZFwiOlwic3BfS1ZkNFdYTURfMjMzMzJcIixcIml0ZW1fbnVtYmVyXCI6M30iLCJ0YXJnZXRfdHlwZSI6ImNvbW1lbnQiLCJhY3Rpb24iOiJjbGlja2VkIiwiZGV2aWNlX3V1aWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAifQ==';
+$lang['debugoutput']           = 'Fantastic blog you have here but I waas wanting to know if you knew off any message boards that cover the sae topics discussed in this article?
+I\'d really like to be a part of community where I can get responses from other knowledgeable people that share the same interest.
+
+If you have any suggestions, please leet me know.
+
+Kudos!';
