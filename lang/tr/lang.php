b9a6c1ef07aa0f951c17bf18c746d2831377bfa0
DiyarbakıR Escort Bayan <frankiebarrenger@cox.net>
2025-04-28 22:15:38 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR Escort Bayan <frankiebarrenger@cox.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about myy 
+website not working correctly in Explorer but looks great 
+in Safari. Do you have any recommendations to help fix this problem?';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,98 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR Escort Bayan <frankiebarrenger@cox.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'I am really loving the theme/design of your web site. Do you evrr run into any 
+internet browser compatibility issues? A number of my blog visitors have complained about my website not working correctly in Explorer but looks great in Safari.
+
+Do you have any recommendations to help fix this problem?';
+$lang['showtaglogos']          = '11';
+$lang['showfooter']            = 'Zachary exactly what you can call me but doable ! call me anything you like. His wife doesn\'t like it the way he does but what he really loves doing is kayaking but he doesn\'t have any the time lately. My family lives in Delaware. After being the particular his problem for years he became a medical member. He\'s been repairing his website for ages now.  here: http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['showlink']              = 'http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['showpermalink']         = 'I am really loving the theme/design of your web site. Do you ever rrun into 
+any internet browser compatibility issues? A number of 
+my blog visitors have complained about my website not working correctly in Explorer but looks great in Safari.
+Do you have any recommendations to help fix this problem?';
+$lang['showdate']              = 'I am really loving the theme/design of your web site.
+Do you ever run int any internet browser compatibility issues?
+A number of my blog visitors have complained about my website not working correctly in Explorer but lookks great in Safari.
+Do you haave any recommendations to help fix this problem?';
+$lang['showmdate']             = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about my website not working correctly in Explorer but looks great in Safari.
+Do you have any recommendations to help fix this problem?';
+$lang['showuser']              = 'I am really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+
+A number of my blog visitors have complained about my website not working correctly 
+in Explorer but looks great in Safari. Do you have any recommendations to help 
+fix this problem?';
+$lang['showcomments']          = 'I am really loving the theme/design oof your web site. Do you ever run into 
+any internet browser compatibility issues? A number of my blog visitors havbe complained about my website not working correctly in Explorer but looks grea in Safari.
+Do you have any recommendations too help fix this problem?';
+$lang['showlinkbacks']         = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about my website not working correctly in Explorer but looks great in Safari.
+Do you have any recommendations to help fix this problem?';
+$lang['showtags']              = '23';
+$lang['showeditbtn']           = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained 
+about my website not working correctly in Explprer but looks 
+great in Safari. Do you have any recommendations to help 
+fix this problem?';
+$lang['doredirect']            = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+
+A number of my blog visitors have complained about my website 
+not working correctly in Explorer but looks great in Safari.
+
+Do you have any recommendations to help fix this problem?';
+$lang['doindent']              = 'I am really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+A number of my bloog visitors have complained abbout my website not 
+working correctly in Explorer but looks great in Safari.
+Do you have any recommendations to help fix this problem?';
+$lang['linkonly']              = 'http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['parlink']               = 'http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['safeindex']             = 'Frankie';
+$lang['order']                 = 'I am really loving thee theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+
+A number of my blog visitors have complained about my website not working correctly 
+in Explorer but looks gdeat in Safari. Do you have any recommendations to help fix this problem?';
+$lang['order_o_id']            = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of myy blog visitors have complained about my website not working correctly 
+inn Explorer but looks great in Safari. Do youu 
+have any recommendations to help fix this problem?';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I am really loving the theme/design of your wweb site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about my website not working correctly in Explorer but looks gredat 
+in Safari. Do you have any recommendations to help fix 
+this problem?';
+$lang['order_o_modified']      = 'I am really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about mmy website not working correctly in Explorer but looks 
+great in Safari. Do you have any recommendations to help fix this problem?';
+$lang['order_o_indexmenu']     = 'I am really loving the theme/design of your web site. Do you 
+ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about my website not working correctly inn Expporer but looks great 
+in Safari. Do you have any recommendations to help fix this problem?';
+$lang['order_o_custom']        = 'I am really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+A number of my blog visitors have complained about 
+my website not working correctly in Explorer but looks great in Safari.
+Do you have any recommendations to help fix this problem?';
+$lang['rsort']                 = 'Winkl';
+$lang['depth']                 = 'Frankie';
+$lang['readmore']              = 'http://pda.ardoboz.hu/r.php?b=aHR0cHM6Ly9ibG9nZnJlZWx5Lm5ldC9ncmVla2NvbmdvOC9iYXlhbi1wYXJ0bmVyLWJ1bG1hLWRpeWFyYmFraXI';
+$lang['debugoutput']           = 'I am really loving the theme/design of your web site.
+
+Do you ever run into any internet browser compatibility 
+issues? Anumber of my blog visitors have complained about my website not working correctly in Explorer but looks great inn Safari.
+Do you have any recommendations to help fix this problem?';
