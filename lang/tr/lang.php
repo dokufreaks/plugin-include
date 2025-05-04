a68198e30f89d66cafa53689f7e9da3ba08cc2cd
diyarbakıR escort <willybrewis@live.nl>
2025-05-04 06:55:46 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakıR escort <willybrewis@live.nl>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Hello my friend! I wish to say that ths article is amazing, nice written and come with approximately 
+all vital infos. I\'d ljke to look more posts like this .';
