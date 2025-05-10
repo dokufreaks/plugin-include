52f1be65b4729358f6218b52ecf0e7f41aba3c45
diyarbakıR Escort bayan Sitesi <dakotagsell@yahoo.com.au>
2025-05-10 05:25:05 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakıR Escort bayan Sitesi <dakotagsell@yahoo.com.au>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Thanks designed for sharing such a fastidious thought, 
+piece of writing is fastidious, thats why i have read it completely';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,54 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author diyarbakıR Escort bayan Sitesi <dakotagsell@yahoo.com.au>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I finally registered';
+$lang['firstseconly']          = 'Thanks designed for sharing such a fastidious thought, piece of riting is fastidious, thats why i have read it completely';
+$lang['showtaglogos']          = '22';
+$lang['showfooter']            = 'His name is Zachary Cantara but he doesn\'t like and also exercising . use his full user name. For a while he\'s visited Arkansas. To go to karaoke is one thing I by no means give to # 1. His job is a credit authoriser. You can always find his website here: https://my-other-half.com/@alisadegaris9';
+$lang['showlink']              = 'https://my-other-half.com/@alisadegaris9';
+$lang['showpermalink']         = 'Tanks designed for sharing such a fastidious thought, piece of writing iss fastidious, thats why i have 
+read it completely';
+$lang['showdate']              = 'Thanks designed for sharing such a fastidious thought, piece of writing is fastidious, thats why i have read it completely';
+$lang['showmdate']             = 'Thanks designed for sharing such a fastidious thought, 
+piece of writing is fastidious, thats why 
+i have read it completely';
+$lang['showuser']              = 'Thawnks designed for sharing such a fastidious thought, piehe of writing 
+is fastidious, thats why i have read it completely';
+$lang['showcomments']          = 'Thanks designed for sharing such a fastidious thought, piece off writing iss fastidious, thats why i 
+have read it completely';
+$lang['showlinkbacks']         = 'Thanks designed for sharing such a fastidious thought, piece 
+of writing is fastidious, thats why i have read it completely';
+$lang['showtags']              = '26';
+$lang['showeditbtn']           = 'Thanks designed for sharing such a fastidious thought, piece 
+of writing is fastidious, thats why i have read it 
+completely';
+$lang['doredirect']            = 'Thanks designed for sharing such a fastidious 
+thought, piece of writing is fastidious, thats why i have read it completely';
+$lang['doindent']              = 'Thanks designed for sharing such a fastidious thought, piece of writing is fastidious, thats why i have read it completely';
+$lang['linkonly']              = 'https://my-other-half.com/@alisadegaris9';
+$lang['title']                 = 'Im happy I finally registered';
+$lang['pageexists']            = 'https://my-other-half.com/@alisadegaris9';
+$lang['parlink']               = 'https://my-other-half.com/@alisadegaris9';
+$lang['safeindex']             = 'Dakota';
+$lang['order']                 = 'Thanks designed for sharing such a fastidious thought,
+piece of writing is fastidious, thats why i have read it completely';
+$lang['order_o_id']            = 'Thanks designed for sharing such a fastidious thought, piece of writing is fastidious, thats why 
+i have read itt completely';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Thanks designed for sharing such a fastodious 
+thought, piece of writing iis fastidious, thats why i have read it completely';
+$lang['order_o_modified']      = 'Thanks designed for sharing such a fastidious thought, piece 
+of writing is fastidious, thats why i have read it 
+completely';
+$lang['order_o_indexmenu']     = 'Thanks designed foor sharing such a fastidious thought, piece of writing 
+is fastidious, thats why i have read it completely';
+$lang['order_o_custom']        = 'Thanks designed for sharing such a fastidious thought, piece of writing is fastidious, thats why i have read it completely';
+$lang['rsort']                 = 'Wallacedale';
+$lang['depth']                 = 'Dakota';
+$lang['readmore']              = 'https://my-other-half.com/@alisadegaris9';
+$lang['debugoutput']           = 'Thanks designed for sharing such a fastidious thought, piece 
+of writing is fastidious, thatgs why i have read it completely';
