86eb5c069627aa42d3ce8c35038f7fceca0ae666
Andreas Gohr <andi@splitbrain.org>
2025-07-04 18:16:59 +0200

    cleaned header syntax
    removed old compatibility checks
    (auto) reformatted
    droped line feeds

--- syntax/header.php
+++ syntax/header.php
@@ -1,5 +1,7 @@
 <?php
 
+use dokuwiki\Extension\SyntaxPlugin;
+
 /**
  * Include plugin (permalink header component)
  *
@@ -9,20 +11,22 @@
  * @author  Gina Haeussge <osd@foosel.net>
  * @author  Michael Klier <chi@chimeric.de>
  */
-
-class syntax_plugin_include_header extends DokuWiki_Syntax_Plugin
+class syntax_plugin_include_header extends SyntaxPlugin
 {
-    function getType()
+    /** @inheritdoc */
+    public function getType()
     {
         return 'formatting';
     }
 
-    function getSort()
+    /** @inheritdoc */
+    public function getSort()
     {
         return 50;
     }
 
-    function handle($match, $state, $pos, Doku_Handler $handler)
+    /** @inheritdoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
     {
         // this is a syntax plugin that doesn't offer any syntax, so there's nothing to handle by the parser
     }
@@ -32,47 +36,45 @@ class syntax_plugin_include_header extends DokuWiki_Syntax_Plugin
      *
      * Code heavily copied from the header renderer from inc/parser/xhtml.php, just
      * added an href parameter to the anchor tag linking to the wikilink.
+     * @inheritdoc
      */
-    function render($mode, Doku_Renderer $renderer, $data)
+    public function render($mode, Doku_Renderer $renderer, $data)
     {
         global $conf;
 
-        list($headline, $lvl, $pos, $page, $sect, $flags) = $data;
+        [$headline, $lvl, $pos, $page, $sect, $flags] = $data;
 
-        if ($mode == 'xhtml') {
-            /** @var Doku_Renderer_xhtml $renderer */
-            $hid = $renderer->_headerToLink($headline, true);
-            $renderer->toc_additem($hid, $headline, $lvl);
-            $url = ($sect) ? wl($page) . '#' . $sect : wl($page);
-            $renderer->doc .= DOKU_LF . '<h' . $lvl;
-            $classes = array();
-            if ($flags['taglogos']) {
-                $tag = $this->_get_firsttag($page);
-                if ($tag) {
-                    $classes[] = 'include_firsttag__' . $tag;
-                }
-            }
-            // the include header instruction is always at the beginning of the first section edit inside the include
-            // wrap so there is no need to close a previous section edit.
-            if ($lvl <= $conf['maxseclevel']) {
-                if (defined('SEC_EDIT_PATTERN')) { // for DokuWiki Greebo and more recent versions
-                    $classes[] = $renderer->startSectionEdit($pos, array('target' => 'section', 'name' => $headline, 'hid' => $hid));
-                } else {
-                    $classes[] = $renderer->startSectionEdit($pos, 'section', $headline);
-                }
-            }
-            if ($classes) {
-                $renderer->doc .= ' class="' . implode(' ', $classes) . '"';
-            }
-            $headline = $renderer->_xmlEntities($headline);
-            $renderer->doc .= ' id="' . $hid . '"><a href="' . $url . '" title="' . $headline . '">';
-            $renderer->doc .= $headline;
-            $renderer->doc .= '</a></h' . $lvl . '>' . DOKU_LF;
-            return true;
-        } else {
+        if ($mode != 'xhtml') {
+            // just output a standard header for all non-xhtml modes
             $renderer->header($headline, $lvl, $pos);
+            return true;
         }
-        return false;
+
+        /** @var Doku_Renderer_xhtml $renderer */
+        $hid = $renderer->_headerToLink($headline, true);
+        $renderer->toc_additem($hid, $headline, $lvl);
+        $url = ($sect) ? wl($page) . '#' . $sect : wl($page);
+        $renderer->doc .= '<h' . $lvl;
+        $classes = [];
+        if ($flags['taglogos']) {
+            $tag = $this->getFirsttag($page);
+            if ($tag) {
+                $classes[] = 'include_firsttag__' . $tag;
+            }
+        }
+        // the include header instruction is always at the beginning of the first section edit inside the include
+        // wrap so there is no need to close a previous section edit.
+        if ($lvl <= $conf['maxseclevel']) {
+            $classes[] = $renderer->startSectionEdit($pos, ['target' => 'section', 'name' => $headline, 'hid' => $hid]);
+        }
+        if ($classes) {
+            $renderer->doc .= ' class="' . implode(' ', $classes) . '"';
+        }
+        $headline = $renderer->_xmlEntities($headline);
+        $renderer->doc .= ' id="' . $hid . '"><a href="' . $url . '" title="' . $headline . '">';
+        $renderer->doc .= $headline;
+        $renderer->doc .= '</a></h' . $lvl . '>';
+        return true;
     }
 
     /**
@@ -80,7 +82,7 @@ class syntax_plugin_include_header extends DokuWiki_Syntax_Plugin
      *
      * @author Michael Klier <chi@chimeric.de>
      */
-    function _get_firsttag($page)
+    protected function getFirsttag($page)
     {
         if (plugin_isdisabled('tag') || (!plugin_load('helper', 'tag'))) {
             return false;
@@ -89,7 +91,7 @@ class syntax_plugin_include_header extends DokuWiki_Syntax_Plugin
         if (is_array($subject)) {
             $tag = $subject[0];
         } else {
-            list($tag, $rest) = explode(' ', $subject, 2);
+            [$tag, $rest] = explode(' ', $subject, 2);
         }
         if ($tag) {
             return $tag;
@@ -98,4 +100,3 @@ class syntax_plugin_include_header extends DokuWiki_Syntax_Plugin
         }
     }
 }
-// vim:ts=4:sw=4:et:
