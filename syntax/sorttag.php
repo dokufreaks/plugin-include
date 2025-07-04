1739944dbcf0b33a10de4d0748ead6c132636ee2
Andreas Gohr <andi@splitbrain.org>
2025-07-04 18:36:47 +0200

    clened up sorttag syntax

--- syntax/sorttag.php
+++ syntax/sorttag.php
@@ -1,61 +1,48 @@
 <?php
 
+use dokuwiki\Extension\SyntaxPlugin;
+
 /**
  * Include plugin sort order tag, idea and parts of the code copied from the indexmenu plugin.
  *
  * @license     GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author      Samuele Tognini <samuele@netsons.org>
  * @author      Michael Hamann <michael@content-space.de>
- *
  */
-class syntax_plugin_include_sorttag extends DokuWiki_Syntax_Plugin
+class syntax_plugin_include_sorttag extends SyntaxPlugin
 {
-    /**
-     * What kind of syntax are we?
-     */
+    /** @inheritdoc */
     public function getType()
     {
         return 'substition';
     }
 
-    /**
-     * The paragraph type - block, we don't need paragraph tags
-     *
-     * @return string The paragraph type
-     */
+    /** @inheritdoc */
     public function getPType()
     {
         return 'block';
     }
 
-    /**
-     * Where to sort in?
-     */
+    /** @inheritdoc */
     public function getSort()
     {
         return 139;
     }
 
-    /**
-     * Connect pattern to lexer
-     */
+    /** @inheritdoc */
     public function connectTo($mode)
     {
         $this->Lexer->addSpecialPattern('{{include_n>.+?}}', $mode, 'plugin_include_sorttag');
     }
 
-    /**
-     * Handle the match
-     */
+    /** @inheritdoc */
     public function handle($match, $state, $pos, Doku_Handler $handler)
     {
         $match = substr($match, 12, -2);
-        return array($match);
+        return [$match];
     }
 
-    /**
-     * Render output
-     */
+    /** @inheritdoc */
     public function render($mode, Doku_Renderer $renderer, $data)
     {
         if ($mode === 'metadata') {
