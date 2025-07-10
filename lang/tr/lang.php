0df554c8e0de940c59fbebfb5c3c3df7569740b0
https://ninja-crash.casino/ko/betano <carmelahollenbeck@gmail.com>
2025-07-10 13:55:33 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author https://ninja-crash.casino/ko/betano <carmelahollenbeck@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts will too.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,52 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author https://ninja-crash.casino/ko/betano <carmelahollenbeck@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just wanted to say Hi.';
+$lang['firstseconly']          = 'I every time emailed this blog post page to all my friends, 
+because if like to read it then my contacts will too.';
+$lang['showtaglogos']          = '21';
+$lang['showfooter']            = 'Hello! I am Carmela. I am happy that I can join to the entire globe. I live in Netherlands, in the south region. I dream to visit the various nations, to obtain familiarized with appealing people.';
+$lang['showlink']              = 'https://ninja-crash.casino/ko/betano';
+$lang['showpermalink']         = 'I every time emailed this blog post page to all 
+my friends, because if like to read it then my contacts will too.';
+$lang['showdate']              = 'I every time emailed this blog post page to all my friends, 
+because if like to read it then my contacts will too.';
+$lang['showmdate']             = 'I every time emailed this blog post page to all my friends, because 
+if like to read it then my contacts will too.';
+$lang['showuser']              = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts will too.';
+$lang['showcomments']          = 'I every time emailed this blog post page to all my friends, 
+because if like to read it then my contacts will too.';
+$lang['showlinkbacks']         = 'I every time emailed this blog post page to all my friends, 
+because if like to read it then my contacts will too.';
+$lang['showtags']              = '27';
+$lang['showeditbtn']           = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts 
+will too.';
+$lang['doredirect']            = 'I every time emailed this blog post page to all my friends, because 
+if like to read it then my contacts will too.';
+$lang['doindent']              = 'I every time emailed this blog post page to all my friends,
+because if like to read it then my contacts will too.';
+$lang['linkonly']              = 'https://ninja-crash.casino/ko/betano';
+$lang['title']                 = 'Just wanted to say Hi.';
+$lang['pageexists']            = 'https://ninja-crash.casino/ko/betano';
+$lang['parlink']               = 'https://ninja-crash.casino/ko/betano';
+$lang['safeindex']             = 'Carmela';
+$lang['order']                 = 'I every time emailed this blog post page to all my friends, because if like to read it then my 
+contacts will too.';
+$lang['order_o_id']            = 'I every time emailed this blog post page to all my friends, because if like to read it then my 
+contacts will too.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts 
+will too.';
+$lang['order_o_modified']      = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts will too.';
+$lang['order_o_indexmenu']     = 'I every time emailed this blog post page to all my friends, because if like to read 
+it then my contacts will too.';
+$lang['order_o_custom']        = 'I every time emailed this blog post page to all my friends, because if like to read it then my contacts will too.';
+$lang['rsort']                 = 'Utrecht';
+$lang['depth']                 = 'Carmela';
+$lang['readmore']              = 'https://ninja-crash.casino/ko/betano';
+$lang['debugoutput']           = 'I every time emailed this blog post page to all my friends, because if like 
+to read it then my contacts will too.';
