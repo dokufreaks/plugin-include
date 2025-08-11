adf054b3e7caf70d3a02c3be453a9b564886fed8
Diyarbakır Ofis Escortları <adela_metters@yahoo.fr>
2025-08-11 19:05:09 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakır Ofis Escortları <adela_metters@yahoo.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Hi Dear, are you actually visiting this web site regularly, 
+if so then yyou will without doubt take fastidious experience.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,51 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakır Ofis Escortları <adela_metters@yahoo.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new girl';
+$lang['firstseconly']          = 'Hi Dear, are you actually visiting this web site regularly, if so 
+then you wil without doubt take fastidious experience.';
+$lang['showtaglogos']          = '15';
+$lang['showfooter']            = 'The one that wrote content is called Britt great wife doesn\'t like it at all. She\'s always loved living in Delaware today she is considering could. One of the most suitable things in the united states for me is body building and I\'ve been doing it for quite a while. Filing is my profession. Check out her website here: https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['showlink']              = 'https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['showpermalink']         = 'Hi Dear, are you actually visiting this web site regularly, if so then you 
+will without doubt take fastidious experience.';
+$lang['showdate']              = 'Hi Dear, are you actually visiting this web site regularly, if sso then you 
+will without doubt take fastidious experience.';
+$lang['showmdate']             = 'Hi Dear, are you actually visiting this web site regularly, 
+if so then you will without doubt take fastidious experience.';
+$lang['showuser']              = 'Hi Dear, are you actually visiting this web site regularly, if so then you will without doubt take fastidious experience.';
+$lang['showcomments']          = 'Hi Dear, are youu actually vissiting this web site regularly, if so then you will without doubt take fastidious experience.';
+$lang['showlinkbacks']         = 'Hi Dear, are you actually visiting this web site regularly, if so then you will without doubt take fastidious experience.';
+$lang['showtags']              = '28';
+$lang['showeditbtn']           = 'Hi Dear, aree you actually visiting this web site regularly, if so then you will without 
+doubt take fastidious experience.';
+$lang['doredirect']            = 'Hi Dear, are you actually visiting this web 
+site regularly, if so then you will ithout doubt takee fastidious experience.';
+$lang['doindent']              = 'Hi Dear, are you actually visiting this web site regularly, if so then you will without doubt take fastidious experience.';
+$lang['linkonly']              = 'https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['title']                 = 'I am the new girl';
+$lang['pageexists']            = 'https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['parlink']               = 'https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['safeindex']             = 'Adela';
+$lang['order']                 = 'Hi Dear, are yyou actually visiting this web site regularly, if so then you 
+will without doubt take fastidious experience.';
+$lang['order_o_id']            = 'Hi Dear, are you actually visiting thus web site regularly, if so 
+then you will without doubt take fastidious experience.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Hi Dear, are you actually visiting this web site regularly, if 
+so then you will withiut doubt take fastidious experience.';
+$lang['order_o_modified']      = 'Hi Dear, are you actually visiting this web site regularly, 
+if so then you will without doubt take fastidious experience.';
+$lang['order_o_indexmenu']     = 'Hi Dear, are you actually visiting this web site regularly, 
+if so then yyou will without doubt take fastidious experience.';
+$lang['order_o_custom']        = 'Hi Dear, are you actually visiting tgis web site regularly, if so then you will 
+without doubt take fastidious experience.';
+$lang['rsort']                 = 'Hof';
+$lang['depth']                 = 'Adela';
+$lang['readmore']              = 'https://Enersoft.ru/go?https://md.kif.rocks/ELAMFaxJSOmB6SPN6ejtaw/';
+$lang['debugoutput']           = 'Hi Dear, are yyou actually visiting this web site regularly,
+iif so then you will without doubt take fastidious experience.';
