24f3c641688ed97143b13b2ad8dafeafd17545d8
DiyarbakıR escort bayan <rodrick_hargraves@bol.com.br>
2025-08-17 11:10:41 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR escort bayan <rodrick_hargraves@bol.com.br>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I like it when folks get together and share opinions.
+Great blog, keep it up!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,56 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author DiyarbakıR escort bayan <rodrick_hargraves@bol.com.br>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I now registered';
+$lang['firstseconly']          = 'I likme it when folks get together and share opinions.
+Great blog, keep it up!';
+$lang['showtaglogos']          = '12';
+$lang['showfooter']            = 'Jacques Arizmendi is how he\'s called but he doesn\'t like when people use his full discover. She\'s always loved living in Delaware exactly how she is considering wireless connections. To research fashion is effortless she\'s been doing hottest. Accounting is how I support my loved one. If you want for more check out my website: https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['showlink']              = 'https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['showpermalink']         = 'I like it when folks get together andd share opinions.
+Great blog, keep it up!';
+$lang['showdate']              = 'I like it when folks get together and share opinions. Great blog, keep it up!';
+$lang['showmdate']             = 'I like it when folks get together and share opinions. Great blog, keep it up!';
+$lang['showuser']              = 'I like it when folks get together andd share opinions. Great blog, keep it up!';
+$lang['showcomments']          = 'I like it when folks gget together and sharre opinions.
+
+Great blog, keep it up!';
+$lang['showlinkbacks']         = 'I like it whhen folks get together and share opinions.
+Great blog, keep itt up!';
+$lang['showtags']              = '13';
+$lang['showeditbtn']           = 'I like it when folks get together and share opinions. Great blog, keep 
+it up!';
+$lang['doredirect']            = 'I liie it when ffolks get together and share opinions. Great 
+blog, keep it up!';
+$lang['doindent']              = 'I like it when folks get together and share opinions.
+Great blog, keep it up!';
+$lang['linkonly']              = 'https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['title']                 = 'Im happy I now registered';
+$lang['pageexists']            = 'https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['parlink']               = 'https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['safeindex']             = 'Rodrick';
+$lang['order']                 = 'I like it when folks get together and share opinions.
+Great blog, keep it up!';
+$lang['order_o_id']            = 'I like iit when folks get together and share opinions.
+
+Great blog, keep iit up!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I like it when folks get together and sgare opinions.
+Great blog, keep itt up!';
+$lang['order_o_modified']      = 'I like it when folks get together and share opinions.
+Great blog, keep it up!';
+$lang['order_o_indexmenu']     = 'I like it when fklks get together and share opinions.
+Great blog, keep it up!';
+$lang['order_o_custom']        = 'I like it when folks get together and share opinions.
+
+Great blog, keep it up!';
+$lang['rsort']                 = 'Frederiksberg C';
+$lang['depth']                 = 'Rodrick';
+$lang['readmore']              = 'https://shorten.Wayhike.com/diyarbakirescortbayan714136';
+$lang['debugoutput']           = 'I like it when folkss get together and share opinions.
+
+Great blog, keep itt up!';
