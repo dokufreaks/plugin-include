8829db842d21c30a7ff1f417481b1dd19ef55aa7
Johnette <finlay_holland@gmail.com>
2025-08-18 21:45:46 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Johnette <finlay_holland@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like to have you share 
+some stories/information. I know my subscribers would enjoy your work.
+
+If you are even remotely interested, feel free to send me 
+an email.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,98 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Johnette <finlay_holland@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'I am the new one';
+$lang['firstseconly']          = 'Have you ever thought about writing an ebook or guest autyhoring on other blogs?
+I have a blog based on the same ideas you discuss and 
+would really like to have you share some stories/information. I know my subscribers would enjoy your work.
+
+If you are even remotely interested, feel free to 
+send me an email.';
+$lang['showtaglogos']          = '27';
+$lang['showfooter']            = 'Let me inroduce myself, my name is Maxine although it is not the most feminine of names. To play rock and roll is there isn\'t any magical he loves most. Pennsylvania is where me and my wife live but my wife wants us to travel. Accounting is what I do for a full time income. I\'ve been working on my website as word spread now. Examine it here: http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['showlink']              = 'http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['showpermalink']         = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like tto have you 
+share some stories/information. I know my subscribers 
+would enjoy your work. If you are even remitely interested, feel free to send me an email.';
+$lang['showdate']              = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like 
+to have you share some stories/information. I know my subscribers would enjoy ykur 
+work. If you are even remotel interested, feel free to send 
+me an email.';
+$lang['showmdate']             = 'Have you ever thought about writing an ebook orr guest 
+authoring on other blogs? I have a blog based on the same ideas you discuss and would really like to have you share some stories/information. I 
+know my subvscribers would enjoy your work.
+If you are even remotely interested, feel free to sebd me an email.';
+$lang['showuser']              = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like to have you share some stories/information. I know my subscribers would enjoy your work.
+If you are even remotely interested, feel free to send me an email.';
+$lang['showcomments']          = 'Have you ever thought about writing an ebook or 
+guest authoring oon other blogs? I have a blog based on the same ideas you 
+discuss and would really like to have you share some stories/information. I know 
+my subscribers would enjoy your work. If you are even remotely interested, feel free to 
+send me an email.';
+$lang['showlinkbacks']         = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really liuke to have you share 
+some stories/information. I know my subscribers would enjoy 
+your work. If you are even remotely interested, feel free to send me an email.';
+$lang['showtags']              = '20';
+$lang['showeditbtn']           = 'Have you ever thought about writing an ebook or guest authoring on other 
+blogs? I have a blog based on the same ideas you discuss 
+and would really like to have you share some stories/information. I kknow 
+my subscribers would enjoy your work. If you are even remotely interested, feel free to send me an email.';
+$lang['doredirect']            = 'Have you ever thought about writing an ebook or 
+guest authoring on othe blogs? I have a blog based on the 
+same ideas you discuss and would really like tto have you share some 
+stories/information. I know my subscribers would 
+enjioy your work. If you are even remotely interested, feel free to send me 
+an email.';
+$lang['doindent']              = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog bawed on the same ideas you discuss and would really like to have you share some stories/information. I 
+know my subscribers would enjoy your work.
+If you are even remotely interested, feel free to send me an email.';
+$lang['linkonly']              = 'http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['title']                 = 'I am the new one';
+$lang['pageexists']            = 'http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['parlink']               = 'http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['safeindex']             = 'Finlay';
+$lang['order']                 = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like 
+to have you share some stories/information. I know my subscribers 
+would enjoy your work. If you are even remotely interested, 
+feel free to send me an email.';
+$lang['order_o_id']            = 'Have yyou ever thought about writing an ebook or guest authoring on other blogs?
+
+I have a blog based on the same ideas you discuss and would really like to 
+have you share some stories/information. I know 
+my subscribers would enjoy yokur work. If you are 
+even remotely interested, feel free to send me an email.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Have you ever thought about wrkting an ebook or guest authoring on other blogs?
+I have a blog based on the same ideas you discuss and would really like to have you share some stories/information. I know my subscribers would enjoy your work.
+If you are even remoteely interested, feel free to send me an email.';
+$lang['order_o_modified']      = 'Have you ever thought about writing an ebook orr guest authoring on other blogs?
+
+I have a blog based on the same idas you discuss and would 
+really like to have you shade some stories/information. 
+I know my subscribers would enjoy your work. If youu 
+are even remotely interested, feel free to send me an email.';
+$lang['order_o_indexmenu']     = 'Have you ever thought about writing an ebook or guest 
+authoring on other blogs? I have a blog based on thee same ideas you discuss andd would really like to have you share some stories/information. I know my subscribers would enjoy your work.
+If you are even remotely interested, feel free to send me an email.';
+$lang['order_o_custom']        = 'Have you ever thought about writing an ebook or guest authoring on other blogs?
+I have a blog based on tthe same ideas you discuss 
+and would really like to have you share some stories/information. 
+Iknow my subscribers would enjoy ypur work. If you are evenn remotely 
+interested, feel free to send me ann email.';
+$lang['rsort']                 = 'Hofsos';
+$lang['depth']                 = 'Finlay';
+$lang['readmore']              = 'http://Edm.Singtaomagazine.com/system/core/clickurl?a=cjdvaDBrZnVxS3JJNnFQNkhOMkJNM2dWNFgxQm9FUHY=&u=https://anotepad.com/note/read/e3w5ck9y';
+$lang['debugoutput']           = 'Have you ever thought about writng an ebook or guest 
+authoring on other blogs? I have a blog based on the same 
+ideas you discuss and would really like tto have you share some stories/information. I know my subscribers would enjoy your 
+work. If you are even emotely interested, feel free to send me an email.';
