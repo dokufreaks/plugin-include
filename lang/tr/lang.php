d5eba6aba90ae11e79ceee388626e0de2511f146
Diyarbakir escort ajansi rehberi <lucillewedel@gmail.com>
2025-08-18 15:10:37 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir escort ajansi rehberi <lucillewedel@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Write more, thats alll I have to say. Literally, it seems as though you 
+relied on the video to make your point. You clearly know what youre talking about, why wase 
+your intelligence on just posting videos to your weblog when you could be giving us something informative 
+to read?';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,78 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Diyarbakir escort ajansi rehberi <lucillewedel@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I finally registered';
+$lang['firstseconly']          = 'Write more, thats all I have to say. Literally, it seems as though you relied on the video 
+to make your point. You clearly know what youre talking 
+about, why waste your intelligence on just postkng videos to your weblog when you could be giving us something informative 
+to read?';
+$lang['showtaglogos']          = '14';
+$lang['showfooter']            = 'Jacques Cantara is what you might call me but it isn\'t the most masculine specify. To research fashion is individuals she\'s been doing a long time. Filing is how he generates a living. Pennsylvania is where me and my wife live but my wife wants us to move. Check out her website here: https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['showlink']              = 'https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['showpermalink']         = 'Write more, thats all I have to say. Literally, it seems as though you 
+relied on the video to make your point. You clearly 
+know what youre talking about, why waste your 
+intelligence on just posting videos to your weblog when youu could be giving us something informative to read?';
+$lang['showdate']              = 'Write more, thats all I have to say. Literally,
+it seems as though you relied on the video to make your point.
+You clearly know whst yourfe talking about, why waste your intelligence on just posting videos to your weblog when you could be giving us 
+something informative to read?';
+$lang['showmdate']             = 'Write more, thats all I have to say. Literally, it seems as though you relied on the video to make your point.
+You clearly know what youre talking about, why waste your intelligence on jjust posting videos to your weblog when yoou could be giving us something infrmative to read?';
+$lang['showuser']              = 'Write more, thats all I have to say. Literally, 
+it seems as though you relied onn the video to make your point.
+You clearly know what youre talking about, why waste your intelligence 
+on just posting videos to your weblog when you could be giving us something 
+imformative to read?';
+$lang['showcomments']          = 'Write more, thats alll I have to say.Literally, it seems ass thoough yyou relied on the video to make your point.
+You clearly know what youre talking about, why waste your 
+intelligence onn just posting videos to your weblog when you could be giving us something informative to read?';
+$lang['showlinkbacks']         = 'Write more, thats all I have to say. Literally, it seems as thokugh you relied on the video to make your point.
+You clearly know whaat youre talking about, why waste your intelligence on just posting 
+videos to your weblog when you culd be giving us something informative to read?';
+$lang['showtags']              = '14';
+$lang['showeditbtn']           = 'Write more, thats all I have to say. Literally, it seems as though you relied on the video to make your point.
+You clearly know what youre talking about, why waste yopur intelligence on just posting videos to your weblog when you could be giving uus something informative to read?';
+$lang['doredirect']            = 'Write more, thats all I hafe to say. Literally, it seems as 
+though you relied on the video to make your point.
+You clearly know what youre talking about, why waste your intelligence on just posting videos to your weblog when you could be giving us something informative to read?';
+$lang['doindent']              = 'Write more, thats all I have to say. Literally,
+it seems as though you relied on the video to make your point.
+
+You clearly know what youre talking about, why waste your intelligence on just posting videos to your weblog when you could be 
+giving us something informative to read?';
+$lang['linkonly']              = 'https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['title']                 = 'Im happy I finally registered';
+$lang['pageexists']            = 'https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['parlink']               = 'https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['safeindex']             = 'Lucille';
+$lang['order']                 = 'Write more, thats all I have to say. Literally, it seems as though you relied on the video to make your point.
+You clearly know what youre talking about, why waste your intelligence on just posting videos to your weblog when you could bbe giving us something informative to read?';
+$lang['order_o_id']            = 'Write more, thats all I hwve to say. Literally, it seems as though 
+you relied on the video to make your point. You clearly know what youre talking about, why waste your 
+intelligence on just posting videos to your weblog when you could be 
+giving us something informative to read?';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Write more, thats all I have tto say. Literally, it seems as though you relied on the video to make your point.
+You clearly know what youre talking about, why waste your intelligence 
+on just posting videos to your weblog when you could be 
+giving uus something informative to read?';
+$lang['order_o_modified']      = 'Write more, thats all I have to say. Literally, it seems as 
+though you relied on the video to make your point. You clearly know what youre talking about, whyy waste your intelligence on just posting videos to your weblog when you could be giving us something informative to read?';
+$lang['order_o_indexmenu']     = 'Write more, thats all I have to say. Literally, it seems as 
+though you relied on the video to make your point. You clearrly 
+know what youre talking about, why waste your intelligence on just posting ideos to your weblog when you could be giving us something informative to 
+read?';
+$lang['order_o_custom']        = 'Write more, thats all I have to say. Literally, it seems as though 
+you relied on the video to make your point. You cleadly know what youre talking about, why waste your intelligence on ust posting videos to your weblog when you could be giving us something informative to read?';
+$lang['rsort']                 = 'Kartause-Ittingen';
+$lang['depth']                 = 'Lucille';
+$lang['readmore']              = 'https://inspiredcollectors.com/component/k2/author/108629-eskorticindecakmak';
+$lang['debugoutput']           = 'Write more, thats all I have to say. Literally, it seems as though you relued onn 
+the video to make your point. You clearly know what youre talking about, why 
+waste your intelligence on just posting videos 
+to your weblog whe you could be giving us something informative to read?';
