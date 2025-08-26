89b90ef0bfe4d5f3004d8b4c9cc1c8256d848b1c
honda vezel 2025 price in sri lanka <muoidaddario@outlook.com>
2025-08-27 01:40:09 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author honda vezel 2025 price in sri lanka <muoidaddario@outlook.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import 
+cars from Japan to Sri Lanka.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,104 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author honda vezel 2025 price in sri lanka <muoidaddario@outlook.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hello!';
+$lang['firstseconly']          = 'Good day! Would you mind if I share your blog with my 
+zynga group? There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri Lanka.';
+$lang['showtaglogos']          = '20';
+$lang['showfooter']            = 'I represent Autolands, a trusted vehicle import company in Nugegoda, Sri Lanka. We assist customers with vehicle import Sri Lanka. Many buyers ask about vehicle permit Sri Lanka 2025, and we provide clear guidance on import cars from Japan to Sri Lanka.';
+$lang['showlink']              = 'https://juzrnom9434.livejournal.com/505.html';
+$lang['showpermalink']         = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client 
+with import cars from Japan to Sri Lanka.';
+$lang['showdate']              = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars 
+from Japan to Sri Lanka.';
+$lang['showmdate']             = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with 
+import cars from Japan to Sri Lanka.';
+$lang['showuser']              = 'Good day! Would you mind if I share your blog with my zynga group?
+
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import 
+cars from Japan to Sri Lanka.';
+$lang['showcomments']          = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your 
+content. Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri Lanka.';
+$lang['showlinkbacks']         = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri Lanka.';
+$lang['showtags']              = '15';
+$lang['showeditbtn']           = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate 
+your content. Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import 
+cars from Japan to Sri Lanka.';
+$lang['doredirect']            = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars 
+from Japan to Sri Lanka.';
+$lang['doindent']              = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from 
+Japan to Sri Lanka.';
+$lang['linkonly']              = 'https://juzrnom9434.livejournal.com/505.html';
+$lang['title']                 = 'Just want to say Hello!';
+$lang['pageexists']            = 'https://juzrnom9434.livejournal.com/505.html';
+$lang['parlink']               = 'https://juzrnom9434.livejournal.com/505.html';
+$lang['safeindex']             = 'Muoi';
+$lang['order']                 = 'Good day! Would you mind if I share your blog 
+with my zynga group? There\'s a lot of folks that I think would 
+really appreciate your content. Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri Lanka.';
+$lang['order_o_id']            = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to 
+Sri Lanka.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Good day! Would you mind if I share your blog with my 
+zynga group? There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri 
+Lanka.';
+$lang['order_o_modified']      = 'Good day! Would you mind if I share your blog with my zynga group?
+There\'s a lot of folks that I think would really appreciate 
+your content. Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri 
+Lanka.';
+$lang['order_o_indexmenu']     = 'Good day! Would you mind if I share your blog with my 
+zynga group? There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars 
+from Japan to Sri Lanka.';
+$lang['order_o_custom']        = 'Good day! Would you mind if I share your 
+blog with my zynga group? There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars from Japan to Sri Lanka.';
+$lang['rsort']                 = 'Craggan';
+$lang['depth']                 = 'Muoi';
+$lang['readmore']              = 'https://juzrnom9434.livejournal.com/505.html';
+$lang['debugoutput']           = 'Good day! Would you mind if I share your blog with my zynga 
+group? There\'s a lot of folks that I think would really appreciate your content.
+Please let me know. Many thanks
+Thanks for sharing, I recently helped a client with import cars 
+from Japan to Sri Lanka.';
