8bcedac6cb90658e9551e53fa67e7cc0d37fca1f
Auto Land <gregsessions@wanadoo.fr>
2025-08-28 11:50:46 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Auto Land <gregsessions@wanadoo.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Good post. I learn something totally new and challenging 
+on blogs I stumbleupon on a daily basis. It will always be exciting to read through articles from other writers and use 
+something from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri Lanka.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,113 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Auto Land <gregsessions@wanadoo.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hello!';
+$lang['firstseconly']          = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily 
+basis. It will always be exciting to read through articles from 
+other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri Lanka.';
+$lang['showtaglogos']          = '19';
+$lang['showfooter']            = 'Working with Autolands, I’ve helped countless clients smoothly manage Japanese car import Sri Lanka. Our mission is to simplify the process and make importing vehicles into Sri Lanka easier than ever.';
+$lang['showlink']              = 'https://carsaleinnugegoda.over-blog.com/2025/08/toyota-aqua-for-sale-sri-lanka-import.html';
+$lang['showpermalink']         = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a 
+daily basis. It will always be exciting to read through articles from other writers 
+and use something from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri 
+Lanka.';
+$lang['showdate']              = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily basis.
+It will always be exciting to read through articles from other writers and use something 
+from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, 
+especially personal vehicle import Sri Lanka.';
+$lang['showmdate']             = 'Good post. I learn something totally new and challenging on blogs I 
+stumbleupon on a daily basis. It will always be exciting to read 
+through articles from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, 
+especially personal vehicle import Sri Lanka.';
+$lang['showuser']              = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily basis.
+It will always be exciting to read through articles from other writers and use something 
+from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import 
+Sri Lanka.';
+$lang['showcomments']          = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a 
+daily basis. It will always be exciting to read through articles from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri Lanka.';
+$lang['showlinkbacks']         = 'Good post. I learn something totally new and 
+challenging on blogs I stumbleupon on a daily basis.
+It will always be exciting to read through articles from other writers and use something 
+from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri Lanka.';
+$lang['showtags']              = '28';
+$lang['showeditbtn']           = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a 
+daily basis. It will always be exciting to read through articles 
+from other writers and use something from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially 
+personal vehicle import Sri Lanka.';
+$lang['doredirect']            = 'Good post. I learn something totally new and challenging 
+on blogs I stumbleupon on a daily basis. It will always be exciting to read through articles from other writers and use something from other websites.
+
+
+Great article! Importing cars is popular here in Sri 
+Lanka, especially personal vehicle import Sri Lanka.';
+$lang['doindent']              = 'Good post. I learn something totally new and challenging on blogs 
+I stumbleupon on a daily basis. It will always be exciting to read through articles from 
+other writers and use something from other websites. 
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri Lanka.';
+$lang['linkonly']              = 'https://carsaleinnugegoda.over-blog.com/2025/08/toyota-aqua-for-sale-sri-lanka-import.html';
+$lang['title']                 = 'Just want to say Hello!';
+$lang['pageexists']            = 'https://carsaleinnugegoda.over-blog.com/2025/08/toyota-aqua-for-sale-sri-lanka-import.html';
+$lang['parlink']               = 'https://carsaleinnugegoda.over-blog.com/2025/08/toyota-aqua-for-sale-sri-lanka-import.html';
+$lang['safeindex']             = 'Greg';
+$lang['order']                 = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily 
+basis. It will always be exciting to read through articles from other writers and 
+use something from other websites. 
+Great article! Importing cars is popular here in Sri 
+Lanka, especially personal vehicle import Sri 
+Lanka.';
+$lang['order_o_id']            = 'Good post. I learn something totally new and challenging 
+on blogs I stumbleupon on a daily basis. It will always be 
+exciting to read through articles from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle import Sri 
+Lanka.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily basis.
+It will always be exciting to read through articles from 
+other writers and use something from other websites. 
+Great article! Importing cars is popular here in Sri Lanka,
+especially personal vehicle import Sri Lanka.';
+$lang['order_o_modified']      = 'Good post. I learn something totally new and challenging 
+on blogs I stumbleupon on a daily basis. It will always be exciting to 
+read through articles from other writers and use something from other 
+websites. 
+Great article! Importing cars is popular here in Sri Lanka,
+especially personal vehicle import Sri Lanka.';
+$lang['order_o_indexmenu']     = 'Good post. I learn something totally new and challenging on blogs I 
+stumbleupon on a daily basis. It will always be exciting 
+to read through articles from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, especially 
+personal vehicle import Sri Lanka.';
+$lang['order_o_custom']        = 'Good post. I learn something totally new and challenging on blogs I 
+stumbleupon on a daily basis. It will always be exciting to read through articles 
+from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri 
+Lanka, especially personal vehicle import Sri Lanka.';
+$lang['rsort']                 = 'Venetico Marina';
+$lang['depth']                 = 'Greg';
+$lang['readmore']              = 'https://carsaleinnugegoda.over-blog.com/2025/08/toyota-aqua-for-sale-sri-lanka-import.html';
+$lang['debugoutput']           = 'Good post. I learn something totally new and challenging on blogs I stumbleupon on a daily basis.
+
+It will always be exciting to read through articles 
+from other writers and use something from other websites.
+
+Great article! Importing cars is popular here in Sri Lanka, especially personal vehicle 
+import Sri Lanka.';
