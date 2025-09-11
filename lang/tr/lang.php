f43e33456be382b0cfb81a398b707fd4f9209b16
최신 야동 <benjamin.connolly@neuf.fr>
2025-09-11 04:25:05 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author 최신 야동 <benjamin.connolly@neuf.fr>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = '1개의 채널 페이더, 1개의 채널 컨트롤, 1개의 오디오 에디터, 2개의 
+LCD 모니터, 2 베이 섀시로 구성된 완벽한 Fairlight 스튜디오 콘솔.';
