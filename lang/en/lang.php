e294763ad7c6f78c298848795f06c6118cd2f78e
Купить асик для добычи криптовалюты <modestabowman@gmail.com>
2025-09-16 22:45:45 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,12 +1,14 @@
 <?php
+
 /**
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ *
+ * @author Купить асик для добычи криптовалюты <modestabowman@gmail.com>
+ * @author Esther Brunner <wikidesign@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['readmore']   = '→ Read more...';
-
-//Setup VIM: ex: et ts=2 :
+$lang['readmore']              = 'Today, while I was at work, my cousin stole my apple 
+ipad and tested to see if it can survive a 40 foot drop, just so 
+she can be a youtube sensation. My iPad is now destroyed and she has 83 views.
+I know this is completely off topic but I had to share it with someone!';
