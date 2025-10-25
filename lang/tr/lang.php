5009eea0b9a6556aded353dd6684654edcea7ed0
уничтожение мышей <christy.smart@hotmail.com>
2025-10-25 17:30:06 +0200

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,18 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author уничтожение мышей <christy.smart@hotmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для 
+уничтожения тараканов. Опытные 
+дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование 
+и препараты. Гарантия результата.
+Срочный выезд. Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,214 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author уничтожение мышей <christy.smart@hotmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'дезинфекция автомобилей';
+$lang['firstseconly']          = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства 
+— доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showtaglogos']          = '11';
+$lang['showfooter']            = 'Уборка после смерти с дезинфекцией в .Профессиональные услуги.Профессиональная уборка с дезинфекцией после смерти.Используем специальные моющие и дезинфицирующие средства.Обрабатываем все поверхности и труднодоступные места.Устраняем запахи.Тактичный подход.Срочный выезд. https://dezchel.ru/dezinfekcziya-pomeshhenij-v-chelyabinske-s-garantiej/dezinfekcziya-posle-smerti-v-chelyabinske-s-garantiej/';
+$lang['showlink']              = 'https://Dezchel.ru';
+$lang['showpermalink']         = 'Вызов специалиста для уничтожения тараканов.
+
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showdate']              = 'Вызов специалиста для 
+уничтожения тараканов. Срочно.
+
+Нужен специалист по уничтожению 
+тараканов? Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает 
+вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showmdate']             = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут 
+осмотр и обработку. Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showuser']              = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут 
+осмотр и обработку. Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showcomments']          = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению 
+тараканов? Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает 
+вызов специалиста для уничтожения 
+тараканов. Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — 
+доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showlinkbacks']         = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов 
+специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+
+Используем профессиональное оборудование и препараты.
+
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — 
+доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['showtags']              = '21';
+$lang['showeditbtn']           = 'Вызов специалиста для уничтожения тараканов.
+
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов 
+специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и 
+препараты. Гарантия результата.
+Срочный выезд. Не тратьте время 
+на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['doredirect']            = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+
+Не тратьте время на бесполезные средства 
+— доверьтесь профессионалам!
+
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['doindent']              = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов 
+специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства 
+— доверьтесь профессионалам!
+
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['linkonly']              = 'https://Dezchel.ru';
+$lang['title']                 = 'дезинфекция автомобилей';
+$lang['pageexists']            = 'https://Dezchel.ru';
+$lang['parlink']               = 'https://Dezchel.ru';
+$lang['safeindex']             = 'Christy';
+$lang['order']                 = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь 
+профессионалам! https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['order_o_id']            = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим 
+проблему! Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и 
+обработку. Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения 
+тараканов. Опытные дезинфекторы проведут осмотр 
+и обработку. Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный 
+выезд. Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['order_o_modified']      = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+
+Опытные дезинфекторы проведут осмотр и 
+обработку. Используем профессиональное 
+оборудование и препараты. Гарантия результата.
+Срочный выезд. Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['order_o_indexmenu']     = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+
+Мы приедем быстро и решим проблему!
+
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+
+Используем профессиональное оборудование 
+и препараты. Гарантия результата.
+Срочный выезд. Не тратьте время на бесполезные 
+средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['order_o_custom']        = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению 
+тараканов? Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+Используем профессиональное оборудование и 
+препараты. Гарантия результата.
+Срочный выезд. Не тратьте время на 
+бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
+$lang['rsort']                 = 'Glaister';
+$lang['depth']                 = 'Christy';
+$lang['readmore']              = 'https://Dezchel.ru';
+$lang['debugoutput']           = 'Вызов специалиста для уничтожения тараканов.
+Срочно. Нужен специалист по уничтожению тараканов?
+Мы приедем быстро и решим проблему!
+Наша санэпидемстанция предлагает вызов специалиста для уничтожения тараканов.
+Опытные дезинфекторы проведут осмотр и обработку.
+
+Используем профессиональное оборудование и препараты.
+Гарантия результата. Срочный выезд.
+
+Не тратьте время на бесполезные средства — доверьтесь профессионалам!
+https://dezchel.ru/professionalnaya-dezinsekcziya-v-chelyabinske-uslugi-dezchel/unichtozhenie-tarakanov-v-chelyabinske-s-garantiej';
