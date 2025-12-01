9aa4cd50d277fab0c61bedb2ba69dca4552ed922
gsa backlinks seo <luciana.jung@aol.com>
2025-12-01 15:00:45 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author gsa backlinks seo <luciana.jung@aol.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me 
+know if this is happening to them as well?
+This may be a issue with my internet browser because I\'ve had this happen before.
+Thank you';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,115 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author gsa backlinks seo <luciana.jung@aol.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I finally signed up';
+$lang['firstseconly']          = 'I don\'t know if it\'s just me or if perhaps 
+everyone else experiencing issues with your website.
+It appears as though some of the written text on your 
+posts are running off the screen. Can somebody else 
+please provide feedback and let me know if this is happening to them 
+as well? This may be a issue with my internet browser because I\'ve had this 
+happen before. Thank you';
+$lang['showtaglogos']          = '23';
+$lang['showfooter']            = 'Hi there! :) My name is Luciana, I\'m a student studying History from Heimertingen, Germany.';
+$lang['showlink']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['showpermalink']         = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with 
+your website. It appears as though some of the written text 
+on your posts are running off the screen. Can somebody 
+else please provide feedback and let me know if this is happening to them as well?
+
+This may be a issue with my internet browser because I\'ve had 
+this happen before. Thank you';
+$lang['showdate']              = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running 
+off the screen. Can somebody else please provide feedback and let me 
+know if this is happening to them as well? This may be 
+a issue with my internet browser because I\'ve had this happen before.
+
+Thank you';
+$lang['showmdate']             = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me know if this is happening to them as well?
+
+This may be a issue with my internet browser because I\'ve had this happen before.
+
+Thank you';
+$lang['showuser']              = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts 
+are running off the screen. Can somebody else please provide feedback and let me know if this is happening to them 
+as well? This may be a issue with my internet browser because I\'ve had this happen before.
+
+Thank you';
+$lang['showcomments']          = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with 
+your website. It appears as though some of the written text on your 
+posts are running off the screen. Can somebody else please provide 
+feedback and let me know if this is happening to 
+them as well? This may be a issue with my internet browser because I\'ve 
+had this happen before. Thank you';
+$lang['showlinkbacks']         = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me know if this is happening to them as well?
+This may be a issue with my internet browser because 
+I\'ve had this happen before. Thank you';
+$lang['showtags']              = '13';
+$lang['showeditbtn']           = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your 
+website. It appears as though some of the 
+written text on your posts are running off the screen. Can somebody 
+else please provide feedback and let me know if 
+this is happening to them as well? This may be a issue with my internet browser 
+because I\'ve had this happen before. Thank you';
+$lang['doredirect']            = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues 
+with your website. It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me know if this is happening to them 
+as well? This may be a issue with my internet browser because I\'ve had this happen before.
+Thank you';
+$lang['doindent']              = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with 
+your website. It appears as though some of the written text on your posts 
+are running off the screen. Can somebody else please provide feedback and let me know if this is happening to 
+them as well? This may be a issue with my internet browser 
+because I\'ve had this happen before. Thank you';
+$lang['linkonly']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['title']                 = 'Im happy I finally signed up';
+$lang['pageexists']            = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['parlink']               = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['safeindex']             = 'Luciana';
+$lang['order']                 = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing 
+issues with your website. It appears as though some 
+of the written text on your posts are running off 
+the screen. Can somebody else please provide feedback and let 
+me know if this is happening to them as well?
+This may be a issue with my internet browser 
+because I\'ve had this happen before. Thank you';
+$lang['order_o_id']            = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off the screen. Can somebody 
+else please provide feedback and let me know if this is happening to them as well?
+This may be a issue with my internet browser because I\'ve 
+had this happen before. Thank you';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts 
+are running off the screen. Can somebody else please provide feedback and let me know if this is happening 
+to them as well? This may be a issue with my internet browser because I\'ve had this happen before.
+Thank you';
+$lang['order_o_modified']      = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me know if this is happening 
+to them as well? This may be a issue with my internet browser because 
+I\'ve had this happen before. Thank you';
+$lang['order_o_indexmenu']     = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing 
+issues with your website. It appears as though some of the written text on your posts are running off the screen. Can somebody else please provide feedback and let me know if this 
+is happening to them as well? This may be a issue with my internet browser because I\'ve had 
+this happen before. Thank you';
+$lang['order_o_custom']        = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues 
+with your website. It appears as though some of the written text 
+on your posts are running off the screen. Can somebody else please provide feedback and let me know if 
+this is happening to them as well? This may be a issue with my internet browser because I\'ve had this 
+happen before. Thank you';
+$lang['rsort']                 = 'Heimertingen';
+$lang['depth']                 = 'Luciana';
+$lang['readmore']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['debugoutput']           = 'I don\'t know if it\'s just me or if perhaps everyone else experiencing issues with your website.
+It appears as though some of the written text on your posts are running off 
+the screen. Can somebody else please provide feedback and let 
+me know if this is happening to them as well? This may be a issue 
+with my internet browser because I\'ve had this happen before.
+Thank you';
