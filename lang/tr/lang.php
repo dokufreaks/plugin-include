2ddb3ebf7bcdf7a9465bcc9a37df8b5e83a117af
GSA SER verified lists <florhurst716@gmail.com>
2026-01-02 05:10:25 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author GSA SER verified lists <florhurst716@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Adhere to authoritative platforms appropriate to 
+your niche and place.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,40 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author GSA SER verified lists <florhurst716@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Exactly How To Develop Backlinks';
+$lang['firstseconly']          = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showtaglogos']          = '23';
+$lang['showfooter']            = '20 yr old Tax Accountant Eleen Rowbrey, hailing from Laurentiens enjoys watching movies like Cold Turkey and Table tennis. Took a trip to Redwood National and State Parks and drives a Alfa Romeo 8C 2300 Monza.';
+$lang['showlink']              = 'https://gsaserlinkbuilding.blogspot.com';
+$lang['showpermalink']         = 'Adhere to authoritative platforms appropriate to 
+your niche and place.';
+$lang['showdate']              = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showmdate']             = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showuser']              = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showcomments']          = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showlinkbacks']         = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['showtags']              = '26';
+$lang['showeditbtn']           = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['doredirect']            = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['doindent']              = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['linkonly']              = 'https://gsaserlinkbuilding.blogspot.com';
+$lang['title']                 = 'Exactly How To Develop Backlinks';
+$lang['pageexists']            = 'https://gsaserlinkbuilding.blogspot.com';
+$lang['parlink']               = 'https://gsaserlinkbuilding.blogspot.com';
+$lang['safeindex']             = 'Flor';
+$lang['order']                 = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['order_o_id']            = 'Adhere to authoritative platforms appropriate to your niche and place.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Adhere to authoritative platforms appropriate to your 
+niche and place.';
+$lang['order_o_modified']      = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['order_o_indexmenu']     = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['order_o_custom']        = 'Adhere to authoritative platforms appropriate to your niche and place.';
+$lang['rsort']                 = 'Klempau';
+$lang['depth']                 = 'Flor';
+$lang['readmore']              = 'https://gsaserlinkbuilding.blogspot.com';
+$lang['debugoutput']           = 'Adhere to authoritative platforms appropriate to your niche and place.';
