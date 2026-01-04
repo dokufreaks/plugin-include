8fad64c2c9517c80cc633814a8f509ff997fe854
Shantell <henry.louise@alice.it>
2026-01-04 02:00:45 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Shantell <henry.louise@alice.it>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+
+My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide here.
+Please let me know if this alright with you. 
+Thank you!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,99 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Shantell <henry.louise@alice.it>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hello.';
+$lang['firstseconly']          = 'Do you mind if I quote a few of your posts as long as I 
+provide credit and sources back to your website?
+
+My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide here.
+
+Please let me know if this alright with you. Thank you!';
+$lang['showtaglogos']          = '15';
+$lang['showfooter']            = 'I am 33 years old and my name is Henry Louise. I life in Purmerend (Netherlands).';
+$lang['showlink']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['showpermalink']         = 'Do you mind if I quote a few of your posts as long as I 
+provide credit and sources back to your website? My website is in the exact same niche as 
+yours and my visitors would really benefit from some of the 
+information you provide here. Please let me know if this alright with you.
+Thank you!';
+$lang['showdate']              = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours and my visitors would 
+really benefit from some of the information you provide here.
+
+Please let me know if this alright with you. Thank you!';
+$lang['showmdate']             = 'Do you mind if I quote a few of your posts as long as I provide credit 
+and sources back to your website? My website is in the 
+exact same niche as yours and my visitors would really benefit from some 
+of the information you provide here. Please let me know 
+if this alright with you. Thank you!';
+$lang['showuser']              = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours 
+and my visitors would really benefit from 
+some of the information you provide here. Please let me know 
+if this alright with you. Thank you!';
+$lang['showcomments']          = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours and my 
+visitors would really benefit from some of the information you provide here.
+Please let me know if this alright with you. Thank you!';
+$lang['showlinkbacks']         = 'Do you mind if I quote a few of your posts as long as 
+I provide credit and sources back to your website? My website 
+is in the exact same niche as yours and my visitors would really benefit from some of the information you provide 
+here. Please let me know if this alright with you.
+Thank you!';
+$lang['showtags']              = '13';
+$lang['showeditbtn']           = 'Do you mind if I quote a few of your posts as long as I provide credit and sources 
+back to your website? My website is in the exact same niche as yours and my visitors would really benefit from 
+some of the information you provide here. Please let me 
+know if this alright with you. Thank you!';
+$lang['doredirect']            = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+
+My website is in the exact same niche as yours and my visitors would really 
+benefit from some of the information you provide here.
+Please let me know if this alright with you. Thank you!';
+$lang['doindent']              = 'Do you mind if I quote a few of your posts as long as 
+I provide credit and sources back to your website? My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide here.
+Please let me know if this alright with you.
+Thank you!';
+$lang['linkonly']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['title']                 = 'Just want to say Hello.';
+$lang['pageexists']            = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['parlink']               = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['safeindex']             = 'Henry';
+$lang['order']                 = 'Do you mind if I quote a few of your posts as long as I provide credit and 
+sources back to your website? My website is in the exact same niche 
+as yours and my visitors would really benefit from some of 
+the information you provide here. Please let me know if this alright 
+with you. Thank you!';
+$lang['order_o_id']            = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back 
+to your website? My website is in the exact same niche as 
+yours and my visitors would really benefit from some of the information you provide here.
+Please let me know if this alright with you. Thank you!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your 
+website? My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide here.
+
+Please let me know if this alright with you. Thank you!';
+$lang['order_o_modified']      = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide here.
+Please let me know if this alright with you.
+Thank you!';
+$lang['order_o_indexmenu']     = 'Do you mind if I quote a few of your posts as long as I 
+provide credit and sources back to your website?
+My website is in the exact same niche as yours and my visitors 
+would really benefit from some of the information you 
+provide here. Please let me know if this alright with you.
+
+Thank you!';
+$lang['order_o_custom']        = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide 
+here. Please let me know if this alright with you. Thank you!';
+$lang['rsort']                 = 'Purmerend';
+$lang['depth']                 = 'Henry';
+$lang['readmore']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['debugoutput']           = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your website?
+My website is in the exact same niche as yours and my visitors would really benefit from some of the information you provide 
+here. Please let me know if this alright with you. Thank you!';
