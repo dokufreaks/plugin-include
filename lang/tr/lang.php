c62232d48127f118d605ac3b2d2441f601f46650
Bookieforums betting community <noelia_maxwell@gmail.com>
2026-01-31 14:40:17 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Bookieforums betting community <noelia_maxwell@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,51 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Bookieforums betting community <noelia_maxwell@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Im happy I finally signed up';
+$lang['firstseconly']          = 'As the admin of this web site is working, no question very shortly it will be renowned,
+due to its feature contents.';
+$lang['showtaglogos']          = '12';
+$lang['showfooter']            = 'My name is Noelia Maxwell but everybody calls me Noelia. I\'m from Austria. I\'m studying at the high school (2nd year) and I play the Pedal Steel Guitar for 10 years. Usually I choose music from my famous films :D. 
+I have two brothers. I love Woodworking, watching movies and Vehicle restoration.';
+$lang['showlink']              = 'http://www.hjjang.org/board_KnBG93/376474';
+$lang['showpermalink']         = 'As the admin of this web site is working,
+no question very shortly it will be renowned, due to its feature contents.';
+$lang['showdate']              = 'As the admin of this web site is working, no question very shortly it will be renowned, due to 
+its feature contents.';
+$lang['showmdate']             = 'As the admin of this web site is working, no question very shortly 
+it will be renowned, due to its feature contents.';
+$lang['showuser']              = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['showcomments']          = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['showlinkbacks']         = 'As the admin of this web site is working, no question very shortly it 
+will be renowned, due to its feature contents.';
+$lang['showtags']              = '14';
+$lang['showeditbtn']           = 'As the admin of this web site is working, no 
+question very shortly it will be renowned, due to its feature 
+contents.';
+$lang['doredirect']            = 'As the admin of this web site is working, no question very shortly it 
+will be renowned, due to its feature contents.';
+$lang['doindent']              = 'As the admin of this web site is working, no question very shortly it will be 
+renowned, due to its feature contents.';
+$lang['linkonly']              = 'http://www.hjjang.org/board_KnBG93/376474';
+$lang['title']                 = 'Im happy I finally signed up';
+$lang['pageexists']            = 'http://www.hjjang.org/board_KnBG93/376474';
+$lang['parlink']               = 'http://www.hjjang.org/board_KnBG93/376474';
+$lang['safeindex']             = 'Noelia';
+$lang['order']                 = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['order_o_id']            = 'As the admin of this web site is working, no question very shortly it 
+will be renowned, due to its feature contents.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['order_o_modified']      = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['order_o_indexmenu']     = 'As the admin of this web site is working, no question very shortly it will be renowned,
+due to its feature contents.';
+$lang['order_o_custom']        = 'As the admin of this web site is working, no question very shortly it will be renowned, due to its feature contents.';
+$lang['rsort']                 = 'Aschau';
+$lang['depth']                 = 'Noelia';
+$lang['readmore']              = 'http://www.hjjang.org/board_KnBG93/376474';
+$lang['debugoutput']           = 'As the admin of this web site is working, no question very 
+shortly it will be renowned, due to its feature contents.';
