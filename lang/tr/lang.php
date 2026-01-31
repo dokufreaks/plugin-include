c4f4da6046fc679484263060077ffbe74f51101e
best ser verified link list <altacrowley116@gmail.com>
2026-01-31 14:05:45 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author best ser verified link list <altacrowley116@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too 
+magnificent. I actually like what you have acquired here, really like what you are saying and the way 
+in which you say it. You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,115 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author best ser verified link list <altacrowley116@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just want to say Hi.';
+$lang['firstseconly']          = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you 
+are saying and the way in which you say it.
+
+You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['showtaglogos']          = '21';
+$lang['showfooter']            = 'Hi there! :) My name is Alta, I\'m a student studying Art from Olafsfjor?Ur, Iceland.';
+$lang['showlink']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['showpermalink']         = 'Wonderful goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you are saying and 
+the way in which you say it. You make it entertaining and you 
+still care for to keep it smart. I can not wait to read much more from you.
+This is actually a tremendous website.';
+$lang['showdate']              = 'Wonderful goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you 
+are saying and the way in which you say it. You make it 
+entertaining and you still care for to keep it smart. I can not wait to read much more from you.
+This is actually a tremendous website.';
+$lang['showmdate']             = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you 
+are saying and the way in which you say it. You make it entertaining and you still care for to keep it 
+smart. I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['showuser']              = 'Wonderful goods from you, man. I have understand your stuff previous 
+to and you are just too magnificent. I actually like what 
+you have acquired here, really like what you are saying and the way in which you say it.
+You make it entertaining and you still care for to keep it 
+smart. I can not wait to read much more from you.
+This is actually a tremendous website.';
+$lang['showcomments']          = 'Wonderful goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really 
+like what you are saying and the way in which you say it.
+You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['showlinkbacks']         = 'Wonderful goods from you, man. I have understand your stuff previous 
+to and you are just too magnificent. I actually like what you have acquired here, really like what 
+you are saying and the way in which you say it.
+You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['showtags']              = '16';
+$lang['showeditbtn']           = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what 
+you are saying and the way in which you say it. You make 
+it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['doredirect']            = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you are saying and the way in which you say 
+it. You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually 
+a tremendous website.';
+$lang['doindent']              = 'Wonderful goods from you, man. I have understand your stuff 
+previous to and you are just too magnificent. I actually like 
+what you have acquired here, really like what you are saying and the way in which you say it.
+You make it entertaining and you still care for to keep 
+it smart. I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['linkonly']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['title']                 = 'Just want to say Hi.';
+$lang['pageexists']            = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['parlink']               = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['safeindex']             = 'Alta';
+$lang['order']                 = 'Wonderful goods from you, man. I have understand your stuff previous to and 
+you are just too magnificent. I actually like what you have acquired here, really like what you are saying and the 
+way in which you say it. You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['order_o_id']            = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really 
+like what you are saying and the way in which you say it.
+
+You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a 
+tremendous website.';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you 
+are saying and the way in which you say it. You make it entertaining and you 
+still care for to keep it smart. I can not wait to read much more from you.
+This is actually a tremendous website.';
+$lang['order_o_modified']      = 'Wonderful goods from you, man. I have understand your 
+stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really 
+like what you are saying and the way in which you say it.
+You make it entertaining and you still care for to 
+keep it smart. I can not wait to read much more 
+from you. This is actually a tremendous website.';
+$lang['order_o_indexmenu']     = 'Wonderful goods from you, man. I have understand 
+your stuff previous to and you are just too magnificent.
+I actually like what you have acquired here, really like what you 
+are saying and the way in which you say it. You make it entertaining and you still care 
+for to keep it smart. I can not wait to read much 
+more from you. This is actually a tremendous website.';
+$lang['order_o_custom']        = 'Wonderful goods from you, man. I have understand your stuff previous to and you are 
+just too magnificent. I actually like what you have acquired here, really like what you are saying and the way in which you say it.
+
+You make it entertaining and you still care for to keep it smart.
+
+I can not wait to read much more from you. This is actually a tremendous website.';
+$lang['rsort']                 = 'Olafsfjor?Ur';
+$lang['depth']                 = 'Alta';
+$lang['readmore']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['debugoutput']           = 'Wonderful goods from you, man. I have understand your stuff previous to and you are just too magnificent.
+
+I actually like what you have acquired here, really like what you are saying and the 
+way in which you say it. You make it entertaining and you still care for to keep it smart.
+I can not wait to read much more from you. This is actually a tremendous website.';
