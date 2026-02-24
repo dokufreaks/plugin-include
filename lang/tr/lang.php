f153d1e8c7ecac2be896b9ca0817237ae7eaa743
best gsa link list <deanadasilva379@gmail.com>
2026-02-24 13:50:13 +0100

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author best gsa link list <deanadasilva379@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'I was suggested this blog by my cousin. I\'m no longer certain whether or 
+not this publish is written via him as nobody 
+else know such special approximately my trouble. 
+You\'re amazing! Thanks!';
--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -2,35 +2,76 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author best gsa link list <deanadasilva379@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noheader']              = 'Eklenen sayfaların/bölümlerin ilk başlığını gösterme';
-$lang['firstseconly']          = 'eklenen sayfaların sadece ilk bölümünü göster';
-$lang['showtaglogos']          = 'ilk etiket için resim göster';
-$lang['showfooter']            = 'alt tarafta, eklenen sayfa için bilgi göster';
-$lang['showlink']              = 'eklenen sayfanın ilk başlığını bağlantı haline getir';
-$lang['showpermalink']         = 'eklenen sayfanın altında kalıcı bağlantılar göster';
-$lang['showdate']              = 'eklenen sayfanın altında tarihleri göster';
-$lang['showmdate']             = 'eklenen sayfanın altında değiştirilme tarihlerini göster';
-$lang['showuser']              = 'eklenen sayfanın altında kullanıcı isimlerini göster';
-$lang['showcomments']          = 'eklenen sayfanın altında tartışmaları görüntüle (Bunun için Discussion eklentisi gereklidir)';
-$lang['showlinkbacks']         = 'eklenen sayfanın altında geri bağlantıları göster (Bunun için Linkback eklentisi gereklidir)';
-$lang['showtags']              = 'eklenen sayfanın altında etiketleri göster (Bunun için Tag eklentisi gereklidir)';
-$lang['showeditbtn']           = 'düzenle düğmesini göster';
-$lang['doredirect']            = 'eklenen sayfayı düzenledikten sonra orjinal sayfaya yönlendir';
-$lang['doindent']              = 'eklenen sayfaları dahil edildikleri sayfaya göre girintili yap';
-$lang['linkonly']              = 'içeriği göstermek yerine, sadece eklenen sayfaya bağlantı göster';
-$lang['title']                 = 'sayfa başlığını kullanma kapalı bile olsa, sayfanın ilk başlığını bağlantıda kullan (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['pageexists']            = 'sayfa mevcut değilse bağlantı gösterme (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['parlink']               = 'bağlantıya bir paragraf ekle  (yalnızca bağlantı göster seceneği etkinken çalışır)';
-$lang['safeindex']             = 'eklenmiş kamuya kapalı (non-public) sayfadan üstverinin (metadata) dizine alınmasına izin verme';
-$lang['order']                 = 'çok sayfalı eklemeler için sıralama ölçütü';
-$lang['order_o_id']            = 'sayfa kimliği (ID)';
+$lang['noheader']              = 'Just wanted to say Hello!';
+$lang['firstseconly']          = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via him as nobody else 
+know such special approximately my trouble. You\'re amazing!
+Thanks!';
+$lang['showtaglogos']          = '17';
+$lang['showfooter']            = 'My name is Deana and I am studying Dramatic Literature and History and Occupational Therapy at Rosedale / Australia.';
+$lang['showlink']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['showpermalink']         = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via 
+him as nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['showdate']              = 'I was suggested this blog by my cousin. I\'m no 
+longer certain whether or not this publish is written via him 
+as nobody else know such special approximately my trouble.
+
+You\'re amazing! Thanks!';
+$lang['showmdate']             = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish 
+is written via him as nobody else know such special approximately my 
+trouble. You\'re amazing! Thanks!';
+$lang['showuser']              = 'I was suggested this blog by my cousin. I\'m 
+no longer certain whether or not this publish is written via 
+him as nobody else know such special approximately my trouble.
+
+You\'re amazing! Thanks!';
+$lang['showcomments']          = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via him as nobody else know 
+such special approximately my trouble. You\'re amazing!
+Thanks!';
+$lang['showlinkbacks']         = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not 
+this publish is written via him as nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['showtags']              = '18';
+$lang['showeditbtn']           = 'I was suggested this blog by my cousin. I\'m 
+no longer certain whether or not this publish is written via him as 
+nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['doredirect']            = 'I was suggested this blog by my cousin. I\'m no longer 
+certain whether or not this publish is written via him as nobody else know such special 
+approximately my trouble. You\'re amazing! Thanks!';
+$lang['doindent']              = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via him as nobody else know such special 
+approximately my trouble. You\'re amazing! Thanks!';
+$lang['linkonly']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['title']                 = 'Just wanted to say Hello!';
+$lang['pageexists']            = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['parlink']               = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['safeindex']             = 'Deana';
+$lang['order']                 = 'I was suggested this blog by my cousin. I\'m no longer certain whether or 
+not this publish is written via him as nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['order_o_id']            = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not 
+this publish is written via him as nobody else 
+know such special approximately my trouble. You\'re amazing!
+Thanks!';
 $lang['order_o_title']         = 'başlık';
-$lang['order_o_created']       = 'oluşturma tarihi';
-$lang['order_o_modified']      = 'değiştirilme tarihi';
-$lang['order_o_indexmenu']     = 'indexmenu sözdizimi ile rasgele sıralı';
-$lang['order_o_custom']        = 'include sözdizimi ile rasgele sıralı';
-$lang['rsort']                 = 'eklenen sayfaların sıralamasını ters çevir';
-$lang['depth']                 = 'dahil edilecek en fazla isimalanı derinliği, sınırsız derinlik için 0';
+$lang['order_o_created']       = 'I was suggested this blog by my cousin. I\'m no longer certain whether 
+or not this publish is written via him as nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['order_o_modified']      = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via him as nobody else know such special approximately my trouble.
+You\'re amazing! Thanks!';
+$lang['order_o_indexmenu']     = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not 
+this publish is written via him as nobody else know such special approximately my 
+trouble. You\'re amazing! Thanks!';
+$lang['order_o_custom']        = 'I was suggested this blog by my cousin. I\'m no longer certain whether or not this publish is written via him as nobody else 
+know such special approximately my trouble. You\'re amazing!
+Thanks!';
+$lang['rsort']                 = 'Rosedale';
+$lang['depth']                 = 'Deana';
+$lang['readmore']              = 'https://www.facebook.com/gsaserverifiedlists/';
+$lang['debugoutput']           = 'I was suggested this blog by my cousin. I\'m no longer 
+certain whether or not this publish is written via him as nobody else know such special approximately 
+my trouble. You\'re amazing! Thanks!';
