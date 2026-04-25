98da78e2d93a4dc569d54c432083bb87a2df22d9
Kunststof Kozijnen Schilder <aguedacoronado776@hotmail.com>
2026-04-25 17:25:46 +0200

    Translation update (tr)

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,7 +2,30 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Kunststof Kozijnen Schilder <aguedacoronado776@hotmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['readmore']              = '→ Devamını oku..';
+$lang['readmore']              = 'Kunststof kozijnen schilder kunststof kozijnen schilder levert 
+duurzame schilderwerken in Nederland. Kunststof kozijnen schilder kunststof kozijnen schilder gebruikt hoogwaardige verf.
+
+Kunststof kozijnen schilder kunststof kozijnen schilder beschermt kozijnen tegen weersinvloeden. Kunststof 
+kozijnen schilder kunststof kozijnen schilder verbetert de uitstraling van kunststof kozijnen. Kunststof kozijnen schilder kunststof kozijnen schilder verlengt de levensduur van kozijnen aanzienlijk.
+Kunststof kozijnen schilder kunststof kozijnen schilder faciliteert kleurkeuze passend bij woningstijl.
+
+Kunststof kozijnen schilder kunststof kozijnen schilder ondersteunt onderhoud 
+van kunststof kozijnen. Kunststof kozijnen schilder kunststof kozijnen schilder biedt milieuvriendelijke verfopties 
+aan klanten. Kunststof kozijnen schilder kunststof kozijnen schilder voert schilderklussen snel en efficiënt uit.
+
+Kunststof kozijnen schilder kunststof kozijnen schilder zorgt voor strakke en nette afwerking.
+Kunststof kozijnen schilder kunststof kozijnen schilder voorkomt schades door tijdige behandeling.
+Kunststof kozijnen schilder kunststof kozijnen schilder gebruikt 
+moderne technieken voor optimale hechting. Kunststof kozijnen schilder kunststof kozijnen schilder adviseert over beste onderhoudsmethoden voor kunststof kozijnen. 
+Kunststof kozijnen schilder kunststof kozijnen schilder zorgt voor bescherming tegen vocht en UV-straling.
+
+Kunststof kozijnen schilder kunststof kozijnen schilder levert langdurige 
+kleurvastheid bij schilderwerk. Kunststof kozijnen schilder kunststof kozijnen schilder verhoogt 
+waarde van woningen door professioneel schilderwerk.
+Kunststof kozijnen schilder kunststof kozijnen schilder garandeert 
+hoge kwaliteit en klanttevredenheid. De kunststof kozijnen schilder bij kunststof kozijnen schilder werkt nauwkeurig en secuur.
+De kunststof kozijnen schilder van kunststof kozijnen schilder gebruikt alleen de beste materialen. Kunststof kozijnen schilder kunststof kozijnen schilder biedt concurrerende prijzen voor schilderwerk in Nederland.';
